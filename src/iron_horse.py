854179f8b327d4c97165df175f8cffc1c9d19171
andythenorth <mail@andythenorth.co.uk>
2013-09-10 21:32:32 +0100

    Codechange: strictly all files containing wagons will define multiple wagons, so rename accordingly

--- src/iron_horse.py
+++ src/iron_horse.py
@@ -28,16 +28,16 @@ import train
 from train import Train
 from vehicles import registered_vehicles
 
+from vehicles import box_cars
 from vehicles import chopper
+from vehicles import covered_hopper_cars
 from vehicles import geep
 from vehicles import gridiron
+from vehicles import mail_cars
+from vehicles import passenger_cars
+from vehicles import tank_cars
 from vehicles import whistler
 from vehicles import zebedee
-from vehicles import passenger_car
-from vehicles import mail_car
-from vehicles import box_cars
-from vehicles import tank_car
-from vehicles import covered_hopper_car
 
 def get_vehicles_in_buy_menu_order():
     sorted_vehicles = []
