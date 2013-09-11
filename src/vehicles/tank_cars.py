567e633099f92ee121163d6cb9233f78f706afd4
andythenorth <mail@andythenorth.co.uk>
2013-09-11 19:28:35 +0100

    Feature: add more tank cars

--- src/vehicles/tank_cars.py
+++ src/vehicles/tank_cars.py
@@ -1,8 +1,52 @@
 import global_constants
 from train import Train, TankCar
 
-vehicle = TankCar(numeric_id = 10050,
+vehicle = TankCar(numeric_id = 14010,
                 vehicle_set = 'euro',
+                vehicle_generation = 'gen_1',
+                title = 'Tank [Car]',
+                capacity_freight = 20,
+                replacement_id = '-none',
+                buy_cost = 22,
+                fixed_run_cost_factor = 3.5,
+                fuel_run_cost_factor = 1.0,
+                weight = 100,
+                vehicle_length = 6,
+                buy_menu_width = 32,
+                loading_speed = 20,
+                intro_date = 1900,
+                str_type_info = 'COASTER',
+                vehicle_life = 40,
+                graphics_status = '')
+
+vehicle.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
+
+vehicle = TankCar(numeric_id = 14020,
+                vehicle_set = 'univ',
+                vehicle_generation = 'gen_2',
+                title = 'Tank [Car]',
+                capacity_freight = 20,
+                replacement_id = '-none',
+                buy_cost = 22,
+                fixed_run_cost_factor = 3.5,
+                fuel_run_cost_factor = 1.0,
+                weight = 100,
+                vehicle_length = 6,
+                buy_menu_width = 32,
+                loading_speed = 20,
+                intro_date = 1900,
+                str_type_info = 'COASTER',
+                vehicle_life = 40,
+                graphics_status = '')
+
+vehicle.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
+
+vehicle = TankCar(numeric_id = 14030,
+                vehicle_set = 'nagn',
                 vehicle_generation = 'gen_1',
                 title = 'Tank [Car]',
                 capacity_freight = 20,
