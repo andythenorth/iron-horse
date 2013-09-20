dcd99cd659f60a4f7cb07bd8a5205ba680952d0c
andythenorth <mail@andythenorth.co.uk>
2013-09-20 21:15:47 +0100

    Change: new graphics for brit gen 1 box car, including 2cc variant (closes #6431)

--- src/vehicles/box_cars.py
+++ src/vehicles/box_cars.py
@@ -22,6 +22,10 @@ vehicle.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
 
+vehicle.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=1)
+
 vehicle = BoxCar(title = 'Box [Car]',
                 vehicle_set = 'brit',
                 wagon_generation = 2,
