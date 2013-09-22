bb11cb266cddf80dee949b821ad645fb2b4e627e
andythenorth <mail@andythenorth.co.uk>
2013-09-22 15:13:34 +0100

    Change: 3 generations of tank car

--- src/vehicles/tank_cars.py
+++ src/vehicles/tank_cars.py
@@ -43,7 +43,26 @@ vehicle.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
 
-# 3rd gen bogie tank wagon = 75t
+vehicle = TankCar(title = 'Tank [Car]',
+                vehicle_set = 'brit',
+                wagon_generation = 3,
+                capacity_freight = 75, 
+                replacement_id = '-none',
+                buy_cost = 22,
+                fixed_run_cost_factor = 3.5,
+                fuel_run_cost_factor = 1.0,
+                weight = 100,
+                vehicle_length = 8,
+                buy_menu_width = 32,
+                loading_speed = 20,
+                intro_date = 1900,
+                str_type_info = 'COASTER',
+                vehicle_life = 40,
+                graphics_status = '')
+
+vehicle.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
 
 """
 vehicle = TankCar(title = 'Tank [Car]',
