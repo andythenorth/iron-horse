0d5890d56a4e751d0016573eb56c286bf6c77120
andythenorth <mail@andythenorth.co.uk>
2013-09-22 13:09:22 +0100

    Change: gen 1 covered hopper car for brit set, also adjust covered hopper car capacities

--- src/vehicles/covered_hopper_cars.py
+++ src/vehicles/covered_hopper_cars.py
@@ -3,8 +3,29 @@ from train import Train, CoveredHopperCar
 
 vehicle = CoveredHopperCar(title = 'Covered Hopper [Car]',
                         vehicle_set = 'brit',
+                        wagon_generation = 1,
+                        capacity_freight = 30,
+                        replacement_id = '-none',
+                        buy_cost = 22,
+                        fixed_run_cost_factor = 3.5,
+                        fuel_run_cost_factor = 1.0,
+                        weight = 100,
+                        vehicle_length = 8,
+                        buy_menu_width = 32,
+                        loading_speed = 20,
+                        intro_date = 1900,
+                        str_type_info = 'COASTER',
+                        vehicle_life = 40,
+                        graphics_status = '')
+
+vehicle.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
+
+vehicle = CoveredHopperCar(title = 'Covered Hopper [Car]',
+                        vehicle_set = 'brit',
                         wagon_generation = 2,
-                        capacity_freight = 20,
+                        capacity_freight = 55,
                         replacement_id = '-none',
                         buy_cost = 22,
                         fixed_run_cost_factor = 3.5,
