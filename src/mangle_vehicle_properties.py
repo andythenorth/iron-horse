60514afa82acc3e3c87bf3bee28511ddc8de9e48
andythenorth <mail@andythenorth.co.uk>
2013-10-04 20:33:54 +0100

    Codechange: add a mangle method for deleting props

--- src/mangle_vehicle_properties.py
+++ src/mangle_vehicle_properties.py
@@ -4,11 +4,25 @@ currentdir = os.curdir
 import sys
 sys.path.append(os.path.join('src')) # add to the module search path
 
+property_to_delete = 'buy_menu_width'
 property_to_move = 'sea_capable'
 property_to_insert_after = 'gross_tonnage'
 line_to_insert = "            graphics_status = 'Unstarted',"
 
-filenames = ['altamira_freighter.py',]
+filenames = ['screamer.py','pendolino.py','slammer.py','box_cars.py','open_cars.py','caboose_cars.py','raven.py','covered_hopper_cars.py','metro.py','gresley.py','hellenic.py','wordsell.py','goods.py','tank_cars.py','reefer_cars.py','livestock_cars.py','hopper_cars.py','flat_cars.py','passenger_cars.py','mail_cars.py','standard.py','suburban.py','collier.py','whistler.py','zebedee.py','chopper.py','gridiron.py','geep.py','dmc_sd40.py','growler.py']
+
+
+def delete_property(filename):
+    file = open(os.path.join('src','vehicles',filename),'r')
+    content = file.readlines()
+
+    for line in content:
+        if property_to_delete in line:
+            content.remove(line)
+
+    file = open(os.path.join('src','vehicles',filename),'w')
+    file.write(''.join(content))
+    file.close
 
 
 def move_property(filename):
@@ -47,5 +61,6 @@ def insert_property(filename):
 
 
 for filename in filenames:
+    delete_property(filename)
     #move_property(filename)
-    insert_property(filename)
+    #insert_property(filename)
