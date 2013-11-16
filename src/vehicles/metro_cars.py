efa13b6f9504c9a4d48f996a630cd29b528964b4
andythenorth <mail@andythenorth.co.uk>
2013-11-16 21:03:48 +0000

    Change: set props for Metro cars

--- src/vehicles/metro_cars.py
+++ src/vehicles/metro_cars.py
@@ -5,12 +5,12 @@ from train import TypeConfig, WagonConsist, Wagon
 
 type_config = TypeConfig(base_id = 'metro_car',
                 template = 'train.pynml',
-                class_refit_groups = ['pax'],
+                class_refit_groups = ['pax', 'express_freight'],
                 label_refits_allowed = [],
                 label_refits_disallowed = [],
                 autorefit = True,
-                default_cargo = 'PASS',
-                default_capacity_type = 'capacity_pax',
+                default_cargo = 'MAIL',
+                default_capacity_type = 'capacity_mail',
                 str_type_info = 'DOGTRACK')
 
 consist = WagonConsist(type_config = type_config,
@@ -18,7 +18,7 @@ consist = WagonConsist(type_config = type_config,
                     vehicle_set = 'brit',
                     wagon_generation = 1,
                     replacement_id = '-none',
-                    intro_date = 1860,
+                    intro_date = 1900,
                     buy_cost = 22,
                     fixed_run_cost_factor = 3.5,
                     fuel_run_cost_factor = 1.0,
@@ -28,10 +28,11 @@ consist = WagonConsist(type_config = type_config,
 
 consist.add_unit(Wagon(type_config = type_config,
                         consist = consist,
-                        capacity_pax = 40,
-                        weight = 100,
-                        vehicle_length = 9,
-                        loading_speed = 20))              
+                        capacity_mail = 40,
+                        capacity_freight = 15,
+                        weight = 20,
+                        vehicle_length = 8,
+                        loading_speed = 10))              
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
