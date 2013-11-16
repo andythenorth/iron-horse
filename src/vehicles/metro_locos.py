8ca5d3240d0c4ce251d4e7ee0d06635e916a17fa
andythenorth <mail@andythenorth.co.uk>
2013-11-16 19:55:07 +0000

    Change: set some props for Metro vehicles

--- src/vehicles/metro_locos.py
+++ src/vehicles/metro_locos.py
@@ -3,15 +3,15 @@ from train import EngineConsist, MetroLoco
 
 consist = EngineConsist(id = 'metro_loco_brit_gen_1',
               base_numeric_id = 1190,
-              title = 'Metro Loco [Electric]',
+              title = 'Metro Loco [Battery]',
               str_type_info = 'COASTER',
               replacement_id = '-none',
-              power = 450,
-              speed = 75,
+              power = 800,
+              speed = 45,
               buy_cost = 22,
               fixed_run_cost_factor = 3.5,
               fuel_run_cost_factor = 1.0,
-              intro_date = 1960,
+              intro_date = 1900,
               vehicle_life = 40,
               graphics_status = '',
               use_legacy_spritesheet = True)
@@ -19,9 +19,8 @@ consist = EngineConsist(id = 'metro_loco_brit_gen_1',
 consist.add_unit(MetroLoco(consist = consist,
                         weight = 65,
                         vehicle_length = 8,
-                        capacity_freight = 30,
                         spriterow_num = 0))              
 
 consist.add_model_variant(intro_date=0,
-                       end_date=1986,
+                       end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
--- src/vehicles/metro_multiple_units.py
+++ src/vehicles/metro_multiple_units.py
@@ -6,25 +6,25 @@ consist = EngineConsist(id = 'metro_mu_brit_gen_1',
               title = '1900 Metro Train [Electric]',
               str_type_info = 'COASTER',
               replacement_id = '-none',
-              power = 450,
-              speed = 75,
+              power = 600,
+              speed = 40,
               buy_cost = 22,
               fixed_run_cost_factor = 3.5,
               fuel_run_cost_factor = 1.0,
-              intro_date = 1960,
+              intro_date = 1900,
               vehicle_life = 40,
               graphics_status = '',
               use_legacy_spritesheet = True)
 
 consist.add_unit(MetroMultipleUnit(consist = consist,
-                        weight = 65,
+                        weight = 40,
                         vehicle_length = 8,
-                        capacity_freight = 30,
+                        capacity_pax = 100,
                         spriterow_num = 0),
                         repeat=2)              
 
 consist.add_model_variant(intro_date=0,
-                       end_date=1986,
+                       end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
 
 
@@ -33,25 +33,25 @@ consist = EngineConsist(id = 'metro_mu_brit_gen_2',
               title = '1950 Metro Train [Electric]',
               str_type_info = 'COASTER',
               replacement_id = '-none',
-              power = 450,
-              speed = 75,
+              power = 900,
+              speed = 55,
               buy_cost = 22,
               fixed_run_cost_factor = 3.5,
               fuel_run_cost_factor = 1.0,
-              intro_date = 1960,
+              intro_date = 1950,
               vehicle_life = 40,
               graphics_status = '',
               use_legacy_spritesheet = True)
 
 consist.add_unit(MetroMultipleUnit(consist = consist,
-                        weight = 65,
+                        weight = 40,
                         vehicle_length = 8,
-                        capacity_freight = 30,
+                        capacity_pax = 100,
                         spriterow_num = 0),
                         repeat=2)              
 
 consist.add_model_variant(intro_date=0,
-                       end_date=1986,
+                       end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
 
 
@@ -60,23 +60,23 @@ consist = EngineConsist(id = 'metro_mu_brit_gen_3',
               title = '2000 Metro Train [Electric]',
               str_type_info = 'COASTER',
               replacement_id = '-none',
-              power = 450,
-              speed = 75,
+              power = 1100,
+              speed = 65,
               buy_cost = 22,
               fixed_run_cost_factor = 3.5,
               fuel_run_cost_factor = 1.0,
-              intro_date = 1960,
+              intro_date = 2000,
               vehicle_life = 40,
               graphics_status = '',
               use_legacy_spritesheet = True)
 
 consist.add_unit(MetroMultipleUnit(consist = consist,
-                        weight = 65,
+                        weight = 30,
                         vehicle_length = 8,
-                        capacity_freight = 30,
+                        capacity_pax = 100,
                         spriterow_num = 0),
                         repeat=2)              
 
 consist.add_model_variant(intro_date=0,
-                       end_date=1986,
+                       end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
