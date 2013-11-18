d71c818d1fe709ed599ebb2943ce21fa2cdd5d86
andythenorth <mail@andythenorth.co.uk>
2013-11-18 19:08:12 +0000

    Feature: add Little Bear trip worker loco

---
+++ src/vehicles/little_bear.py
@@ -0,0 +1,26 @@
+import global_constants
+from train import EngineConsist, DieselLoco
+
+consist = EngineConsist(id = 'little_bear',
+              base_numeric_id = 1160,
+              title = 'Little Bear [Diesel]',
+              str_type_info = 'COASTER',
+              replacement_id = '-none',
+              power = 750,
+              speed = 60,
+              buy_cost = 22,
+              fixed_run_cost_factor = 3.5,
+              fuel_run_cost_factor = 1.0,
+              vehicle_life = 40,
+              intro_date = 1964,
+              graphics_status = '',
+              use_legacy_spritesheet = True)
+              
+consist.add_unit(DieselLoco(consist = consist,
+                        weight = 54,
+                        vehicle_length = 7,
+                        spriterow_num = 0))              
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
