265320129bbdd48151efa32ff7f1ada576a10f5e
andythenorth <mail@andythenorth.co.uk>
2013-11-22 21:24:31 +0000

    Fix: Metro tank had spurious tender, removed

--- src/vehicles/metro.py
+++ src/vehicles/metro.py
@@ -22,11 +22,6 @@ consist.add_unit(SteamLoco(consist = consist,
                         vehicle_length = 6,
                         spriterow_num = 0))              
 
-consist.add_unit(SteamLocoTender(consist = consist,
-                        weight = 30,
-                        vehicle_length = 4,
-                        spriterow_num = 1))              
-
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
