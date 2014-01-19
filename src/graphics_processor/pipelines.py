42044072dc5927141134c66c7f2be3dfa805ab47
andythenorth <mail@andythenorth.co.uk>
2014-01-19 12:58:44 +0000

    Codechange: proof of concept for graphics processing units in pipeline

--- src/graphics_processor/pipelines.py
+++ src/graphics_processor/pipelines.py
@@ -7,6 +7,7 @@ from pixa import make_cheatsheet as make_cheatsheet
 from PIL import Image
 
 from graphics_processor import registered_pipelines
+from graphics_processor.units import PassThrough
 
 DOS_PALETTE = Image.open('palette_key.png').palette
 
@@ -20,12 +21,19 @@ class Pipeline(object):
         print "I am a pipeline"
         register_pipeline(self)
 
-    def render_common(self, variant, consist, options):
-        input_path = os.path.join(currentdir, 'src', 'graphics', options['template'])
+    def render_common(self, variant, consist, input_image, units, options):
+        # expects to be passed a PIL Image object
+        # options is a dict and can be used abitrarily to pass options wherever needed in the pipeline
+        # units is a list of objects, usually with their config data already baked in (don't usually need to go looking in options)
+        # each unit is then called in order, passing in and returning a pixa SpriteSheet
+        # finally the spritesheet is saved
         output_path = os.path.join(currentdir, 'generated', 'graphics', variant.get_spritesheet_name(consist))
-        input_image = Image.open(input_path)
         spritesheet = Spritesheet(width=input_image.size[0], height=input_image.size[1] , palette=DOS_PALETTE)
         spritesheet.sprites.paste(input_image)
+        
+        for unit in units:
+            spritesheet = unit.render(spritesheet)
+        
         spritesheet.save(output_path)
 
 class TestPipeline(Pipeline):
@@ -36,8 +44,10 @@ class TestPipeline(Pipeline):
     def render(self, variant, consist):
         print 'render'
         options = variant.graphics_processor.options
-        print options
-        result = self.render_common(variant, consist, options)
+        input_path = os.path.join(currentdir, 'src', 'graphics', options['template'])
+        input_image = Image.open(input_path)
+        units = [PassThrough()]
+        result = self.render_common(variant, consist, input_image, units, options)
         return result
         
 TestPipeline()
---
+++ src/graphics_processor/units.py
@@ -0,0 +1,7 @@
+class PassThrough(object):
+    def __init__(self):
+        pass
+        
+    def render(self, spritesheet):
+        print 'PassThrough'
+        return spritesheet
