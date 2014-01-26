17b5e1315d2da87d4447204401dcfe3d003792fe
andythenorth <mail@andythenorth.co.uk>
2014-01-26 10:26:18 +0000

    Change: adjust a bunch of wagon properties

--- src/vehicles/covered_hopper_cars.py
+++ src/vehicles/covered_hopper_cars.py
@@ -16,7 +16,7 @@ consist = WagonConsist(type_config = type_config,
                     vehicle_set = 'brit',
                     wagon_generation = 1,
                     replacement_id = '-none',
-                    intro_date = 1920,
+                    intro_date = 1922,
                     buy_cost = 22,
                     fixed_run_cost_factor = 3.5,
                     fuel_run_cost_factor = 1.0,
@@ -27,7 +27,7 @@ consist = WagonConsist(type_config = type_config,
 consist.add_unit(Wagon(type_config = type_config,
                         consist = consist,
                         capacity_freight = 30,
-                        weight = 100,
+                        weight = 14,
                         vehicle_length = 5,
                         loading_speed = 20))              
 
@@ -35,11 +35,12 @@ consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
 
+# no gen 2 for covered hopper - straight to gen 3
 
 consist = WagonConsist(type_config = type_config,
                     title = 'Covered Hopper [Car]',
                     vehicle_set = 'brit',
-                    wagon_generation = 2,
+                    wagon_generation = 3,
                     replacement_id = '-none',
                     intro_date = 1960,
                     buy_cost = 22,
@@ -52,7 +53,7 @@ consist = WagonConsist(type_config = type_config,
 consist.add_unit(Wagon(type_config = type_config,
                         consist = consist,
                         capacity_freight = 55,
-                        weight = 100,
+                        weight = 30,
                         vehicle_length = 8,
                         loading_speed = 20))    
                         
