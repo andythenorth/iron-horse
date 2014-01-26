d55bda8d5f3b5d49263c1c7ceadd4a1582819234
andythenorth <mail@andythenorth.co.uk>
2014-01-26 09:40:00 +0000

    Codechange: tidy up some compile script stuff

--- src/build_iron_horse.py
+++ src/build_iron_horse.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from time import time
-start = time()
 
 print "[BUILD] build_iron_horse.py"
 
@@ -15,19 +14,25 @@ import sys
 sys.path.append(os.path.join('src')) # add to the module search path
 
 # render the nml file
+start = time()
 import render_nml
 render_nml.main()
+print format((time() - start), '.2f')+'s'
 
 # render the graphics
+start = time()
 import render_graphics
 render_graphics.main()
+print format((time() - start), '.2f')+'s'
 
 # render the lang files
+start = time()
 import render_lang
 render_lang.main()
+print format((time() - start), '.2f')+'s'
 
 # render the docs
+start = time()
 import render_docs
 render_docs.main()
-
-print (time() - start)
+print format((time() - start), '.2f')+'s'
