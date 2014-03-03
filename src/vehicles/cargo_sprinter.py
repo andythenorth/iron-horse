e7c0cbf5f393fc60e3c6a1137eb83addb4dda804
andythenorth <mail@andythenorth.co.uk>
2014-03-03 22:09:21 +0000

    Change: balance speed of Cargo Sprinter

--- src/vehicles/cargo_sprinter.py
+++ src/vehicles/cargo_sprinter.py
@@ -2,21 +2,21 @@ import global_constants
 import graphics_processor.utils as graphics_utils
 from train import EngineConsist, CargoSprinter, GraphicsProcessorFactory
 
-recolour_maps = graphics_utils.get_container_recolour_maps() 
+recolour_maps = graphics_utils.get_container_recolour_maps()
 graphics_options = {'template': 'cargo_sprinter_template.png',
            'recolour_maps': (recolour_maps),
            'copy_block_top_offset': 0,
            'num_rows_per_unit': 3,
            'num_unit_types': 3}
 graphics_processor_1 = GraphicsProcessorFactory('extend_spriterows_for_recoloured_cargos_pipeline', graphics_options)
-                
+
 consist = EngineConsist(id = 'cargo_sprinter',
               base_numeric_id = 1100,
               title = 'Cargo Sprinter [Diesel]',
               str_type_info = 'COASTER',
               replacement_id = '-none',
               power = 1520,
-              speed = 75,
+              speed = 100,
               buy_cost = 69,
               fixed_run_cost_factor = 3.5,
               fuel_run_cost_factor = 1.0,
@@ -30,7 +30,7 @@ consist.add_unit(CargoSprinter(consist = consist,
                         vehicle_length = 8,
                         capacity_freight = 30,
                         num_random_cargo_variants = len(graphics_options['recolour_maps'])),
-                        repeat=2)              
+                        repeat=2)
 
 consist.add_model_variant(intro_date=0,
                        end_date=1986,
