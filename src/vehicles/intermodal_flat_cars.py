83b0bbd9d794c475b6246bb9ab470ee54676362c
andythenorth <mail@andythenorth.co.uk>
2014-03-08 20:48:43 +0000

    Change: make container refits consistent with Squid

--- src/vehicles/intermodal_flat_cars.py
+++ src/vehicles/intermodal_flat_cars.py
@@ -4,10 +4,10 @@ from train import TypeConfig, WagonConsist, Wagon, GraphicsProcessorFactory
 
 cargo_graphics_mappings = {}
 
-recolour_maps = graphics_utils.get_container_recolour_maps() 
+recolour_maps = graphics_utils.get_container_recolour_maps()
 graphics_options = {'template': 'intermodal_flat_car_brit_gen_3_template.png',
            'recolour_maps': (recolour_maps),
-           'copy_block_top_offset': 30, 
+           'copy_block_top_offset': 30,
            'num_rows_per_unit': 3,
            'num_unit_types': 1}
 graphics_processor_1 = GraphicsProcessorFactory('extend_spriterows_for_recoloured_cargos_pipeline', graphics_options)
@@ -17,10 +17,11 @@ type_config = TypeConfig(base_id = 'intermodal_flat_car',
                 fixed_run_cost_factor = 3.5,
                 num_cargo_rows = 3,
                 generic_cargo_rows = [0, 1, 2],
-                class_refit_groups = ['express_freight'],
+                class_refit_groups = ['express_freight', 'packaged_freight'],
                 cargo_graphics_mappings = cargo_graphics_mappings,
-                label_refits_allowed = cargo_graphics_mappings.keys(),
-                label_refits_disallowed = [],
+                #label_refits_allowed = cargo_graphics_mappings.keys(),
+                label_refits_allowed = ['FRUT','WATR'],
+                label_refits_disallowed = ['FISH','LVST','OIL_','TOUR','WOOD'],
                 autorefit = True,
                 default_cargo = 'GOOD',
                 default_capacity_type = 'capacity_freight',
@@ -42,7 +43,7 @@ consist.add_unit(Wagon(type_config = type_config,
                         capacity_freight = 60,
                         weight = 20,
                         vehicle_length = 8,
-                        loading_speed = 20))          
+                        loading_speed = 20))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
