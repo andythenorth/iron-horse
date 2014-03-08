645c2aa8020473306320ce0429f8b35c71e13264
andythenorth <mail@andythenorth.co.uk>
2014-03-08 20:49:35 +0000

    Codechange: add a comment

--- src/vehicles/intermodal_flat_cars.py
+++ src/vehicles/intermodal_flat_cars.py
@@ -20,6 +20,7 @@ type_config = TypeConfig(base_id = 'intermodal_flat_car',
                 class_refit_groups = ['express_freight', 'packaged_freight'],
                 cargo_graphics_mappings = cargo_graphics_mappings,
                 #label_refits_allowed = cargo_graphics_mappings.keys(),
+                # maintain other sets (e.g. Squid etc) when changing container refits
                 label_refits_allowed = ['FRUT','WATR'],
                 label_refits_disallowed = ['FISH','LVST','OIL_','TOUR','WOOD'],
                 autorefit = True,
