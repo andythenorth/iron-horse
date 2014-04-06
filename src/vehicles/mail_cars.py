377c7ca5717655c32454a44155e45a8793edebe2
andythenorth <mail@andythenorth.co.uk>
2014-04-06 13:46:18 +0100

    Fix: mail cars no longer travel with open doors #6641

--- src/vehicles/mail_cars.py
+++ src/vehicles/mail_cars.py
@@ -6,7 +6,7 @@ from train import TypeConfig, WagonConsist, MailCar
 cargo_graphics_mappings = {} # template needs this, but mail car has zero cargo-specific graphics, all generic
 
 type_config = TypeConfig(base_id = 'mail_car',
-                template = 'car_with_visible_cargo.pynml',
+                template = 'car_with_open_doors_during_loading.pynml',
                 fixed_run_cost_factor = 3.5,
                 num_cargo_rows = 1, # template needs this, but mail car has zero cargo-specific graphics, all generic
                 class_refit_groups = ['mail', 'express_freight'],
