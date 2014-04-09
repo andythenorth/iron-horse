cc7c6d02096a3987710fa94b058c39af42d75cef
andythenorth <mail@andythenorth.co.uk>
2014-04-09 14:42:09 +0100

    Fix: Lemon is a 4-8-0 #6853

--- src/vehicles/lemon.py
+++ src/vehicles/lemon.py
@@ -3,7 +3,7 @@ from train import EngineConsist, SteamLoco, SteamLocoTender
 
 consist = EngineConsist(id = 'lemon',
               base_numeric_id = 1170,
-              title = '2-10-0 Lemon [Steam]',
+              title = '4-8-0 Lemon [Steam]',
               replacement_id = '-none',
               power = 2100,
               tractive_effort_coefficient = 0.29,
