a2cdc2c340ad904074b7142017a639969e4e2ea6
andythenorth <mail@andythenorth.co.uk>
2014-04-17 20:13:56 +0100

    Fix: length of NG box car

--- src/vehicles/box_cars.py
+++ src/vehicles/box_cars.py
@@ -117,7 +117,7 @@ consist.add_unit(Wagon(type_config = type_config,
                         capacity_freight = 12,
                         capacity_mail = 12,
                         weight = 6,
-                        vehicle_length = 5,
+                        vehicle_length = 4,
                         loading_speed = 20))
 
 consist.add_model_variant(intro_date=0,
