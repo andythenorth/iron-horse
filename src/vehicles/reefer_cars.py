b1a94d967596f32386e13552586ee7b797c267d1
andythenorth <mail@andythenorth.co.uk>
2014-09-07 12:37:11 +0100

    Feature: random colours for gen 1 reefer

--- src/vehicles/reefer_cars.py
+++ src/vehicles/reefer_cars.py
@@ -1,10 +1,9 @@
 import global_constants
 from train import TypeConfig, WagonConsist, Wagon, GraphicsProcessorFactory
 
-options = {'template': 'reefer_car_brit_gen_2_template.png'}
+options = {'template': 'reefer_car_brit_gen_1_template.png'}
 graphics_processor_1 = GraphicsProcessorFactory('pass_through_pipeline', options)
 graphics_processor_2 = GraphicsProcessorFactory('swap_company_colours_pipeline', options)
-
 
 type_config = TypeConfig(base_id = 'reefer_car',
                     template = 'train.pynml',
@@ -35,9 +34,19 @@ consist.add_unit(Wagon(type_config = type_config,
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
-                       spritesheet_suffix=0)
+                       spritesheet_suffix=0,
+                       graphics_processor=graphics_processor_1)
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=1,
+                       graphics_processor=graphics_processor_2)
+
 
 # no gen 2 reefer - straight to gen 3
+options = {'template': 'reefer_car_brit_gen_3_template.png'}
+graphics_processor_1 = GraphicsProcessorFactory('pass_through_pipeline', options)
+graphics_processor_2 = GraphicsProcessorFactory('swap_company_colours_pipeline', options)
 
 consist = WagonConsist(type_config = type_config,
                     title = 'Reefer [Car]',
