c4a17f70834f74c220c5e4d09ad9939e6342b459
andythenorth <mail@andythenorth.co.uk>
2014-09-10 22:58:12 +0100

    Fix: 2-8-2 Northock not 4-8-2 #7111

--- src/vehicles/northcock.py
+++ src/vehicles/northcock.py
@@ -3,7 +3,7 @@ from train import EngineConsist, SteamLoco, SteamLocoTender
 
 consist = EngineConsist(id = 'northcock',
               base_numeric_id = 1070,
-              title = '4-8-2 Northcock [Steam]',
+              title = '2-8-2 Northcock [Steam]',
               replacement_id = '-none',
               power = 1750,
               tractive_effort_coefficient = 0.2,
@@ -13,16 +13,16 @@ consist = EngineConsist(id = 'northcock',
               intro_date = 1935,
               graphics_status = '',
               use_legacy_spritesheet = True)
-              
+
 consist.add_unit(SteamLoco(consist = consist,
                         weight = 120,
                         vehicle_length = 8,
-                        spriterow_num = 0))              
+                        spriterow_num = 0))
 
 consist.add_unit(SteamLocoTender(consist = consist,
                         weight = 50,
                         vehicle_length = 5,
-                        spriterow_num = 1))              
+                        spriterow_num = 1))
 
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
