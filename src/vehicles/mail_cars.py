46a200ee885baf30d719e6a7a3fe63b3f85ca321
andythenorth <mail@andythenorth.co.uk>
2014-09-14 16:03:25 +0100

    Codechange: remove commented code

--- src/vehicles/mail_cars.py
+++ src/vehicles/mail_cars.py
@@ -1,8 +1,6 @@
 import global_constants
 from train import TypeConfig, WagonConsist, MailCar
 
-#self.capacities_freight = [int(0.5 * capacity) for capacity in self.capacities_mail]
-
 cargo_graphics_mappings = {} # template needs this, but mail car has zero cargo-specific graphics, all generic
 
 type_config = TypeConfig(base_id = 'mail_car',
