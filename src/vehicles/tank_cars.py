e85ec6c87ecac820984931da7586ee729ea148f0
andythenorth <mail@andythenorth.co.uk>
2014-11-23 08:57:57 +0000

    Change: introduce large tank in 1960

--- src/vehicles/tank_cars.py
+++ src/vehicles/tank_cars.py
@@ -54,7 +54,7 @@ consist = WagonConsist(type_config = type_config,
                     vehicle_set = 'brit',
                     wagon_generation = 2,
                     replacement_id = '-none',
-                    intro_date = 1964,
+                    intro_date = 1960,
                     vehicle_life = 40,
                     graphics_status = '',
                     use_legacy_spritesheet = True)
