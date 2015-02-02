1ee513583f3e52a3196838195de3873fadf1565a
andythenorth <mail@andythenorth.co.uk>
2015-02-02 21:53:08 +0000

    Change: add a bunch of SOAM test vehicles

--- src/rosters/soam.py
+++ src/rosters/soam.py
@@ -1,6 +1,22 @@
 from roster import Roster
 
 from vehicles import burro
+from vehicles import thing_1
+from vehicles import thing_2
+from vehicles import thing_3
+from vehicles import thing_4
+from vehicles import thing_5
+from vehicles import thing_6
+from vehicles import thing_7
+from vehicles import thing_8
 
 roster = Roster(id = 'soam',
-                buy_menu_sort_order = ['burro'])
+                buy_menu_sort_order = ['burro',
+                                       'thing_1',
+                                       'thing_2',
+                                       'thing_3',
+                                       'thing_4',
+                                       'thing_5',
+                                       'thing_6',
+                                       'thing_7',
+                                       'thing_8'])
---
+++ src/vehicles/thing_1.py
@@ -0,0 +1,25 @@
+import global_constants
+from train import EngineConsist, DieselLoco
+
+consist = EngineConsist(id = 'thing_1',
+              base_numeric_id = 2010,
+              title = 'Thing 1 [Steam]',
+              replacement_id = '-none',
+              power = 900,
+              track_type = 'NG',
+              speed = 55,
+              type_base_buy_cost_points = -10, # dibble buy cost for game balance
+              type_base_running_cost_points = -15, # dibble running costs for game balance
+              vehicle_life = 40,
+              intro_date = 1955,
+              graphics_status = '',
+              use_legacy_spritesheet = True)
+
+consist.add_unit(DieselLoco(consist = consist,
+                        weight = 40,
+                        vehicle_length = 6,
+                        spriterow_num = 0))
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
