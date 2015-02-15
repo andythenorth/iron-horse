9b6e566994f26f312629fbaca161fee1643c60cb
andythenorth <mail@andythenorth.co.uk>
2015-02-15 19:20:31 +0000

    Codechange: refactor handling of engines in rosters

--- src/roster.py
+++ src/roster.py
@@ -6,7 +6,11 @@ class Roster(object):
     """
     def __init__(self, **kwargs):
         self.id = kwargs.get('id')
-        self.buy_menu_sort_order = kwargs.get('buy_menu_sort_order')
+        self.engines = kwargs.get('engines')
+
+    @property
+    def buy_menu_sort_order(self):
+        return [engine.consist.id for engine in self.engines]
 
     def register(self):
         registered_rosters.append(self)
