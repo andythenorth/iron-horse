f968e7ef50f38be7f08e3557561626b09e506f9e
andythenorth <mail@andythenorth.co.uk>
2015-02-16 21:12:48 +0000

    Feature: SOAM gen 1 flat car

--- src/vehicles/flat_cars.py
+++ src/vehicles/flat_cars.py
@@ -132,3 +132,29 @@ def main():
                            spritesheet_suffix=0)
 
     #--------------- soam ----------------------------------------------------------------------
+    consist = WagonConsist(type_config = type_config_normal,
+                        title = '[Flat Car]',
+                        roster = 'soam',
+                        wagon_generation = 1,
+                        replacement_id = '-none',
+                        intro_date = 1860,
+                        vehicle_life = 40)
+
+    consist.add_unit(Wagon(consist = consist,
+                            capacity_freight = 20,
+                            weight = 6,
+                            vehicle_length = 5))
+
+    options = {'template': 'flat_car_soam_gen_1_template.png'}
+
+    consist.add_model_variant(intro_date=0,
+                           end_date=global_constants.max_game_date,
+                           spritesheet_suffix=0,
+                           graphics_processor=GraphicsProcessorFactory('pass_through_pipeline', options))
+
+    consist.add_model_variant(intro_date=0,
+                           end_date=global_constants.max_game_date,
+                           spritesheet_suffix=1,
+                           graphics_processor=GraphicsProcessorFactory('swap_company_colours_pipeline', options))
+
+
