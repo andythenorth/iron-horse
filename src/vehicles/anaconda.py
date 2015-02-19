043ff170b52c1e9df9ff08a0d6f6110ce1ef78a6
andythenorth <mail@andythenorth.co.uk>
2015-02-19 19:43:45 +0000

    Change: new sprites for Anaaconda Railcar, also mail capacity not 0

--- src/vehicles/anaconda.py
+++ src/vehicles/anaconda.py
@@ -9,14 +9,13 @@ consist = EngineConsist(id = 'anaconda',
               speed = 75,
               type_base_running_cost_points = -32, # dibble running costs for game balance
               intro_date = 1960,
-              vehicle_life = 40,
-              use_legacy_spritesheet = True)
+              vehicle_life = 40)
 
 consist.add_unit(DieselRailcar(consist = consist,
                         weight = 65,
-                        vehicle_length = 8,
+                        vehicle_length = 9,
                         capacity_pax = 55,
-                        capacity_mail = 0,
+                        capacity_mail = 30,
                         spriterow_num = 0))
 
 consist.add_model_variant(intro_date=0,
