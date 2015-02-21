28db0bec142a0a9ee17ab2b97b40be8f8711bdf6
andythenorth <mail@andythenorth.co.uk>
2015-02-21 09:50:53 +0000

    Feature: new sprites for gen 2 hot metal car (DanMacK)

--- src/vehicles/metal_cars.py
+++ src/vehicles/metal_cars.py
@@ -1,8 +1,13 @@
 import global_constants
 from train import TypeConfig, WagonConsist, Wagon
 
+cargo_graphics_mappings = {}
+
 type_config = TypeConfig(base_id = 'metal_car',
-                template = 'train.pynml',
+                template = 'car_with_visible_cargo.pynml',
+                num_cargo_rows = 1,
+                generic_cargo_rows = [0],
+                cargo_graphics_mappings = cargo_graphics_mappings,
                 class_refit_groups = [],
                 label_refits_allowed = ['STEL'],
                 label_refits_disallowed = [],
@@ -11,34 +16,25 @@ type_config = TypeConfig(base_id = 'metal_car',
                 default_capacity_type = 'capacity_freight',
                 loading_speed_multiplier = 2)
 
+
 def main():
     #--------------- brit ----------------------------------------------------------------------
+
+    # no gen 1 yet
+
     consist = WagonConsist(type_config = type_config,
                         title = '[Metal Car]',
                         roster = 'brit',
-                        wagon_generation = 1,
+                        wagon_generation = 2,
                         replacement_id = '-none',
                         intro_date = 1910,
                         vehicle_life = 40,
-                        speed = 45,
-                        use_legacy_spritesheet = True)
-
-    consist.add_unit(Wagon(consist = consist,
-                            capacity_freight = 0,
-                            weight = 10,
-                            vehicle_length = 3,
-                            spriterow_num = 0))
+                        speed = 45)
 
     consist.add_unit(Wagon(consist = consist,
                             capacity_freight = 60,
-                            weight = 30,
-                            vehicle_length = 5,
-                            spriterow_num = 1))
-
-    consist.add_unit(Wagon(consist = consist,
-                            capacity_freight = 0,
-                            weight = 10,
-                            vehicle_length = 3,
+                            weight = 60,
+                            vehicle_length = 9,
                             spriterow_num = 0))
 
     consist.add_model_variant(intro_date=0,
