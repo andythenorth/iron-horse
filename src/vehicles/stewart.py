bf5ba202966ffe2e6ddc735d852d733816530e8c
andythenorth <mail@andythenorth.co.uk>
2015-03-15 10:18:20 +0000

    Change: random reversed sprites for Stewart (brit ng roster)

--- src/vehicles/stewart.py
+++ src/vehicles/stewart.py
@@ -23,3 +23,7 @@ consist.add_unit(SteamLoco(consist = consist,
 consist.add_model_variant(intro_date=0,
                        end_date=global_constants.max_game_date,
                        spritesheet_suffix=0)
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=1)
