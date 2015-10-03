14a4d46af1495e57e5ec27d8278454afc9ef97a9
andythenorth <mail@andythenorth.co.uk>
2015-10-03 21:31:11 +0100

    Change: stats for Electrico

--- src/vehicles/electrico.py
+++ src/vehicles/electrico.py
@@ -5,15 +5,15 @@ consist = EngineConsist(id = 'electrico',
               base_numeric_id = 180,
               title = 'Electrico 2-B+B-2 [Electric]',
               replacement_id = '-none',
-              power = 900,
-              speed = 55,
+              power = 2400,
+              speed = 70,
               type_base_buy_cost_points = -10, # dibble buy cost for game balance
               type_base_running_cost_points = -15, # dibble running costs for game balance
               vehicle_life = 40,
               intro_date = 1920)
 
 consist.add_unit(ElectricLoco(consist = consist,
-                        weight = 40,
+                        weight = 140,
                         vehicle_length = 10,
                         spriterow_num = 0))
 
