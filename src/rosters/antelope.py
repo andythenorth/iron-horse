cf7c89526c1d5567b813316cf21ad0047a7e065c
andythenorth <mail@andythenorth.co.uk>
2016-02-21 13:36:49 +0000

    Feature: Antlion railcar for Antelope roster

--- src/rosters/antelope.py
+++ src/rosters/antelope.py
@@ -1,14 +1,16 @@
 from roster import Roster
 
+from vehicles import antlion
 from vehicles import savannah_slammer
 
 # speed for wagons in mph (some generations may optionally have no speed set)
 # format is [standard, speedy]
-speeds = dict(gen_1_wagon_speeds = [60, 75],
+speeds = dict(gen_1_wagon_speeds = [55, 75],
               gen_2_wagon_speeds = [75, 75],
               ng_gen_1_wagon_speeds = [55, 55])
 
 roster = Roster(id = 'antelope',
                 numeric_id = 3,
                 speeds = speeds,
-                engines = [savannah_slammer])
+                engines = [antlion,
+                           savannah_slammer])
--- src/vehicles/antlion.py
+++ src/vehicles/antlion.py
@@ -1,21 +1,21 @@
 import global_constants
 from train import EngineConsist, DieselRailcar
 
-consist = EngineConsist(id = 'savannah_slammer',
-              base_numeric_id = 1540,
-              title = 'Savannah Slammer [Diesel]',
+consist = EngineConsist(id = 'antlion',
+              base_numeric_id = 1550,
+              title = 'Antlion [Diesel]',
               replacement_id = '-none',
-              power = 450,
-              speed = 75,
+              power = 350,
+              speed = 55,
               type_base_running_cost_points = -32, # dibble running costs for game balance
-              intro_date = 1990,
+              intro_date = 1950,
               vehicle_life = 40,
               use_legacy_spritesheet = True)
 
 consist.add_unit(DieselRailcar(consist = consist,
                         weight = 65,
                         vehicle_length = 8,
-                        capacity_pax = 65,
+                        capacity_pax = 45,
                         capacity_mail = 40,
                         spriterow_num = 0))
 
