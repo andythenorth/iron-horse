495fc98ac187123388237e128983b27fd050a25b
andythenorth <mail@andythenorth.co.uk>
2016-02-21 12:20:35 +0000

    Feature: Savannah Slammer railcar

--- src/rosters/antelope.py
+++ src/rosters/antelope.py
@@ -1,15 +1,14 @@
 from roster import Roster
 
-#from vehicles import aberdare
+from vehicles import savannah_slammer
 
 # speed for wagons in mph (some generations may optionally have no speed set)
 # format is [standard, speedy]
-speeds = dict(gen_1_wagon_speeds = [65, 85],
-              gen_2_wagon_speeds = [85, 100],
-              gen_3_wagon_speeds = [100, None],
+speeds = dict(gen_1_wagon_speeds = [60, 75],
+              gen_2_wagon_speeds = [75, 75],
               ng_gen_1_wagon_speeds = [55, 55])
 
 roster = Roster(id = 'antelope',
-                numeric_id = 2,
+                numeric_id = 3,
                 speeds = speeds,
-                engines = [])
+                engines = [savannah_slammer])
