99cd7dfaf18e348b253ceca8c77716c283c00c55
andythenorth <mail@andythenorth.co.uk>
2016-02-23 21:36:13 +0000

    Change: adjust Big Boat TE and weight

--- src/vehicles/big_boat.py
+++ src/vehicles/big_boat.py
@@ -6,12 +6,13 @@ consist = EngineConsist(id = 'big_boat',
               title = 'Big Boat [Diesel]',
               replacement_id = '-none',
               power = 4500,
+              tractive_effort_coefficient = 0.35, # dibble up TE, modern diesels can cheat adhesion using wheel slip
               speed = 75,
               vehicle_life = 40,
               intro_date = 1985)
 
 consist.add_unit(DieselLoco(consist = consist,
-                        weight = 120,
+                        weight = 190,
                         vehicle_length = 10,
                         spriterow_num = 0))
 
