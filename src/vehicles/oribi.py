f6732f22231ec8f4e3609435803c5e4e1585837b
andythenorth <mail@andythenorth.co.uk>
2016-03-06 07:01:55 +0000

    Change: add Oribi railcar files, tweak Okapi sprites

---
+++ src/vehicles/oribi.py
@@ -0,0 +1,24 @@
+import global_constants
+from train import EngineConsist, DieselRailcar
+
+consist = EngineConsist(id = 'oribi',
+              base_numeric_id = 1980,
+              title = 'Oribi [Diesel]',
+              replacement_id = '-none',
+              power = 450,
+              track_type = 'NG',
+              speed = 65,
+              type_base_running_cost_points = -28, # dibble running costs for game balance
+              intro_date = 1960,
+              vehicle_life = 40)
+
+consist.add_unit(DieselRailcar(consist = consist,
+                        weight = 65,
+                        vehicle_length = 8,
+                        capacity_pax = 30,
+                        capacity_mail = 18,
+                        spriterow_num = 0))
+
+consist.add_model_variant(intro_date=0,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=0)
