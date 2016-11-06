3fdac2c927ccbf5db5dedb099bc0465269c5a844
andythenorth <mail@andythenorth.co.uk>
2016-11-06 17:47:07 +0000

    Change: balance costs for gen 2 Antelope steam engines

--- src/vehicles/bush_elephant.py
+++ src/vehicles/bush_elephant.py
@@ -8,6 +8,8 @@ consist = EngineConsist(id = 'bush_elephant',
               power = 2200,
               track_type = 'NG',
               speed = 45,
+              type_base_buy_cost_points = 35, # dibble buy cost for game balance
+              type_base_running_cost_points = 35, # dibble running costs for game balance
               vehicle_life = 40,
               intro_date = 1915)
 
