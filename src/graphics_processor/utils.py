78b627eb14f67699ec0122b1d01b1074c8cfcb7f
andythenorth <mail@andythenorth.co.uk>
2016-11-11 21:57:56 +0000

    Codechange: containers only have one input colour, simplify the graphics processing for them

--- src/graphics_processor/utils.py
+++ src/graphics_processor/utils.py
@@ -7,19 +7,10 @@ def make_colour_map(input, output, map_size):
     return result
 
 def get_container_recolour_maps():
-    CC1 = graphics_constants.CC1
-    CC2 = graphics_constants.CC2
-    map_1 = make_colour_map(170, CC1, 8)
-    map_1.update(make_colour_map(42, CC1, 8))
-
-    map_2 = make_colour_map(170, CC2, 8)
-    map_2.update(make_colour_map(42, CC2, 8))
-
+    map_1 = make_colour_map(170, graphics_constants.CC1, 8)
+    map_2 = make_colour_map(170, graphics_constants.CC2, 8)
     map_3 = make_colour_map(170, 8, 8)
-    map_3.update(make_colour_map(42, 8, 8))
-
     return (map_1, map_2, map_3)
-
 
 def get_bulk_cargo_recolour_maps():
     # this is interim glue to the old methods whilst porting the Road Hog graphics processor to Iron Horse
