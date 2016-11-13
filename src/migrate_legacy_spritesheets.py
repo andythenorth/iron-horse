a5298c6e4ccb03b7b90ec9856c53994dbfe230a9
andythenorth <mail@andythenorth.co.uk>
2016-11-13 07:09:25 +0000

    Codechange: (WIP) spritesheet migration basically functional, needs testing to see if works as intended

--- src/migrate_legacy_spritesheets.py
+++ src/migrate_legacy_spritesheets.py
@@ -6,26 +6,40 @@ sys.path.append(os.path.join('src')) # add to the module search path
 
 import iron_horse
 import global_constants
-from pixa import Spritesheet
 from PIL import Image
 
 consists = iron_horse.get_consists_in_buy_menu_order()
+input_graphics_dir = os.path.join('src', 'graphics')
 base_template_spritesheet = Image.open(os.path.join('src','base_10_8_spritesheet.png'))
 spriterow_height = 30
+DOS_PALETTE = Image.open('palette_key.png').palette
 
 def get_legacy_bounding_boxes(y=0):
     return ([60,  y, 8, 27], [76,  y, 26, 24], [107, y, 40, 16], [156, y, 26, 24],
             [188, y, 8, 27], [200, y, 26, 24], [235, y, 40, 16], [284, y, 26, 24],
             [316, y, 64, 16])
 
-def slice_out_legacy_sprites(spriterow):
+def recomp_legacy_spriterows(count, spriterow, migrated_spritesheet):
+    migrated_spriterow = base_template_spritesheet.crop((0, 10, 400, 40))
     for vertexes in get_legacy_bounding_boxes():
         crop_box = (vertexes[0], vertexes[1], vertexes[0] + vertexes[2], vertexes[1] + vertexes[3])
         sprite = spriterow.crop(crop_box)
-        sprite.show()
+        migrated_spriterow.paste(sprite, (vertexes[0], vertexes[1]))
+    insert_loc = (0, 10 + count * spriterow_height)
+    migrated_spritesheet.paste(migrated_spriterow, insert_loc)
+    return migrated_spritesheet
 
-def detect_spriterows_with_content(file_path):
-    legacy_spritesheet = Image.open(file_path)
+def migrate_spritesheet(rows_with_valid_content):
+    new_height = 10 + spriterow_height * len(rows_with_valid_content)
+    migrated_spritesheet = Image.new("P", (400, new_height), 255)
+    migrated_spritesheet.putpalette(DOS_PALETTE)
+    for count, spriterow in enumerate(rows_with_valid_content):
+        migrated_spritesheet = recomp_legacy_spriterows(count, spriterow, migrated_spritesheet)
+    #migrated_spritesheet.show()
+    return migrated_spritesheet
+
+def detect_spriterows_with_content(filename):
+    legacy_spritesheet = Image.open(os.path.join(input_graphics_dir, filename))
     base_y = 10
     rows_with_valid_content = []
     while base_y + spriterow_height < legacy_spritesheet.size[1]:
@@ -40,30 +54,35 @@ def detect_spriterows_with_content(file_path):
             if min(list(test_row.getdata())) > 0:
                 # this is just leftover parts, so show this image so it can be cleaned up manually
                 test_row.show()
-                print(file_path, "contains some orphaned / leftover / junk pixels")
+                print(filename, "contains some orphaned / leftover / junk pixels")
             else:
                 rows_with_valid_content.append(test_row)
-    for spriterow in rows_with_valid_content:
-        slice_out_legacy_sprites(spriterow)
+    return rows_with_valid_content
 
+def main():
+    legacy_filenames = []
+    for consist in consists:
+        if consist.use_legacy_spritesheet:
+            # assumes spritesheets are suffixed _0 or _1, which is probably true
+            for i in range(2):
+                # non-template case
+                filename = os.path.join(consist.id + '_' + str(i) + '.png')
+                if os.path.isfile(os.path.join(input_graphics_dir, filename)):
+                    legacy_filenames.append(filename)
+                # template case
+                filename = os.path.join(consist.id + '_template_' + str(i) + '.png')
+                if os.path.isfile(os.path.join(input_graphics_dir, filename)):
+                    legacy_filenames.append(filename)
+    legacy_filenames.sort()
 
-legacy_file_paths = []
-for consist in consists:
-    if consist.use_legacy_spritesheet:
-        # assumes spritesheets are suffixed _0 or _1, which is probably true
-        for i in range(2):
-            # non-template case
-            file_path = os.path.join('src', 'graphics', consist.id + '_' + str(i) + '.png')
-            if os.path.isfile(file_path):
-                legacy_file_paths.append(file_path)
-            # template case
-            file_path = os.path.join('src', 'graphics', consist.id + '_template_' + str(i) + '.png')
-            if os.path.isfile(file_path):
-                legacy_file_paths.append(file_path)
-legacy_file_paths.sort()
+    for filename in legacy_filenames:
+        output_path = os.path.join(currentdir, 'src', 'graphics_migrated', filename)
+        rows_with_valid_content = detect_spriterows_with_content(filename)
+        migrated_spritesheet = migrate_spritesheet(rows_with_valid_content)
+        migrated_spritesheet.save(output_path)
 
-#for path in legacy_file_paths:
-detect_spriterows_with_content(legacy_file_paths[0])
-#detect_spriterows_with_content('src/graphics/cargo_sprinter_template_0.png')
+    print('Count', len(legacy_filenames))
 
-print('Count', len(legacy_file_paths))
+if __name__ == '__main__':
+    main()
+
