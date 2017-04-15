c1dc8ca7806addb2e88c3feaa199602ae084b1ab
andythenorth <mail@andythenorth.co.uk>
2017-04-15 11:45:13 +0100

    Codechange: comment

--- src/rosters/pony.py
+++ src/rosters/pony.py
@@ -34,9 +34,10 @@ from vehicles import westbourne
 
 # speed for wagons in mph (some generations may optionally have no speed set)
 # format is [standard, speedy]
+# speeds are roughly matched to RH trucks of same era + 5mph or so
 speeds = dict(gen_1_wagon_speeds = [45, 85],
-              gen_2_wagon_speeds = [65, 100],
-              gen_3_wagon_speeds = [85, None],
+              gen_2_wagon_speeds = [60, 100],
+              gen_3_wagon_speeds = [75, None],
               ng_gen_1_wagon_speeds = [55, 55])
 
 roster = Roster(id = 'pony',
