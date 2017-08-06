c0a3fa4b31b933cab1f09cb3a2422cda002ca389
andythenorth <mail@andythenorth.co.uk>
2017-08-06 13:38:32 +0100

    Change: this will be Iron Horse v2 so bump the grfid

--- src/global_constants.py
+++ src/global_constants.py
@@ -166,9 +166,7 @@ cargo_labels = ['PASS', # pax first
                 #
                 'NULL']
 
-# meaning of grfid should be obvious :)
-# (DanMacK is from canada, and IH starts in 1830)
-grfid = r"CA\12\1E"
+grfid = r"CA\12\1F"
 
 # chameleon templating goes faster if a cache dir is used; this specifies which dir is cache dir
 chameleon_cache_dir = '.chameleon_cache'
