f6d6f7d8dcb39d2faa5c707d126e2243bbb36b44
andythenorth <mail@andythenorth.co.uk>
2017-09-16 10:58:49 +0100

    Codechange: wagon type numeric ids list is no longer used, IDs are just assigned manually

--- src/global_constants.py
+++ src/global_constants.py
@@ -37,30 +37,6 @@ buy_menu_sort_order_wagons = ['metro_car',
                               'vehicle_transporter_car_ng',
                               'caboose_car_ng']
 
-# sets have up to 500 IDs for main set, zero-based, and 400 IDs for 'extras' (metro, narrow gauge etc)
-# 900-990 IDs are reserved in case we run into any 'wtf' stuff that needs a gap
-# wagon IDs run 250-490 and 750-890, the 0-240 and 500-740 IDs in a vehicle set are reserved for engines
-
-# revised
-# allow 1000 IDs for wagons, with room for 10 rosters
-# engines seem to need no more than 500 IDs total per roster
-# so we can pack in 0-5000 as engines
-# we can pack in 5000-15000 as wagons
-# 15000-16000 are free to bail us out of a hole
-# allow 15 IDs per wagon type, up to 5 generations
-# stop allocating into 'main' and 'extras', just put in order, from 0
-# room for 66 types, totally excessive tbh, even allowing for inventing new railtypes
-
-wagon_type_numeric_ids = {'caboose_car': 250, 'box_car': 260, 'covered_hopper_car': 270, 'flat_car': 280,
-                          'hopper_car': 290, 'tank_car': 300, 'livestock_car': 310, 'mail_car': 320,
-                          'reefer_car': 330, 'open_car': 340, 'passenger_car': 350, # 'empty': 360,
-                          'intermodal_flat_car': 370, 'edibles_tank_car': 380, 'supplies_car': 390,
-                          'metro_car': 400, # metro car should be in #760+ range, but isn't; no need to break savegames by moving it
-                          'metal_car': 410,
-                          # extra (NG, maglev) wagon IDs start at 750, max extra wagon type ID is 890
-                          'mail_car_ng': 750, 'box_car_ng': 760, 'hopper_car_ng': 770, 'flat_car_ng': 780,
-                          'caboose_car_ng' : 790, 'tank_car_ng' : 800, 'livestock_car_ng': 810,
-                          'open_car_ng': 840, 'passenger_car_ng': 850}
 
 # shared lists of allowed classes, shared across multiple vehicle types
 # these lists are similar but not identical across Iron Horse, Squid, Road Hog etc
