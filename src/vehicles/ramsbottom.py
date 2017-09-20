d31c8d160c16a1eff836df958117f5b10d0d3c5a
andythenorth <mail@andythenorth.co.uk>
2017-09-20 07:53:15 +0100

    Fix: adjust Ramsbottom speed

--- src/vehicles/ramsbottom.py
+++ src/vehicles/ramsbottom.py
@@ -6,7 +6,7 @@ consist = EngineConsist(id = 'ramsbottom',
               title = '0-6-0 Ramsbottom [Steam]',
               power = 1100,
               tractive_effort_coefficient = 0.22,
-              speed = 40,
+              speed = 45,
               type_base_buy_cost_points = 12, # dibble buy cost for game balance
               vehicle_life = 40,
               intro_date = 1860)
