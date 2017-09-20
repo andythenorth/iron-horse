5f3f75fd5c03cae7df47d740a52d20a0d376e388
andythenorth <mail@andythenorth.co.uk>
2017-09-20 11:32:07 +0100

    Change: adjust Double Juice to freight loco class + stats

--- src/vehicles/double_juice.py
+++ src/vehicles/double_juice.py
@@ -4,13 +4,12 @@ from train import EngineConsist, ElectroDieselLoco
 consist = EngineConsist(id = 'double_juice',
               base_numeric_id = 160,
               title = 'Double Juice [ElectroDiesel]',
-              power = 6750,
-              tractive_effort_coefficient = 0.4, #dibble for game balance, assume super-slip control
-              speed = 100,
+              power = 1700,
+              speed = 90,
               type_base_buy_cost_points = 60, # dibble buy cost for game balance
               vehicle_life = 40,
-              intro_date = 2015, # it's modern-ish eh, don't introduce too early, keeps IH Pony roster alive past 1990
-              power_by_railtype = {'RAIL': 3750, 'ELRL': 6750})
+              intro_date = 1990,
+              power_by_railtype = {'RAIL': 1700, 'ELRL': 3400})
 
 consist.add_unit(ElectroDieselLoco(consist = consist,
                         weight = 120,
