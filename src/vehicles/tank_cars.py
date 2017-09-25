d2c407d6eaf94d1d98f7f456c70be4dac27e8547
andythenorth <mail@andythenorth.co.uk>
2017-09-25 08:19:08 +0100

    Change: better – views for tank cars

--- src/vehicles/tank_cars.py
+++ src/vehicles/tank_cars.py
@@ -27,8 +27,29 @@ def main():
     consist = TankConsist(title = '[Tank Car]',
                       roster = 'pony',
                       base_numeric_id = 640,
-                      wagon_generation = 2,
+                      wagon_generation = 3,
                       intro_date = 1960,
+                      vehicle_life = 40)
+
+    consist.add_unit(Wagon(consist = consist,
+                            capacity_freight = 40,
+                            weight = 27,
+                            vehicle_length = 6))
+
+    consist.add_model_variant(intro_date=0,
+                           end_date=global_constants.max_game_date,
+                           graphics_processor=consist.graphics_processors['pass_through'])
+
+    consist.add_model_variant(intro_date=0,
+                           end_date=global_constants.max_game_date,
+                           graphics_processor=consist.graphics_processors['swap_company_colours'])
+
+
+    consist = TankConsist(title = '[Tank Car]',
+                      roster = 'pony',
+                      base_numeric_id = 960,
+                      wagon_generation = 4,
+                      intro_date = 1990,
                       vehicle_life = 40)
 
     consist.add_unit(Wagon(consist = consist,
