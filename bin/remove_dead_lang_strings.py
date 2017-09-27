1c96ccfc61c362c60f1bbcfd257703f2f482bee2
andythenorth <mail@andythenorth.co.uk>
2017-09-27 13:15:33 +0100

    Codechange: add script to remove dead lang strings

---
+++ bin/remove_dead_lang_strings.py
@@ -0,0 +1,35 @@
+import os.path
+currentdir = os.curdir
+
+import codecs
+
+import sys
+sys.path.append(os.path.join('src')) # add to the module search path
+
+# Sometimes strings are deprecated and need removing from all lang files
+# this script removes dead strings - adjust the 'dead_strings' list to suit
+
+# never leave empty strings or strings with only spaces in this list, that will strip everything from a lang file
+dead_strings = ["STR_NAME_SUFFIX_COMBINE_CAR",]
+
+def delete_string(dead_string):
+    for filename in os.listdir(os.path.join('src','lang')):
+        print(filename)
+        if filename is not '.DS_Store':
+            file = codecs.open(os.path.join('src','lang', filename),'r', encoding='utf-8')
+            content = file.readlines()
+            result = []
+
+            for line in content:
+                if dead_string not in line:
+                    result.append(line)
+
+            file = open(os.path.join('src','lang',filename),'w')
+            for line in result:
+                file.write(line)
+            file.close
+
+for dead_string in dead_strings:
+    #pass
+    delete_string(dead_string)
+    #insert_property(filename)
