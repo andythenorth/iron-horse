c04d4a1551788715ece1b3889c98b4e0b215f302
andythenorth <mail@andythenorth.co.uk>
2017-09-29 18:03:48 +0100

    Change: gen 3/4 hopper improvements

--- src/vehicles/hopper_cars.py
+++ src/vehicles/hopper_cars.py
@@ -43,6 +43,44 @@ def main():
                               graphics_processor=consist.graphics_processors[1])
 
     consist = HopperConsist(roster='pony',
+                            base_numeric_id=2320,
+                            gen=4,
+                            subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=30,
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = HopperConsist(roster='pony',
+                            base_numeric_id=1380,
+                            gen=4,
+                            subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=40,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = HopperConsist(roster='pony',
                             base_numeric_id=1080,
                             gen=5,
                             subtype='A')
