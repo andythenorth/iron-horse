3915a36b21ae57037698491e7abe2b499a7b7f46
andythenorth <mail@andythenorth.co.uk>
2017-09-29 19:46:29 +0100

    Change: gen 6 dump car

--- src/vehicles/dump_cars.py
+++ src/vehicles/dump_cars.py
@@ -118,6 +118,24 @@ def main():
                               spritesheet_suffix=1,
                               graphics_processor=consist.graphics_processors[1])
 
+    consist = DumpConsist(roster='pony',
+                          base_numeric_id=2390,
+                          gen=6,
+                          subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=40,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
     #--------------- llama ----------------------------------------------------------------------
 
     #--------------- antelope ----------------------------------------------------------------------
