581eef010b7263df576ad29d560ea92ae6021c08
andythenorth <mail@andythenorth.co.uk>
2017-09-29 19:30:20 +0100

    Change: dump car generations

--- src/vehicles/dump_cars.py
+++ src/vehicles/dump_cars.py
@@ -5,7 +5,7 @@ from train import DumpConsist, FreightCar
 def main():
     #--------------- pony ----------------------------------------------------------------------
     consist = DumpConsist(roster='pony',
-                          base_numeric_id=1340,
+                          base_numeric_id=2350,
                           gen=3,
                           subtype='A')
 
@@ -24,13 +24,89 @@ def main():
                               graphics_processor=consist.graphics_processors[1])
 
     consist = DumpConsist(roster='pony',
-                          base_numeric_id=1810,
-                          gen=4,
-                          subtype='A')
+                          base_numeric_id=2360,
+                          gen=3,
+                          subtype='B')
 
     consist.add_unit(type=FreightCar,
                      capacity=40,
                      vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = DumpConsist(roster='pony',
+                          base_numeric_id=2370,
+                          gen=4,
+                          subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=20,
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = DumpConsist(roster='pony',
+                          base_numeric_id=2380,
+                          gen=4,
+                          subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=40,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = DumpConsist(roster='pony',
+                          base_numeric_id=1340,
+                          gen=5,
+                          subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=20,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = DumpConsist(roster='pony',
+                          base_numeric_id=1810,
+                          gen=5,
+                          subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=40,
+                     vehicle_length=8)
 
     consist.add_model_variant(start_date=0,
                               end_date=global_constants.max_game_date,
