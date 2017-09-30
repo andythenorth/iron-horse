9ab7d162bb967c6a0a2663fa38767a4c0b6a909b
andythenorth <mail@andythenorth.co.uk>
2017-09-30 15:58:54 +0100

    Change: flat cars

--- src/vehicles/flat_cars.py
+++ src/vehicles/flat_cars.py
@@ -27,12 +27,12 @@ def main():
     consist = FlatConsist(roster='pony',
                           base_numeric_id=1150,
                           gen=2,
-                          subtype='A')
+                          subtype='B')
 
     consist.add_unit(type=FreightCar,
                      capacity=40,
                      cargo_length=3,
-                     vehicle_length=8)
+                     vehicle_length=6)
 
     consist.add_model_variant(start_date=0,
                               end_date=global_constants.max_game_date,
@@ -48,6 +48,106 @@ def main():
                           base_numeric_id=1160,
                           gen=3,
                           subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=50,
+                     cargo_length=3,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = FlatConsist(roster='pony',
+                          base_numeric_id=2550,
+                          gen=3,
+                          subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=50,
+                     cargo_length=3,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = FlatConsist(roster='pony',
+                          base_numeric_id=2540,
+                          gen=4,
+                          subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=50,
+                     cargo_length=3,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = FlatConsist(roster='pony',
+                          base_numeric_id=2530,
+                          gen=4,
+                          subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=50,
+                     cargo_length=3,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = FlatConsist(roster='pony',
+                          base_numeric_id=2520,
+                          gen=5,
+                          subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     capacity=50,
+                     cargo_length=3,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = FlatConsist(roster='pony',
+                          base_numeric_id=2510,
+                          gen=5,
+                          subtype='B')
 
     consist.add_unit(type=FreightCar,
                      capacity=50,
