e2f2ff4047f94bd6b8cee728d12b44a36d524841
andythenorth <mail@andythenorth.co.uk>
2017-10-07 09:59:51 +0100

    Change: stub in stake cars

--- src/vehicles/stake_cars.py
+++ src/vehicles/stake_cars.py
@@ -5,9 +5,33 @@ from train import StakeConsist, FreightCar
 def main():
     #--------------- pony ----------------------------------------------------------------------
     consist = StakeConsist(roster='pony',
+                         base_numeric_id=2740,
+                         gen=2,
+                         subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0)
+
+    consist = StakeConsist(roster='pony',
                          base_numeric_id=2730,
                          gen=3,
                          subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0)
+
+    consist = StakeConsist(roster='pony',
+                         base_numeric_id=2750,
+                         gen=3,
+                         subtype='B')
 
     consist.add_unit(type=FreightCar,
                      vehicle_length=6)
@@ -22,6 +46,18 @@ def main():
                          subtype='A')
 
     consist.add_unit(type=FreightCar,
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0)
+
+    consist = StakeConsist(roster='pony',
+                         base_numeric_id=2760,
+                         gen=4,
+                         subtype='B')
+
+    consist.add_unit(type=FreightCar,
                      vehicle_length=6)
 
     consist.add_model_variant(start_date=0,
@@ -34,12 +70,47 @@ def main():
                          subtype='A')
 
     consist.add_unit(type=FreightCar,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0)
+
+    consist = StakeConsist(roster='pony',
+                         base_numeric_id=2770,
+                         gen=5,
+                         subtype='B')
+
+    consist.add_unit(type=FreightCar,
                      vehicle_length=8)
 
     consist.add_model_variant(start_date=0,
                               end_date=global_constants.max_game_date,
                               spritesheet_suffix=0)
 
+    consist = StakeConsist(roster='pony',
+                         base_numeric_id=2780,
+                         gen=6,
+                         subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0)
+
+    consist = StakeConsist(roster='pony',
+                         base_numeric_id=2790,
+                         gen=6,
+                         subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0)
     # --------------- antelope ----------------------------------------------------------------------
 
     #--------------- llama ----------------------------------------------------------------------
