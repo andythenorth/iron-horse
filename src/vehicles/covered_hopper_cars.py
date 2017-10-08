b57997c86de6beb85cc8b213fc39440abf3cc14f
andythenorth <mail@andythenorth.co.uk>
2017-10-08 09:13:20 +0100

    Change: stub in Covered Hopper Cars

--- src/vehicles/covered_hopper_cars.py
+++ src/vehicles/covered_hopper_cars.py
@@ -6,11 +6,47 @@ def main():
     #--------------- pony ----------------------------------------------------------------------
     consist = CoveredHopperConsist(roster='pony',
                                    base_numeric_id=1270,
-                                   gen=1,
+                                   gen=2,
                                    subtype='A')
 
     consist.add_unit(type=FreightCar,
-                     vehicle_length=5)
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = CoveredHopperConsist(roster='pony',
+                                   base_numeric_id=1260,
+                                   gen=3,
+                                   subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=4)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = CoveredHopperConsist(roster='pony',
+                                   base_numeric_id=2940,
+                                   gen=4,
+                                   subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=4)
 
     consist.add_model_variant(start_date=0,
                               end_date=global_constants.max_game_date,
@@ -25,7 +61,7 @@ def main():
     consist = CoveredHopperConsist(roster='pony',
                                    base_numeric_id=1230,
                                    gen=4,
-                                   subtype='A')
+                                   subtype='B')
 
     consist.add_unit(type=FreightCar,
                      vehicle_length=6)
@@ -46,6 +82,60 @@ def main():
                                    subtype='A')
 
     consist.add_unit(type=FreightCar,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = CoveredHopperConsist(roster='pony',
+                                   base_numeric_id=2910,
+                                   gen=5,
+                                   subtype='B')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = CoveredHopperConsist(roster='pony',
+                                   base_numeric_id=2920,
+                                   gen=6,
+                                   subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=6)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = CoveredHopperConsist(roster='pony',
+                                   base_numeric_id=2930,
+                                   gen=6,
+                                   subtype='B')
+
+    consist.add_unit(type=FreightCar,
                      vehicle_length=8)
 
     consist.add_model_variant(start_date=0,
@@ -59,38 +149,3 @@ def main():
                               graphics_processor=consist.graphics_processors[1])
 
     #--------------- llama ----------------------------------------------------------------------
-    consist = CoveredHopperConsist(roster='llama',
-                                   base_numeric_id=1250,
-                                   gen=2,
-                                   subtype='A')
-
-    consist.add_unit(type=FreightCar,
-                     vehicle_length=7)
-
-    consist.add_model_variant(start_date=0,
-                              end_date=global_constants.max_game_date,
-                              spritesheet_suffix=0,
-                              graphics_processor=consist.graphics_processors[0])
-
-    consist.add_model_variant(start_date=0,
-                              end_date=global_constants.max_game_date,
-                              spritesheet_suffix=1,
-                              graphics_processor=consist.graphics_processors[1])
-
-    consist = CoveredHopperConsist(roster='llama',
-                                   base_numeric_id=1260,
-                                   gen=3,
-                                   subtype='A')
-
-    consist.add_unit(type=FreightCar,
-                     vehicle_length=8)
-
-    consist.add_model_variant(start_date=0,
-                              end_date=global_constants.max_game_date,
-                              spritesheet_suffix=0,
-                              graphics_processor=consist.graphics_processors[0])
-
-    consist.add_model_variant(start_date=0,
-                              end_date=global_constants.max_game_date,
-                              spritesheet_suffix=1,
-                              graphics_processor=consist.graphics_processors[1])
