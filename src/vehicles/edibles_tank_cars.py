48341673b685af1d456683b9593d74421dd40d8f
andythenorth <mail@andythenorth.co.uk>
2017-10-08 16:25:54 +0100

    Change: stub in more edibles tank cars

--- src/vehicles/edibles_tank_cars.py
+++ src/vehicles/edibles_tank_cars.py
@@ -26,8 +26,44 @@ def main():
                               graphics_processor=consist.graphics_processors[1])
 
     consist = EdiblesTankConsist(roster='pony',
+                                 base_numeric_id=2990,
+                                 gen=4,
+                                 subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = EdiblesTankConsist(roster='pony',
                                  base_numeric_id=1200,
                                  gen=5,
+                                 subtype='A')
+
+    consist.add_unit(type=FreightCar,
+                     vehicle_length=8)
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=0,
+                              graphics_processor=consist.graphics_processors[0])
+
+    consist.add_model_variant(start_date=0,
+                              end_date=global_constants.max_game_date,
+                              spritesheet_suffix=1,
+                              graphics_processor=consist.graphics_processors[1])
+
+    consist = EdiblesTankConsist(roster='pony',
+                                 base_numeric_id=3000,
+                                 gen=6,
                                  subtype='A')
 
     consist.add_unit(type=FreightCar,
