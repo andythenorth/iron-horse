4451d879fb76bcc6af8f97ae164b5341e7ff500c
andythenorth <mail@andythenorth.co.uk>
2017-10-13 07:50:20 +0100

    Change: tank cars

--- src/vehicles/edibles_tank_cars.py
+++ src/vehicles/edibles_tank_cars.py
@@ -31,7 +31,7 @@ def main():
                                  subtype='A')
 
     consist.add_unit(type=FreightCar,
-                     vehicle_length=8)
+                     vehicle_length=6)
 
     consist.add_model_variant(start_date=0,
                               end_date=global_constants.max_game_date,
