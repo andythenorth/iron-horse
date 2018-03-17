033696a4dc9945f694ad6bbc33ccf0043aa6ba59
andythenorth <mail@andythenorth.co.uk>
2018-03-17 17:05:22 +0000

    Codechange: no need for a lang_templates dir, there's only one template, move it to templates dir

--- src/render_lang.py
+++ src/render_lang.py
@@ -15,7 +15,7 @@ import codecs # used for writing files - more unicode friendly than standard ope
 
 from chameleon import PageTemplateLoader # chameleon used in most template cases
 # setup the places we look for templates
-lang_templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'lang_templates'))
+templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'templates'))
 
 # get args passed by makefile
 makefile_args = utils.get_makefile_args(sys)
@@ -38,7 +38,7 @@ def main():
     languages_with_generation = ('english',)
     for i in languages_with_generation:
         #compile strings to single lang file - english
-        lang_template = lang_templates[i + '.pylng']
+        lang_template = templates[i + '.pylng']
 
         src_file = codecs.open(os.path.join(lang_src, i + '.lng'), 'r','utf8')
         dst_file = codecs.open(os.path.join(lang_dst, i + '.lng'), 'w','utf8')
