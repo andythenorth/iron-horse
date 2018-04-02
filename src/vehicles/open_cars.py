5ad7f29c01ef797a6e17b9f4fb109ad3d6c99293
andythenorth <mail@andythenorth.co.uk>
2018-04-02 19:44:14 +0100

    Change: gen 4B open car

--- src/vehicles/open_cars.py
+++ src/vehicles/open_cars.py
@@ -24,7 +24,7 @@ def main():
                              subtype='A')
 
     consist.add_unit(type=FreightCar,
-                     cargo_length=3,
+                     cargo_length=4,
                      vehicle_length=4,
                      chassis='2_axle_filled_16px')
 
@@ -37,7 +37,7 @@ def main():
                              subtype='B')
 
     consist.add_unit(type=FreightCar,
-                     cargo_length=3,
+                     cargo_length=6,
                      vehicle_length=6,
                      chassis='4_axle_gapped_24px')
 
@@ -48,7 +48,7 @@ def main():
                              subtype='A')
 
     consist.add_unit(type=FreightCar,
-                     cargo_length=3,
+                     cargo_length=4,
                      vehicle_length=4,
                      chassis='2_axle_filled_16px')
 
@@ -59,7 +59,7 @@ def main():
                              subtype='B')
 
     consist.add_unit(type=FreightCar,
-                     cargo_length=3,
+                     cargo_length=6,
                      vehicle_length=6,
                      chassis='4_axle_gapped_24px')
 
@@ -70,8 +70,9 @@ def main():
                              subtype='A')
 
     consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=4)
+                     cargo_length=4,
+                     vehicle_length=4,
+                     chassis='2_axle_filled_16px')
 
 
     consist = OpenCarConsist(roster='pony',
@@ -80,8 +81,9 @@ def main():
                              subtype='B')
 
     consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=6)
+                     cargo_length=6,
+                     vehicle_length=6,
+                     chassis='2_axle_filled_24px')
 
 
     consist = OpenCarConsist(roster='pony',
@@ -105,100 +107,3 @@ def main():
 
 
     # no gen 6 open cars, cap to gen 5 in Pony
-
-    #--------------- llama ----------------------------------------------------------------------
-    consist = OpenCarConsist(roster='llama',
-                             base_numeric_id=860,
-                             gen=1,
-                             subtype='A')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=5)
-
-
-    consist = OpenCarConsist(roster='llama',
-                             base_numeric_id=1330,
-                             gen=2,
-                             subtype='A')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=5)
-
-
-    consist = OpenCarConsist(roster='llama',
-                             base_numeric_id=870,
-                             gen=1,
-                             subtype='A',
-                             track_type='NG')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=5)
-
-
-    consist = OpenCarConsist(roster='llama',
-                             base_numeric_id=1320,
-                             gen=2,
-                             subtype='A',
-                             track_type='NG')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=5)
-
-
-    #--------------- antelope ----------------------------------------------------------------------
-    consist = OpenCarConsist(roster='antelope',
-                             base_numeric_id=1760,
-                             gen=1,
-                             subtype='A')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=5)
-
-
-    consist = OpenCarConsist(roster='antelope',
-                             base_numeric_id=1770,
-                             gen=2,
-                             subtype='A')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=8)
-
-
-    consist = OpenCarConsist(roster='antelope',
-                             base_numeric_id=2090,
-                             gen=1,
-                             subtype='A',
-                             track_type='NG')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=5)
-
-
-    consist = OpenCarConsist(roster='antelope',
-                             base_numeric_id=1830,
-                             gen=2,
-                             subtype='A',
-                             track_type='NG')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=6)
-
-
-    consist = OpenCarConsist(roster='antelope',
-                             base_numeric_id=1820,
-                             gen=3,
-                             subtype='A',
-                             track_type='NG')
-
-    consist.add_unit(type=FreightCar,
-                     cargo_length=3,
-                     vehicle_length=6)
-
