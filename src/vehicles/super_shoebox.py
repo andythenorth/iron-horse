7b5025de964e6d04eca13c803bd2d2f0f8afa66b
andythenorth <mail@andythenorth.co.uk>
2018-04-21 22:40:18 +0100

    Change: improved sprites for all shoebox engines

--- src/vehicles/super_shoebox.py
+++ src/vehicles/super_shoebox.py
@@ -9,6 +9,7 @@ consist = EngineConsist(id='super_shoebox',
                         type_base_buy_cost_points=0,  # dibble buy cost for game balance
                         type_base_running_cost_points=-28,  # dibble run cost for game balance
                         gen=5,
+                        random_reverse=True,
                         power_by_railtype={'RAIL': 1100, 'ELRL': 2200})
 
 consist.add_unit(type=ElectroDieselEngineUnit,
