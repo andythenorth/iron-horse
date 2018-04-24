8006f732f90d8edfbc803717cb0869a3b4e34b6f
andythenorth <mail@andythenorth.co.uk>
2018-04-24 10:39:55 +0100

    Fix: wrong speed for Ultra Shoebox

--- src/vehicles/ultra_shoebox.py
+++ src/vehicles/ultra_shoebox.py
@@ -6,7 +6,7 @@ consist = EngineConsist(id='ultra_shoebox',
                         role='branch',
                         power=1250,
                         power_by_railtype={'RAIL': 1250, 'ELRL': 2500},
-                        speed=125,
+                        speed=140,
                         type_base_buy_cost_points=0,  # dibble buy cost for game balance
                         type_base_running_cost_points=-28,  # dibble run cost for game balance
                         random_reverse=True,
