0ce00d26c4ae21602c5daed176dfb47dc7f750bb
andythenorth <mail@andythenorth.co.uk>
2018-04-29 19:06:56 +0100

    Change: adjust Peasweep stats

--- src/vehicles/peasweep.py
+++ src/vehicles/peasweep.py
@@ -5,14 +5,14 @@ consist = EngineConsist(id='peasweep',
                         name='Peasweep',
                         role='heavy_freight_2',
                         power=3600,
-                        tractive_effort_coefficient=0.25,
-                        type_base_buy_cost_points=10,  # dibble buy cost for game balance
+                        type_base_buy_cost_points=40,  # dibble buy cost for game balance
+                        type_base_running_cost_points=-5,  # dibble run cost for game balance
                         joker=True, # this engine doesn't fit the set roster pattern, by design it's to mix things up
                         gen=4,
                         intro_date_offset=-13) # introduce earlier than gen epoch by design
 
 consist.add_unit(type=ElectricEngineUnit,
-                 weight=70,
+                 weight=75,
                  vehicle_length=6,
                  spriterow_num=0,
                  repeat=2)
