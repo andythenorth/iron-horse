6f57ab369fd957f30b209e7f3200666bf7928885
andythenorth <mail@andythenorth.co.uk>
2018-04-30 18:38:05 +0100

    Change: adjust Super Shoebox and Ultra Shoebox stats to (1) balance with brananch_freight locos (2) support the higher speed with a bit more HP

--- src/vehicles/super_shoebox.py
+++ src/vehicles/super_shoebox.py
@@ -4,8 +4,8 @@ consist = EngineConsist(id='super_shoebox',
                         base_numeric_id=880,
                         name='Super Shoebox',
                         role='branch',
-                        power=1100,
-                        power_by_railtype={'RAIL': 1100, 'ELRL': 2200},
+                        power=1200, # a bit higher than progression would require, to support higher speed
+                        power_by_railtype={'RAIL': 1200, 'ELRL': 2200},
                         type_base_buy_cost_points=0,  # dibble buy cost for game balance
                         type_base_running_cost_points=-28,  # dibble run cost for game balance
                         random_reverse=True,
--- src/vehicles/ultra_shoebox.py
+++ src/vehicles/ultra_shoebox.py
@@ -4,8 +4,8 @@ consist = EngineConsist(id='ultra_shoebox',
                         base_numeric_id=2170,
                         name='Ultra Shoebox',
                         role='branch',
-                        power=1250,
-                        power_by_railtype={'RAIL': 1250, 'ELRL': 2500},
+                        power=1450, # a bit higher than progression would require, to support higher speed
+                        power_by_railtype={'RAIL': 1450, 'ELRL': 2600},
                         type_base_buy_cost_points=0,  # dibble buy cost for game balance
                         type_base_running_cost_points=-28,  # dibble run cost for game balance
                         random_reverse=True,
