96c8a15cfbf14fe56569ee4316a93b96f58d67e5
andythenorth <mail@andythenorth.co.uk>
2018-09-21 09:32:25 +0100

    Change: WIP on Donnegal railcar

--- src/vehicles/donegal.py
+++ src/vehicles/donegal.py
@@ -1,17 +1,17 @@
-from train import EngineConsist, DieselRailcarUnit
+from train import PassengerEngineRailcarConsist, DieselRailcarUnit
 
-consist = EngineConsist(id='donegal',
-                        base_numeric_id=140,
-                        name='Donnegal',
-                        role='pax_railcar',
-                        base_track_type='NG',
-                        power=250,
-                        type_base_buy_cost_points=-18,  # dibble buy cost for game balance
-                        type_base_running_cost_points=-30,  # dibble running costs for game balance
-                        gen=3)
+consist = PassengerEngineRailcarConsist(id='donegal',
+                                        base_numeric_id=140,
+                                        name='Donnegal',
+                                        role='pax_railcar',
+                                        base_track_type='NG',
+                                        power=250,
+                                        type_base_buy_cost_points=-18,  # dibble buy cost for game balance
+                                        type_base_running_cost_points=-30,  # dibble running costs for game balance
+                                        gen=3)
 
 consist.add_unit(type=DieselRailcarUnit,
                  weight=20,
-                 vehicle_length=7,
-                 capacity=35,
-                 spriterow_num=0)
+                 vehicle_length=6,
+                 capacity=30,
+                 chassis='railcar_ng')
