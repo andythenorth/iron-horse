5365bad4a5c6e6f03798286e97c6df0d18b3cb12
andythenorth <mail@andythenorth.co.uk>
2018-09-29 16:22:58 +0100

    Change: sprites complete for gen 4B covered hopper car

--- src/vehicles/covered_hopper_cars.py
+++ src/vehicles/covered_hopper_cars.py
@@ -36,7 +36,8 @@ def main():
     consist = CoveredHopperCarConsist(roster='pony',
                                       base_numeric_id=1230,
                                       gen=4,
-                                      subtype='B')
+                                      subtype='B',
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      vehicle_length=6,
