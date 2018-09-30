eb26f7f2c343b3c58f49a41643711e938a7de300
andythenorth <mail@andythenorth.co.uk>
2018-09-30 13:31:17 +0100

    Change: sprites complete for Faun

--- src/vehicles/faun.py
+++ src/vehicles/faun.py
@@ -8,7 +8,8 @@ consist = EngineConsist(id='faun',
                         random_reverse=True,
                         joker=True,
                         gen=5,
-                        intro_date_offset=-5)
+                        intro_date_offset=-5,
+                        sprites_complete=True)
 
 consist.add_unit(type=DieselEngineUnit,
                  weight=70,
