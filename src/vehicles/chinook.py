7c3530f8e14fb3b094957e1fecc304ae621a6daf
andythenorth <mail@andythenorth.co.uk>
2018-09-30 09:08:00 +0100

    Change: sprites complete for Chinook

--- src/vehicles/chinook.py
+++ src/vehicles/chinook.py
@@ -6,7 +6,8 @@ consist = EngineConsist(id='chinook',
                         role='heavy_freight_1',
                         power=3000,
                         type_base_buy_cost_points=30,  # dibble buy cost for game balance
-                        gen=4)
+                        gen=4,
+                        sprites_complete=True)
 
 consist.add_unit(type=DieselEngineUnit,
                  weight=80,
