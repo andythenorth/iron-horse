7e32b0bcd8ed5654f4a50df1742ce8f3787214c1
andythenorth <mail@andythenorth.co.uk>
2018-10-28 21:25:15 +0000

    Change: WIP on NG sprites

--- src/vehicles/boar_cat.py
+++ src/vehicles/boar_cat.py
@@ -5,6 +5,7 @@ consist = EngineConsist(id='boar_cat',
                         name='Boar Cat',
                         role='universal',
                         power=800,
+                        random_reverse=True,
                         base_track_type='NG',
                         type_base_buy_cost_points=-10,  # dibble buy cost for game balance
                         type_base_running_cost_points=-15,  # dibble running costs for game balance
