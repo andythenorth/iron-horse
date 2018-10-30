27d12a50199f93a2fd9568e13292e7aebb799c1a
andythenorth <mail@andythenorth.co.uk>
2018-10-30 21:52:29 +0000

    Change: sprites complete for Pikel

--- src/vehicles/pikel.py
+++ src/vehicles/pikel.py
@@ -9,7 +9,8 @@ consist = EngineConsist(id='pikel',
                         base_track_type='NG',
                         type_base_buy_cost_points=-10,  # dibble buy cost for game balance
                         type_base_running_cost_points=-15,  # dibble running costs for game balance
-                        gen=3)
+                        gen=3,
+                        sprites_complete=True)
 
 consist.add_unit(type=DieselEngineUnit,
                  weight=40,
