d8474647c4101ce04d25763576873eead0650c3f
andythenorth <mail@andythenorth.co.uk>
2018-11-24 12:27:16 +0000

    Codechange: id_gaps printer was missing many gaps, reimplement to be accurate

--- src/iron_horse.py
+++ src/iron_horse.py
@@ -146,7 +146,10 @@ def vacant_numeric_ids_formatted():
     # when adding vehicles it's useful to know what the next free numeric ID is
     # tidy-mind problem, but do we have any vacant numeric ID slots in the currently used range?
     # 'print' eh? - but it's fine echo_message isn't intended for this kind of info, don't bother changing
-    id_gaps = [str(i - 10) for i in sorted(numeric_id_defender) if not (i - 10)
-               in numeric_id_defender and i is not 0 and i % 10 is 0]
-
+    max_id = max(numeric_id_defender)
+    id_gaps = []
+    for i in range(0, int(max_id/10)):
+        id = i * 10
+        if id not in numeric_id_defender:
+            id_gaps.append(str(id))
     return "Vacant numeric ID slots: " + ', '.join(id_gaps) + (" and from " if len(id_gaps) > 0 else '') + str(max(numeric_id_defender) + 10) + " onwards"
