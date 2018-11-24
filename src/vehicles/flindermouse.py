f27228d49657e4054a082b49fe437f1c0addd850
andythenorth <mail@andythenorth.co.uk>
2018-11-24 13:15:51 +0000

    Change: tweaks to Flinder Mouse and Hurly Burly

--- src/vehicles/flindermouse.py
+++ src/vehicles/flindermouse.py
@@ -4,14 +4,14 @@ consist = EngineConsist(id='flindermouse',
                         base_numeric_id=790,
                         name='Flindermouse',
                         role='heavy_freight_2',
-                        power=3600,
+                        power=2200,
                         joker=True,  # this engine doesn't fit the set roster pattern, by design it's to mix things up
-                        gen=2,
+                        gen=3,
                         pantograph_type='diamond-double',
-                        intro_date_offset=-13)  # introduce earlier than gen epoch by design
+                        intro_date_offset=-20)  # introduce earlier than gen epoch by design
 
 consist.add_unit(type=ElectricEngineUnit,
-                 weight=75,
+                 weight=65,
                  vehicle_length=6,
                  spriterow_num=0,
                  repeat=2)
