19d37516f9f379def8092a00c7f041fa232d362e
andythenorth <mail@andythenorth.co.uk>
2018-12-01 00:22:00 +0000

    Change: make Shredder a double-headed engine, and stop mucking around with TE etc

--- src/vehicles/shredder.py
+++ src/vehicles/shredder.py
@@ -4,14 +4,14 @@ consist = EngineConsist(id='shredder',
                         base_numeric_id=900,
                         name='Shredder',
                         role='express_1',
-                        power=1300,
-                        # nerf TE for game balance, assume magic or sand or something
-                        tractive_effort_coefficient=0.27,
+                        power=2750,
                         joker=True,  # this engine doesn't fit the set roster pattern, by design it's to mix things up
                         random_reverse=True,
+                        intro_date_offset=2, # let's not have everything turn up in 1960
                         gen=4)
 
 consist.add_unit(type=DieselEngineUnit,
                  weight=72,
                  vehicle_length=6,
-                 spriterow_num=0)
+                 spriterow_num=0,
+                 repeat=2)
