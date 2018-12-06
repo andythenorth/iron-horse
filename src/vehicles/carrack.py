9101fecf3b158631484acbf0da1415ffe6eee061
andythenorth <mail@andythenorth.co.uk>
2018-12-06 21:22:51 +0000

    Change: try introducing Carrak a bit earlier than epoch, as lots of engines appear in 1900 and Spinner is abit gutless

--- src/vehicles/carrack.py
+++ src/vehicles/carrack.py
@@ -4,10 +4,12 @@ consist = EngineConsist(id='carrack',
                         base_numeric_id=1040,
                         name='4-4-0 Carrack',
                         role='express_1',
+                        joker=True,  # this engine doesn't fit the intro date pattern, to mix things up a bit
                         power=1050,
                         tractive_effort_coefficient=0.18,
                         buy_cost=47,
-                        gen=2)
+                        gen=2,
+                        intro_date_offset=-3)  # introduce earlier than gen epoch by design
 
 consist.add_unit(type=SteamEngineUnit,
                  weight=60,
