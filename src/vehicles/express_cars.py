098a926179219f2b9842fcf1a8286bd57e4b3a55
andythenorth <mail@andythenorth.co.uk>
2018-12-09 21:06:18 +0000

    Change: gen 3 express cars

--- src/vehicles/express_cars.py
+++ src/vehicles/express_cars.py
@@ -40,3 +40,25 @@ def main():
                      vehicle_length=6,
                      chassis='4_axle_solid_express_24px')
 
+
+    consist = ExpressCarConsist(roster='pony',
+                             base_numeric_id=1110,
+                             gen=3,
+                             subtype='A',
+                             sprites_complete=False)
+
+    consist.add_unit(type=ExpressCar,
+                     vehicle_length=4,
+                     chassis='3_axle_solid_express_16px')
+
+
+    consist = ExpressCarConsist(roster='pony',
+                             base_numeric_id=1120,
+                             gen=3,
+                             subtype='B',
+                             sprites_complete=False)
+
+    consist.add_unit(type=ExpressCar,
+                     vehicle_length=6,
+                     chassis='4_axle_solid_express_24px')
+
