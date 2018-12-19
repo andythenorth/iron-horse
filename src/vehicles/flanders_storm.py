652e0b445831e631d4646b75b09868e1f1ebb3b7
andythenorth <mail@andythenorth.co.uk>
2018-12-19 19:47:49 +0000

    Change: sprites complete for Roarer and Flanders Storm

--- src/vehicles/flanders_storm.py
+++ src/vehicles/flanders_storm.py
@@ -11,7 +11,8 @@ consist = EngineConsist(id='flanders_storm',
                         joker=True,  # this engine doesn't fit the set roster pattern, by design it's to mix things up
                         gen=5,
                         pantograph_type='z-shaped-double',
-                        intro_date_offset=5)  # introduce later than gen epoch by design
+                        intro_date_offset=5,  # introduce later than gen epoch by design
+                        sprites_complete=True)
 
 consist.add_unit(type=ElectricEngineUnit,
                  weight=120,
