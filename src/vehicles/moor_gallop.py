3a28711186affe4b65d4386854b20f72961c6525
andythenorth <mail@andythenorth.co.uk>
2018-12-26 11:08:30 +0000

    Change: sprites complete for Moor Gallop

--- src/vehicles/moor_gallop.py
+++ src/vehicles/moor_gallop.py
@@ -9,7 +9,8 @@ consist = EngineConsist(id='moor_gallop',
                         random_reverse=True,
                         gen=3,
                         pantograph_type='diamond-double',
-                        intro_date_offset=5)  # introduce later than gen epoch by design
+                        intro_date_offset=5,  # introduce later than gen epoch by design
+                        sprites_complete=True)
 
 consist.add_unit(type=ElectricEngineUnit,
                  weight=105,
