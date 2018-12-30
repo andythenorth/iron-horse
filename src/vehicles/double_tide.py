f3db6ee86afaefef9e60b7b78a98dc24cccb1e14
andythenorth <mail@andythenorth.co.uk>
2018-12-30 11:26:27 +0000

    Change: make Double Tide pure electric engine, high HP electro-diesels just don't do anything for gameplay

--- src/vehicles/double_tide.py
+++ src/vehicles/double_tide.py
@@ -1,11 +1,10 @@
-from train import EngineConsist, ElectroDieselEngineUnit
+from train import EngineConsist, ElectricEngineUnit
 
 consist = EngineConsist(id='double_tide',
                         base_numeric_id=2960,
                         name='Double Tide',
                         role='heavy_freight_2',
-                        power=2500,
-                        power_by_railtype={'RAIL': 2500, 'ELRL': 8500},
+                        power=8500,
                         # dibble for game balance, assume super-slip control
                         tractive_effort_coefficient=0.4,
                         random_reverse=True,
@@ -15,7 +14,7 @@ consist = EngineConsist(id='double_tide',
                         intro_date_offset=7,  # introduce later than gen epoch by design
                         sprites_complete=False)
 
-consist.add_unit(type=ElectroDieselEngineUnit,
+consist.add_unit(type=ElectricEngineUnit,
                  weight=132,
                  vehicle_length=8,
                  spriterow_num=0)
