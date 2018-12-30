fc0e0d2b6fd4140b85bf673f5c9d7f88c449f5d1
andythenorth <mail@andythenorth.co.uk>
2018-12-30 11:02:15 +0000

    Change: restore previous Super Shoebox and Ultra Shoebox power settings, reflecting that Shredder probably *will* get replaced now in gen 5, maybe 6

--- src/vehicles/super_shoebox.py
+++ src/vehicles/super_shoebox.py
@@ -4,8 +4,8 @@ consist = EngineConsist(id='super_shoebox',
                         base_numeric_id=880,
                         name='Super Shoebox',
                         role='branch_express',
-                        power=1250,  # double the previous progression, to support higher speed and offer replacement for Shredder
-                        power_by_railtype={'RAIL': 1250, 'ELRL': 2200},
+                        power=1200,  # slight increase on the previous progression, to support higher speed
+                        power_by_railtype={'RAIL': 1200, 'ELRL': 2200},
                         random_reverse=True,
                         pantograph_type='z-shaped-single',
                         gen=5,
