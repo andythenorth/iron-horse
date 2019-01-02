99aa994a41e6562af5612addcfb17d5f9dfcc2b9
andythenorth <mail@andythenorth.co.uk>
2019-01-02 12:59:51 +0000

    Change: restore Express Cars

--- src/iron_horse.py
+++ src/iron_horse.py
@@ -52,11 +52,8 @@ dump_cars.main()
 from vehicles import edibles_tank_cars
 edibles_tank_cars.main()
 
-"""
-# commented out for 2.0 alpha - may not be a good idea at all?
 from vehicles import express_cars
 express_cars.main()
-"""
 
 from vehicles import flat_cars
 flat_cars.main()
