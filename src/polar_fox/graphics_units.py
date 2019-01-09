24c4f8c6e1f797b889b11c28e4d74c0cb5dd33a6
andythenorth <mail@andythenorth.co.uk>
2019-01-09 09:57:37 +0000

    Codechange: updated Polar Fox

--- src/polar_fox/graphics_units.py
+++ src/polar_fox/graphics_units.py
@@ -16,6 +16,12 @@ Don't make changes here, make them in the Polar Fox project and redistribute.
 Any changes made here are liable to be over-written.
 """
 
+"""
+This file is generated from the Polar Fox project.
+Don't make changes here, make them in the Polar Fox project and redistribute.
+Any changes made here are liable to be over-written.
+"""
+
 import os
 from PIL import Image, ImageDraw, ImageFont
 
