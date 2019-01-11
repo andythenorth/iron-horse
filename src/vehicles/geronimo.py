b5602717f777dd9c6b18563a183eb60f090aad18
andythenorth <mail@andythenorth.co.uk>
2019-01-11 21:22:21 +0000

    Change: try Geronimo as single-car unit to support livery flip

--- src/vehicles/geronimo.py
+++ src/vehicles/geronimo.py
@@ -4,15 +4,14 @@ consist = PassengerEngineRailcarConsist(id='geronimo',
                                         base_numeric_id=2140,
                                         name='Geronimo',
                                         role='pax_railcar',
-                                        power=1500, # RL EMU HP is much lower, but eh
+                                        power=500, # RL EMU HP is much lower, but eh
                                         pantograph_type='z-shaped-single',
                                         gen=4,
                                         sprites_complete=False,
                                         intro_date_offset=-5)  # introduce early by design
 
 consist.add_unit(type=ElectricRailcarUnit,
-                 weight=37,
+                 weight=40,
                  vehicle_length=8,
                  capacity=40,
-                 chassis='railcar',
-                 repeat=3)
+                 chassis='railcar')
