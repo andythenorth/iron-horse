c929e34fbfc874215d355adf6e1c7ac0e3eefd87
andythenorth <mail@andythenorth.co.uk>
2019-01-14 07:39:25 +0000

    Change: (WIP) stub in a gen 4C pax car as a test

--- src/vehicles/passenger_cars.py
+++ src/vehicles/passenger_cars.py
@@ -79,13 +79,24 @@ def main():
     consist = PassengerCarConsist(roster='pony',
                                   base_numeric_id=3110,
                                   gen=4,
-                                  subtype='U',
+                                  subtype='B',
                                   sprites_complete=True)
 
     consist.add_unit(type=PaxCar,
                      capacity=40,
                      vehicle_length=6,
                      chassis='4_axle_solid_express_24px')
+
+    consist = PassengerCarConsist(roster='pony',
+                                  base_numeric_id=3260,
+                                  gen=4,
+                                  subtype='C',
+                                  sprites_complete=False)
+
+    consist.add_unit(type=PaxCar,
+                     capacity=40,
+                     vehicle_length=8,
+                     chassis='4_axle_solid_express_32px')
 
     consist = PassengerCarConsist(roster='pony',
                                   base_numeric_id=3100,
