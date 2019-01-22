1d4a3129ee1201b30a5661d7b07c9b8e7fb25950
andythenorth <mail@andythenorth.co.uk>
2019-01-22 22:22:32 +0000

    Change: sprites complete for Helm Wind middle cars

--- src/vehicles/helm_wind_middle.py
+++ src/vehicles/helm_wind_middle.py
@@ -10,7 +10,8 @@ def main(roster):
                                                         power=0,  # set power 0, when attached to correct cab, cab power will be increased
                                                         # no pantographs for Helm Wind middle cars
                                                         gen=5,
-                                                        intro_date_offset=-3)  # introduce earlier than gen epoch by design
+                                                        intro_date_offset=-3,  # introduce earlier than gen epoch by design
+                                                        sprites_complete=True)
 
     consist.add_unit(type=ElectricHighSpeedPaxUnit,
                      weight=31,
