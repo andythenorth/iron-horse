70f068f8fa1ab4c5f70e94b27ef8db72f95bf7d8
andythenorth <mail@andythenorth.co.uk>
2019-02-11 11:37:37 +0000

    Change: door sprites improved for Tin Rocket, Breeze

--- src/vehicles/breeze.py
+++ src/vehicles/breeze.py
@@ -11,7 +11,7 @@ def main(roster):
                                             pantograph_type='z-shaped-single-with-base',
                                             easter_egg_haulage_speed_bonus=True,
                                             gen=5,
-                                            sprites_complete=False,
+                                            sprites_complete=True,
                                             intro_date_offset=-3)  # introduce early by design
 
     consist.add_unit(type=ElectricRailcarPaxUnit,
