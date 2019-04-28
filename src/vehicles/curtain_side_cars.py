821175e8127f9d4687af27054df04268d3a87c22
andythenorth <mail@andythenorth.co.uk>
2019-04-28 22:52:41 +0100

    Change: stub in curtain side vans, as an alternative to box vans

---
+++ src/vehicles/curtain_side_cars.py
@@ -0,0 +1,61 @@
+from train import CurtainSideCarConsist, FreightCar
+
+
+def main():
+    #--------------- pony ----------------------------------------------------------------------
+
+    # gen 4 start, only B and C lengths
+
+    consist = CurtainSideCarConsist(roster='pony',
+                            base_numeric_id=320,
+                            gen=4,
+                            subtype='B',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_filled_24px')
+
+    consist = CurtainSideCarConsist(roster='pony',
+                            base_numeric_id=330,
+                            gen=4,
+                            subtype='C',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_1cc_filled_32px')
+
+    consist = CurtainSideCarConsist(roster='pony',
+                            base_numeric_id=340,
+                            gen=5,
+                            subtype='B',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_filled_24px')
+
+    consist = CurtainSideCarConsist(roster='pony',
+                            base_numeric_id=350,
+                            gen=5,
+                            subtype='C',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_1cc_filled_32px')
+
+    consist = CurtainSideCarConsist(roster='pony',
+                            base_numeric_id=370,
+                            gen=6,
+                            subtype='B',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_filled_24px')
+
+    consist = CurtainSideCarConsist(roster='pony',
+                            base_numeric_id=400,
+                            gen=6,
+                            subtype='C',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_1cc_filled_32px')
