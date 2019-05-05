8af94b3fd3a652c068abb7f0085deb9b1e07c423
andythenorth <mail@andythenorth.co.uk>
2019-05-05 08:11:45 +0100

    Feature: Sliding Wall Cars complete

--- src/vehicles/sliding_wall_cars.py
+++ src/vehicles/sliding_wall_cars.py
@@ -10,7 +10,7 @@ def main():
                             base_numeric_id=410,
                             gen=5,
                             subtype='B',
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_1cc_filled_24px')
@@ -19,7 +19,7 @@ def main():
                             base_numeric_id=440,
                             gen=5,
                             subtype='C',
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_1cc_filled_32px')
@@ -29,7 +29,7 @@ def main():
                             base_numeric_id=330,
                             gen=6,
                             subtype='B',
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_1cc_filled_24px')
@@ -39,7 +39,7 @@ def main():
                             base_numeric_id=320,
                             gen=6,
                             subtype='C',
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_1cc_filled_32px')
