ccbbace069aa91a4ad4dc374b9522dbad79f5412
andythenorth <mail@andythenorth.co.uk>
2019-05-06 10:28:49 +0100

    Change: WIP on curtain side box cars

--- src/vehicles/curtain_side_box_cars.py
+++ src/vehicles/curtain_side_box_cars.py
@@ -50,7 +50,7 @@ def main():
                             sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='2_axle_filled_24px')
+                     chassis='2_axle_2cc_filled_24px')
 
 
     consist = CurtainSideCarBoxConsist(roster='pony',
