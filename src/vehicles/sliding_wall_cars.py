f6d9980aa767677f65f37dc79f381534c4c5aed9
andythenorth <mail@andythenorth.co.uk>
2019-05-06 10:11:25 +0100

    Change: add gen 4 sliding wall car (WIP)

--- src/vehicles/sliding_wall_cars.py
+++ src/vehicles/sliding_wall_cars.py
@@ -4,7 +4,17 @@ from train import SlidingWallCarConsist, FreightCar
 def main():
     #--------------- pony ----------------------------------------------------------------------
 
-    # gen 5 start, only B and C lengths
+    # gen 4 start, only C length (offbeat eh?), then gen 5 and 6 have B and C lengths
+
+    consist = SlidingWallCarConsist(roster='pony',
+                            base_numeric_id=1790,
+                            gen=4,
+                            subtype='C',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_1cc_filled_32px')
+
 
     consist = SlidingWallCarConsist(roster='pony',
                             base_numeric_id=410,
@@ -14,6 +24,7 @@ def main():
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_1cc_filled_24px')
+
 
     consist = SlidingWallCarConsist(roster='pony',
                             base_numeric_id=440,
