ab401947971a862911acbd029cf4dfb7fd123bc6
andythenorth <mail@andythenorth.co.uk>
2019-05-08 08:29:43 +0100

    Change: add more coil cars

--- src/vehicles/coil_cars.py
+++ src/vehicles/coil_cars.py
@@ -5,6 +5,50 @@ def main():
     #--------------- pony ----------------------------------------------------------------------
 
     consist = CoilCarConsist(roster='pony',
+                             base_numeric_id=2030,
+                             gen=4,
+                             subtype='B',
+                             sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     cargo_length=8,
+                     chassis='4_axle_sparse_24px')
+
+
+    consist = CoilCarConsist(roster='pony',
+                             base_numeric_id=2040,
+                             gen=4,
+                             subtype='C',
+                             sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     cargo_length=8,
+                     chassis='4_axle_sparse_32px')
+
+
+    consist = CoilCarConsist(roster='pony',
+                             base_numeric_id=2010,
+                             gen=5,
+                             subtype='B',
+                             sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     cargo_length=8,
+                     chassis='4_axle_sparse_24px')
+
+
+    consist = CoilCarConsist(roster='pony',
+                             base_numeric_id=2020,
+                             gen=5,
+                             subtype='C',
+                             sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     cargo_length=8,
+                     chassis='4_axle_sparse_32px')
+
+
+    consist = CoilCarConsist(roster='pony',
                              base_numeric_id=1990,
                              gen=6,
                              subtype='B',
