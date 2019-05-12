55d19e3158e4a9edb48ae56a3389d05357a5f9f2
andythenorth <mail@andythenorth.co.uk>
2019-05-12 07:50:07 +0100

    Change: remove gen 4C sliding wall box car, doesn't look right

--- src/vehicles/sliding_wall_cars.py
+++ src/vehicles/sliding_wall_cars.py
@@ -4,17 +4,9 @@ from train import SlidingWallCarConsist, FreightCar
 def main():
     #--------------- pony ----------------------------------------------------------------------
 
-    # gen 4 start, only C length (offbeat eh?), then gen 5 and 6 have B and C lengths
+    # only gen 5 and 6, B and C lengths
 
-    consist = SlidingWallCarConsist(roster='pony',
-                            base_numeric_id=1790,
-                            gen=4,
-                            subtype='C',
-                            sprites_complete=False)
-
-    consist.add_unit(type=FreightCar,
-                     chassis='4_axle_1cc_filled_32px')
-
+    # a gen 4C was tried and removed, didn't look right
 
     consist = SlidingWallCarConsist(roster='pony',
                             base_numeric_id=410,
