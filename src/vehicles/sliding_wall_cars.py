b126961d02760ecc33af1babc79a41e88c511808
andythenorth <mail@andythenorth.co.uk>
2019-05-12 14:13:49 +0100

    Change: restore gen 4C sliding wall car, and adjust appearance of gen 5 and 6 sliding wall cars

--- src/vehicles/sliding_wall_cars.py
+++ src/vehicles/sliding_wall_cars.py
@@ -4,9 +4,17 @@ from train import SlidingWallCarConsist, FreightCar
 def main():
     #--------------- pony ----------------------------------------------------------------------
 
-    # only gen 5 and 6, B and C lengths
+    # starts with gen 4C only, then gen 5 and 6, B and C lengths
 
-    # a gen 4C was tried and removed, didn't look right
+    consist = SlidingWallCarConsist(roster='pony',
+                            base_numeric_id=1790,
+                            gen=4,
+                            subtype='C',
+                            sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_1cc_filled_32px')
+
 
     consist = SlidingWallCarConsist(roster='pony',
                             base_numeric_id=410,
