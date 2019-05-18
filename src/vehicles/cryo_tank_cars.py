5a997b869f8dfad0726a0173d8e4a9cf4be60cf4
andythenorth <mail@andythenorth.co.uk>
2019-05-18 07:31:00 +0100

    Change: sprites complete for cryo tank cars

--- src/vehicles/cryo_tank_cars.py
+++ src/vehicles/cryo_tank_cars.py
@@ -16,7 +16,7 @@ def main():
                              base_numeric_id=250,
                              gen=3,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_24px')
@@ -25,7 +25,7 @@ def main():
                              base_numeric_id=20,
                              gen=4,
                              subtype='A',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_sparse_16px')
@@ -34,7 +34,7 @@ def main():
                              base_numeric_id=50,
                              gen=4,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_24px')
@@ -43,7 +43,7 @@ def main():
                              base_numeric_id=60,
                              gen=4,
                              subtype='C',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_32px')
@@ -52,7 +52,7 @@ def main():
                              base_numeric_id=80,
                              gen=5,
                              subtype='A',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_sparse_16px')
@@ -61,7 +61,7 @@ def main():
                              base_numeric_id=90,
                              gen=5,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_24px')
@@ -70,7 +70,7 @@ def main():
                              base_numeric_id=150,
                              gen=5,
                              subtype='C',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_32px')
@@ -92,7 +92,7 @@ def main():
                              base_numeric_id=190,
                              gen=6,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_24px')
@@ -101,7 +101,7 @@ def main():
                              base_numeric_id=200,
                              gen=6,
                              subtype='C',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_32px')
