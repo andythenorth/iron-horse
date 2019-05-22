7015623576beb145fd5b6993f94e293afb249111
andythenorth <mail@andythenorth.co.uk>
2019-05-22 19:59:56 +0100

    Change: faff with aggregate hoppers

--- src/iron_horse.py
+++ src/iron_horse.py
@@ -43,11 +43,9 @@ from vehicles import alignment_cars
 alignment_cars.main()
 """
 
-"""
 # commented out for 2.0 alpha
 from vehicles import aggregate_hopper_cars
 aggregate_hopper_cars.main()
-"""
 
 from vehicles import box_cars
 box_cars.main()
--- src/vehicles/aggregate_hopper_cars.py
+++ src/vehicles/aggregate_hopper_cars.py
@@ -12,7 +12,7 @@ def main():
                                sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='2_axle_sparse_16px')
+                     chassis='2_axle_gapped_16px')
 
 
     consist = AggregateHopperCarConsist(roster='pony',
@@ -23,4 +23,15 @@ def main():
                                sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_sparse_24px')
+                     chassis='4_axle_gapped_24px')
+
+
+    consist = AggregateHopperCarConsist(roster='pony',
+                               base_numeric_id=1620,
+                               gen=5,
+                               subtype='C',
+                               intro_date_offset=5,  # let's be a little bit later for this one
+                               sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_gapped_32px')
