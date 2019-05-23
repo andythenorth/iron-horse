64e0c3a9f37c5b156eb038f867a802bf31eb3a41
andythenorth <mail@andythenorth.co.uk>
2019-05-23 23:13:01 +0100

    Change: WIP on  aggregate hoppers

--- src/vehicles/aggregate_hopper_cars.py
+++ src/vehicles/aggregate_hopper_cars.py
@@ -5,6 +5,39 @@ def main():
     #--------------- pony ----------------------------------------------------------------------
 
     consist = AggregateHopperCarConsist(roster='pony',
+                               base_numeric_id=1080,
+                               gen=4,
+                               subtype='A',
+                               intro_date_offset=5,  # let's be a little bit later for this one
+                               sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_gapped_16px')
+
+
+    consist = AggregateHopperCarConsist(roster='pony',
+                               base_numeric_id=2000,
+                               gen=4,
+                               subtype='B',
+                               intro_date_offset=5,  # let's be a little bit later for this one
+                               sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_gapped_24px')
+
+
+    consist = AggregateHopperCarConsist(roster='pony',
+                               base_numeric_id=2010,
+                               gen=4,
+                               subtype='C',
+                               intro_date_offset=5,  # let's be a little bit later for this one
+                               sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_gapped_32px')
+
+
+    consist = AggregateHopperCarConsist(roster='pony',
                                base_numeric_id=1610,
                                gen=5,
                                subtype='A',
@@ -35,6 +68,16 @@ def main():
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_gapped_32px')
+
+
+    consist = AggregateHopperCarConsist(roster='pony',
+                               base_numeric_id=2020,
+                               gen=6,
+                               subtype='B',
+                               sprites_complete=False)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_sparse_24px')
 
 
     consist = AggregateHopperCarConsist(roster='pony',
