30691736c2ab35d60ca8588d3d81ba965a15b397
andythenorth <mail@andythenorth.co.uk>
2019-05-25 10:57:54 +0100

    Change: add gen 2 and 3 coal hopper cars

--- src/vehicles/coal_hopper_cars.py
+++ src/vehicles/coal_hopper_cars.py
@@ -5,7 +5,27 @@ def main():
     #--------------- pony ----------------------------------------------------------------------
 
     # no gen 1 hoppers in Pony eh
-    # also just type A for gen 2
+    # also just type A for gen 2 and 3
+
+    consist = CoalHopperCarConsist(roster='pony',
+                               base_numeric_id=2030,
+                               gen=2,
+                               subtype='A',
+                               sprites_complete=True)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_gapped_16px')
+
+
+    consist = CoalHopperCarConsist(roster='pony',
+                               base_numeric_id=2040,
+                               gen=3,
+                               subtype='A',
+                               sprites_complete=True)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_gapped_16px')
+
 
     consist = CoalHopperCarConsist(roster='pony',
                                base_numeric_id=2320,
@@ -31,7 +51,7 @@ def main():
                                base_numeric_id=1090,
                                gen=5,
                                subtype='B',
-                               sprites_complete=True)
+                               sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_24px')
@@ -41,7 +61,7 @@ def main():
                                base_numeric_id=2780,
                                gen=5,
                                subtype='C',
-                               sprites_complete=True)
+                               sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_32px')
@@ -51,7 +71,7 @@ def main():
                                base_numeric_id=3010,
                                gen=6,
                                subtype='B',
-                               sprites_complete=True)
+                               sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_24px')
@@ -61,7 +81,7 @@ def main():
                                base_numeric_id=3020,
                                gen=6,
                                subtype='C',
-                               sprites_complete=True)
+                               sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_32px')
