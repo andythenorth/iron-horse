be546f7537f6b82b0455633df8572e880c8ebdb6
andythenorth <mail@andythenorth.co.uk>
2019-05-28 20:44:36 +0100

    Change: add gen 2A and 3A grain hoppers

--- src/vehicles/grain_hopper_cars.py
+++ src/vehicles/grain_hopper_cars.py
@@ -3,9 +3,8 @@ from train import GrainHopperCarConsist, FreightCar
 
 def main():
     #--------------- pony ----------------------------------------------------------------------
-    """
     consist = GrainHopperCarConsist(roster='pony',
-                                      base_numeric_id=1270,
+                                      base_numeric_id=2850,
                                       gen=2,
                                       subtype='A',
                                       sprites_complete=False)
@@ -15,7 +14,7 @@ def main():
 
 
     consist = GrainHopperCarConsist(roster='pony',
-                                      base_numeric_id=1260,
+                                      base_numeric_id=2870,
                                       gen=3,
                                       subtype='A',
                                       sprites_complete=False)
@@ -23,7 +22,7 @@ def main():
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
 
-    """
+
     consist = GrainHopperCarConsist(roster='pony',
                                       base_numeric_id=2080,
                                       gen=4,
@@ -44,17 +43,6 @@ def main():
                      chassis='2_axle_gapped_24px')
 
 
-    """
-    consist = GrainHopperCarConsist(roster='pony',
-                                      base_numeric_id=2070,
-                                      gen=5,
-                                      subtype='A',
-                                      sprites_complete=True)
-
-    consist.add_unit(type=FreightCar,
-                     chassis='2_axle_filled_16px')
-
-    """
     consist = GrainHopperCarConsist(roster='pony',
                                       base_numeric_id=2800,
                                       gen=5,
