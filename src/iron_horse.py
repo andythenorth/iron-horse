a0ee93aa6be11cf5473dbff2a62491a553490b81
andythenorth <mail@andythenorth.co.uk>
2019-06-12 06:40:18 +0100

    Codechange: more accurate calculation of max_id when calculating vacant numeric IDs

--- src/iron_horse.py
+++ src/iron_horse.py
@@ -188,10 +188,10 @@ def vacant_numeric_ids_formatted():
     # when adding vehicles it's useful to know what the next free numeric ID is
     # tidy-mind problem, but do we have any vacant numeric ID slots in the currently used range?
     # 'print' eh? - but it's fine echo_message isn't intended for this kind of info, don't bother changing
-    max_id = max(numeric_id_defender)
+    max_id = max(numeric_id_defender) -  (max(numeric_id_defender) % 10)
     id_gaps = []
     for i in range(0, int(max_id/10)):
         id = i * 10
         if id not in numeric_id_defender:
             id_gaps.append(str(id))
-    return "Vacant numeric ID slots: " + ', '.join(id_gaps) + (" and from " if len(id_gaps) > 0 else '') + str(max(numeric_id_defender) + 10) + " onwards"
+    return "Vacant numeric ID slots: " + ', '.join(id_gaps) + (" and from " if len(id_gaps) > 0 else '') + str(max_id + 10) + " onwards"
