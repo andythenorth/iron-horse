ed98a56312db44baf4c81f0f70933a0573fa15ba
andythenorth <mail@andythenorth.co.uk>
2019-07-05 23:40:51 +0100

    Codechange: gen 2 and 3 covered hopper sprites are not in fact complete

--- src/vehicles/covered_hopper_cars.py
+++ src/vehicles/covered_hopper_cars.py
@@ -7,7 +7,7 @@ def main():
                                       base_numeric_id=1270,
                                       gen=2,
                                       subtype='A',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
@@ -17,7 +17,7 @@ def main():
                                       base_numeric_id=1260,
                                       gen=3,
                                       subtype='A',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
