febc455bd04ddb6797d5ee24ec2fdcfe60557d55
andythenorth <mail@andythenorth.co.uk>
2019-07-11 19:26:39 +0100

    Change: sprites complete for gen 5 chemicals tank cars

--- src/vehicles/chemicals_tank_cars.py
+++ src/vehicles/chemicals_tank_cars.py
@@ -78,7 +78,7 @@ def main():
                              base_numeric_id=3410,
                              gen=5,
                              subtype='A',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_sparse_16px')
@@ -88,7 +88,7 @@ def main():
                              base_numeric_id=3420,
                              gen=5,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_24px')
@@ -98,7 +98,7 @@ def main():
                              base_numeric_id=3430,
                              gen=5,
                              subtype='C',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_32px')
