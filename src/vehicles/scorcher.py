654e07b800a8976c3af65e8862f7ee0894fc4b2b
andythenorth <mail@andythenorth.co.uk>
2019-07-14 15:35:03 +0100

    Change: sprites complete for Scorcher, also fix some errors with Blaze

--- src/vehicles/scorcher.py
+++ src/vehicles/scorcher.py
@@ -12,7 +12,7 @@ def main(roster):
                             dual_headed=True,
                             intro_date_offset=-10,  # let's be a little bit earlier for this one - keep match to HST coaches
                             gen=6,
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
                      weight=70,
