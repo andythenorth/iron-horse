7f57a7d76d184d6c46ea70ebdd0d372fc5e67d26
andythenorth <mail@andythenorth.co.uk>
2019-07-19 18:22:19 +0100

    Change: sprites complete for Flindermouse

--- src/vehicles/flindermouse.py
+++ src/vehicles/flindermouse.py
@@ -11,7 +11,8 @@ def main(roster):
                             joker=True,  # this engine doesn't fit the set roster pattern, by design it's to mix things up
                             gen=3,
                             pantograph_type='diamond-double',
-                            intro_date_offset=-23)  # introduce earlier than gen epoch by design
+                            intro_date_offset=-23,  # introduce earlier than gen epoch by design
+                            sprites_complete=True)
 
     consist.add_unit(type=ElectricEngineUnit,
                      weight=65,
