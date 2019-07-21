5438a62147925eecdf7855db082687fa6c799cd8
andythenorth <mail@andythenorth.co.uk>
2019-07-21 21:56:03 +0100

    Change: reduce Merrylegs run costs to improve profitability following testing

--- src/vehicles/merrylegs.py
+++ src/vehicles/merrylegs.py
@@ -9,6 +9,7 @@ def main(roster):
                             role='branch_express_1',
                             power=650,
                             tractive_effort_coefficient=0.2,
+                            fixed_run_cost_points=120, # substantial cost bonus so it can make money
                             random_reverse=True,
                             gen=2)
 
