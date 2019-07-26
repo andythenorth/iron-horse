a45dd690b17c77631101fac6a36dc1ec4232d019
andythenorth <mail@andythenorth.co.uk>
2019-07-26 12:21:31 +0100

    Fix: drop train_flag_mu from Very High Speed trains, as the middle units don't pick it up and won't show the correct EMU livery

--- src/train.py
+++ src/train.py
@@ -684,7 +684,8 @@ class PassengerVeryHighSpeedCabEngineConsist(PassengerEngineConsist):
         # ...and very high fixed (baseline) run costs on this subtype
         self.fixed_run_cost_points = 256
         # train_flag_mu solely used for ottd livery (company colour) selection
-        self.train_flag_mu = True
+        # !! commented out as of July 2019 because the middle engines won't pick this up, which causes inconsistency in the buy menu
+        # self.train_flag_mu = True
         """
         # !! this type needs new graphics processing and/or template rules if it is to handle opening doors
         # !! box car variant expects symmetry
@@ -717,7 +718,8 @@ class PassengerVeryHighSpeedMiddleEngineConsist(PassengerEngineConsist):
         # eh as of Feb 2019, OpenTTD won't actually use this for middle cars, as not engines
         # this means the buy menu won't match, but wagons will match anyway when attached to cab
         # prop left in place in case that ever gets changed :P
-        self.train_flag_mu = True
+        # !! commented out as of July 2019 because the middle engines won't pick this up, which causes inconsistency in the buy menu
+        # self.train_flag_mu = True
         # Graphics configuration
         self.roof_type = 'pax_mail_smooth'
         # 1 livery as can't be flipped, 1 spriterow may be left blank for compatibility with Gestalt (TBC)
