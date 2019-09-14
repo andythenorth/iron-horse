601523ba041609103fae4822527769a93acba74a
andythenorth <mail@andythenorth.co.uk>
2019-09-14 18:25:08 +0100

    Change: redrawn gen 4, gen 5, gen 6 dump cars to all be low height

--- src/vehicles/dump_cars.py
+++ src/vehicles/dump_cars.py
@@ -49,7 +49,7 @@ def main():
                              base_numeric_id=2370,
                              gen=4,
                              subtype='A',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
@@ -59,7 +59,7 @@ def main():
                              base_numeric_id=2380,
                              gen=4,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_sparse_24px')
@@ -69,57 +69,57 @@ def main():
                              base_numeric_id=1340,
                              gen=5,
                              subtype='A',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='2_axle_gapped_16px')
+                     chassis='2_axle_gapped_greebled_16px')
 
 
     consist = DumpCarConsist(roster='pony',
                              base_numeric_id=1810,
                              gen=5,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_sparse_24px')
+                     chassis='2_axle_gapped_greebled_24px')
 
 
     consist = DumpCarConsist(roster='pony',
                              base_numeric_id=2110,
                              gen=5,
                              subtype='C',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_sparse_32px')
+                     chassis='4_axle_gapped_greebled_32px')
 
 
     consist = DumpCarConsist(roster='pony',
                              base_numeric_id=1940,
                              gen=6,
                              subtype='A',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='2_axle_gapped_16px')
+                     chassis='2_axle_gapped_greebled_16px')
 
 
     consist = DumpCarConsist(roster='pony',
                              base_numeric_id=2400,
                              gen=6,
                              subtype='B',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_sparse_24px')
+                     chassis='2_axle_gapped_greebled_24px')
 
 
     consist = DumpCarConsist(roster='pony',
                              base_numeric_id=2390,
                              gen=6,
                              subtype='C',
-                             sprites_complete=False)
+                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_sparse_32px')
+                     chassis='4_axle_gapped_greebled_32px')
