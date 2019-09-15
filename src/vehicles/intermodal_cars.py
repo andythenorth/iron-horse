4a93ff0c519733eec011be675a41ecae77f48ab7
andythenorth <mail@andythenorth.co.uk>
2019-09-15 08:15:51 +0100

    Change: provide gen 4 and 5 intermodal cars

--- src/vehicles/intermodal_cars.py
+++ src/vehicles/intermodal_cars.py
@@ -3,42 +3,65 @@ from train import IntermodalCarConsist, FreightCar
 
 def main():
     #--------------- pony ----------------------------------------------------------------------
-    """
+    consist = IntermodalCarConsist(roster='pony',
+                                   base_numeric_id=3880,
+                                   gen=4,
+                                   subtype='A',
+                                   sprites_complete=True)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='2_axle_filled_16px')
+
+
+    consist = IntermodalCarConsist(roster='pony',
+                                   base_numeric_id=3890,
+                                   gen=4,
+                                   subtype='B',
+                                   sprites_complete=True)
+
+    consist.add_unit(type=FreightCar,
+                     chassis='4_axle_gapped_24px')
+
+
     consist = IntermodalCarConsist(roster='pony',
                                    base_numeric_id=3710,
                                    gen=4,
-                                   subtype='B')
+                                   subtype='C',
+                                   sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     vehicle_length=6)
+                     chassis='4_axle_gapped_32px')
 
 
     consist = IntermodalCarConsist(roster='pony',
                                    base_numeric_id=3720,
-                                   gen=4,
-                                   subtype='C')
+                                   gen=5,
+                                   subtype='A',
+                                   sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     vehicle_length=8)
+                     chassis='2_axle_filled_greebled_16px')
 
 
     consist = IntermodalCarConsist(roster='pony',
                                    base_numeric_id=3730,
                                    gen=5,
-                                   subtype='B')
+                                   subtype='B',
+                                   sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     vehicle_length=6)
+                     chassis='4_axle_filled_greebled_24px')
 
 
     consist = IntermodalCarConsist(roster='pony',
                                    base_numeric_id=3740,
                                    gen=5,
-                                   subtype='C')
+                                   subtype='C',
+                                   sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     vehicle_length=8)
-    """
+                     chassis='4_axle_filled_greebled_32px')
+
 
     consist = IntermodalCarConsist(roster='pony',
                                    base_numeric_id=3770,
