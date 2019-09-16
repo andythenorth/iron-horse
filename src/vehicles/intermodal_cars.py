574e94d0f8daaac6e9dc6a13fe116fb1fa0968f8
andythenorth <mail@andythenorth.co.uk>
2019-09-16 08:53:34 +0100

    Codechange: mark sprites complete for NG intermodal car

--- src/vehicles/intermodal_cars.py
+++ src/vehicles/intermodal_cars.py
@@ -8,7 +8,7 @@ def main():
                                    gen=4,
                                    subtype='U',
                                    base_track_type='NG',
-                                   sprites_complete=False)
+                                   sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_ng_16px')
