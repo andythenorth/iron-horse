4ccae6cc6ead1596a3426cca50761edb1645d9e7
Andy <andy@teamrubber.com>
2019-12-19 06:51:54 +0000

    Codechange: refactor filenames and comments for procedures

--- src/render_nml.py
+++ src/render_nml.py
@@ -63,7 +63,8 @@ def main():
 
     consists = iron_horse.get_consists_in_buy_menu_order()
 
-    header_items = ['header', 'cargo_table', 'railtype_table', 'spriteset_templates', 'tail_lights', 'haulage_bonus', 'visible_cargo', 'intermodal_containers']
+    header_items = ['header', 'cargo_table', 'railtype_table', 'spriteset_templates', 'tail_lights',
+                    'procedures_haulage_bonus', 'procedures_visible_cargo', 'intermodal_containers']
     for header_item in header_items:
         grf_nml.write(render_header_item_nml(header_item, consists))
 
