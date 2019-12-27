d8ec1634ea16163fed4f3d0b4b6a493646278f2e
Andy <andy@teamrubber.com>
2019-12-27 22:57:46 +0000

    Change: WIP on carbon black hoppers

--- src/vehicles/carbon_black_hopper_cars.py
+++ src/vehicles/carbon_black_hopper_cars.py
@@ -3,21 +3,12 @@ from train import CarbonBlackHopperCarConsist, FreightCar
 
 def main():
     #--------------- pony ----------------------------------------------------------------------
-    consist = CarbonBlackHopperCarConsist(roster_id='pony',
-                                      base_numeric_id=4400,
-                                      gen=2,
-                                      subtype='A',
-                                      sprites_complete=True)
-
-    consist.add_unit(type=FreightCar,
-                     chassis='2_axle_gapped_16px')
-
 
     consist = CarbonBlackHopperCarConsist(roster_id='pony',
                                       base_numeric_id=4410,
                                       gen=3,
                                       subtype='A',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
@@ -27,7 +18,7 @@ def main():
                                       base_numeric_id=4420,
                                       gen=4,
                                       subtype='A',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_sparse_16px')
@@ -37,70 +28,50 @@ def main():
                                       base_numeric_id=4430,
                                       gen=4,
                                       subtype='B',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_24px')
 
 
     consist = CarbonBlackHopperCarConsist(roster_id='pony',
-                                      base_numeric_id=4440,
-                                      gen=5,
-                                      subtype='A',
-                                      sprites_complete=True)
-
-    consist.add_unit(type=FreightCar,
-                     chassis='2_axle_filled_16px')
-
-
-    consist = CarbonBlackHopperCarConsist(roster_id='pony',
                                       base_numeric_id=4450,
                                       gen=5,
                                       subtype='B',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_filled_greebled_24px')
+                     chassis='2_axle_gapped_greebled_24px')
 
 
     consist = CarbonBlackHopperCarConsist(roster_id='pony',
                                       base_numeric_id=4460,
                                       gen=5,
                                       subtype='C',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_filled_greebled_32px')
-
-
-    consist = CarbonBlackHopperCarConsist(roster_id='pony',
-                                      base_numeric_id=4470,
-                                      gen=6,
-                                      subtype='A',
-                                      sprites_complete=True)
-
-    consist.add_unit(type=FreightCar,
-                     chassis='2_axle_filled_16px')
+                     chassis='4_axle_gapped_greebled_32px')
 
 
     consist = CarbonBlackHopperCarConsist(roster_id='pony',
                                       base_numeric_id=4480,
                                       gen=6,
                                       subtype='B',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_filled_greebled_24px')
+                     chassis='2_axle_gapped_greebled_24px')
 
 
     consist = CarbonBlackHopperCarConsist(roster_id='pony',
                                       base_numeric_id=4490,
                                       gen=6,
                                       subtype='C',
-                                      sprites_complete=True)
+                                      sprites_complete=False)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_filled_greebled_32px')
+                     chassis='4_axle_gapped_greebled_32px')
 
 
     #--------------- llama ----------------------------------------------------------------------
