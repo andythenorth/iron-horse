96559d1095877d4218b03279b0fbe72a2f2c9031
Andy <andy@teamrubber.com>
2020-01-04 13:35:47 +0000

    Change: run cost malus for luxury railcars, prevent them being silly-cheap to run

--- src/train.py
+++ src/train.py
@@ -692,7 +692,10 @@ class PassengerEngineLuxuryRailcarConsist(PassengerEngineConsist):
         # this won't make much difference except over *very* long routes, but set it anyway
         self.cargo_age_period = 8 * global_constants.CARGO_AGE_PERIOD
         self.buy_cost_adjustment_factor = 1.3
-        self.floating_run_cost_multiplier = 16
+        # to avoid these railcars being super-bargain cheap, add a significant malus
+        self.floating_run_cost_multiplier = 24
+        # ...and high fixed (baseline) run costs on this subtype
+        self.fixed_run_cost_points = 180
 
         # Graphics configuration
         if self.gen in [2, 3]:
