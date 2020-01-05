bfe5c62dd670d349d9f59fc6422295d9992d687c
Andy <andy@teamrubber.com>
2020-01-05 15:38:46 +0000

    Change: tweak Firebird weight up a bit as it is OP anyway

--- src/vehicles/firebird.py
+++ src/vehicles/firebird.py
@@ -7,12 +7,12 @@ def main(roster_id):
                                            base_numeric_id=3830,
                                            name='Firebird',
                                            role='hst', # quite a specific role, may or may not scale to other rosters
-                                           power=3300, # it's the Deltic that never was!
+                                           power=3300, # it's the Deltic that never was!  It's OP, but eh, it's just cartoon trains.
                                            gen=4,
                                            sprites_complete=False)
 
     consist.add_unit(type=DieselEngineUnit,
-                     weight=65,
+                     weight=68,
                      vehicle_length=8,
                      capacity=16,
                      effect_offsets=[(0, 1), (0, -1)], # double the smoke eh?
