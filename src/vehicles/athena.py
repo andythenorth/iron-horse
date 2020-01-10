c13daf44ae357d08848e0486024ace2ea418493e
Andy <andy@teamrubber.com>
2020-01-10 21:54:00 +0000

    Change: reworked sprites for Athena

--- src/vehicles/athena.py
+++ src/vehicles/athena.py
@@ -11,7 +11,7 @@ def main(roster_id):
                                             pantograph_type='diamond-single-with-base',
                                             easter_egg_haulage_speed_bonus=True,
                                             gen=3,
-                                            sprites_complete=False,
+                                            sprites_complete=True,
                                             intro_date_offset=-3)  # introduce early by design
 
     consist.add_unit(type=ElectricRailcarPaxUnit,
