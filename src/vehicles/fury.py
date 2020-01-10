4ae463c873cbf0b37ad4dd1d7a859573780048b7
Andy <andy@teamrubber.com>
2020-01-10 21:31:39 +0000

    Change: revised sprites complete for Fury

--- src/vehicles/fury.py
+++ src/vehicles/fury.py
@@ -13,7 +13,7 @@ def main(roster_id):
                             replacement_consist_id='revolution', # this line ends with Fury and is merged to heavy_express_3
                             pantograph_type='z-shaped-double',
                             intro_date_offset=1,  # introduce later than gen epoch by design
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=ElectricEngineUnit,
                      weight=82,
