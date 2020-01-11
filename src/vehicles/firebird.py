8d3f483caa71eb9fdd6e96fefee94f1a8781aab7
Andy <andy@teamrubber.com>
2020-01-11 11:16:28 +0000

    Change: sprites complete for Firebird

--- src/vehicles/firebird.py
+++ src/vehicles/firebird.py
@@ -9,7 +9,7 @@ def main(roster_id):
                                            role='hst', # quite a specific role, may or may not scale to other rosters
                                            power=3300, # it's the Deltic that never was!  It's OP, but eh, it's just cartoon trains.
                                            gen=4,
-                                           sprites_complete=False)
+                                           sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
                      weight=68,
