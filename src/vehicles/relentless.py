6d3215edcaa9a01add6fb09e3d8252b21a9b8637
Andy <andy@teamrubber.com>
2020-01-11 16:55:47 +0000

    Change: sprites complete for Relentless

--- src/vehicles/relentless.py
+++ src/vehicles/relentless.py
@@ -12,7 +12,7 @@ def main(roster_id):
                             gen=6,
                             intro_date_offset=-7, # let's be earlier on this to keep the mail up with the HSTs etc
                             fixed_run_cost_points=600, # give a serious malus to this one (balancing eh?)
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
                      weight=95,
