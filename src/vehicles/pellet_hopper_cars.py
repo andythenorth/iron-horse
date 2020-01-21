c5c13eaea120c2202e88d7016d1cda868448b845
Andy <andy@teamrubber.com>
2020-01-21 22:13:35 +0000

    Change: sprites complete for pellet hoppers

--- src/vehicles/pellet_hopper_cars.py
+++ src/vehicles/pellet_hopper_cars.py
@@ -7,7 +7,7 @@ def main():
                                       base_numeric_id=4280,
                                       gen=2,
                                       subtype='A',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
@@ -17,7 +17,7 @@ def main():
                                       base_numeric_id=4300,
                                       gen=3,
                                       subtype='A',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
@@ -27,7 +27,7 @@ def main():
                                       base_numeric_id=4310,
                                       gen=4,
                                       subtype='A',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_16px')
@@ -37,7 +37,7 @@ def main():
                                       base_numeric_id=4330,
                                       gen=4,
                                       subtype='B',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_gapped_24px')
@@ -47,7 +47,7 @@ def main():
                                       base_numeric_id=4340,
                                       gen=5,
                                       subtype='B',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_filled_greebled_24px')
@@ -57,7 +57,7 @@ def main():
                                       base_numeric_id=4350,
                                       gen=5,
                                       subtype='C',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_greebled_32px')
@@ -67,7 +67,7 @@ def main():
                                       base_numeric_id=4360,
                                       gen=6,
                                       subtype='B',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_greebled_24px')
@@ -77,7 +77,7 @@ def main():
                                       base_numeric_id=4370,
                                       gen=6,
                                       subtype='C',
-                                      sprites_complete=False)
+                                      sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_greebled_32px')
