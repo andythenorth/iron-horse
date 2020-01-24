a40fa5adf3e5e797c1f4e08428e106c0b1e641f7
Andy <andy@teamrubber.com>
2020-01-24 08:26:40 +0000

    Change: sprites complete for gen 5 ore hoppers and gen 5 covered hoppers

--- src/vehicles/ore_hopper_cars.py
+++ src/vehicles/ore_hopper_cars.py
@@ -61,7 +61,7 @@ def main():
                                base_numeric_id=4380,
                                gen=5,
                                subtype='A',
-                               sprites_complete=False)
+                               sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='2_axle_filled_16px')
@@ -71,7 +71,7 @@ def main():
                                base_numeric_id=4140,
                                gen=5,
                                subtype='B',
-                               sprites_complete=False)
+                               sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_greebled_24px')
@@ -81,7 +81,7 @@ def main():
                                base_numeric_id=4150,
                                gen=5,
                                subtype='C',
-                               sprites_complete=False)
+                               sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
                      chassis='4_axle_filled_greebled_32px')
