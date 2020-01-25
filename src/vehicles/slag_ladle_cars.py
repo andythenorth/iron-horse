b8219169a91fad3a2c6f9c9c583e9f3ff5446684
Andy <andy@teamrubber.com>
2020-01-25 12:08:12 +0000

    Change: add NG slag ladle car

--- src/vehicles/slag_ladle_cars.py
+++ src/vehicles/slag_ladle_cars.py
@@ -2,6 +2,19 @@ from train import SlagLadleCarConsist, SlagLadleCar
 
 
 def main():
+    #--------------- pony NG ----------------------------------------------------------------------
+
+    consist = SlagLadleCarConsist(roster_id='pony',
+                                  base_numeric_id=4440,
+                                  gen=1,
+                                  subtype='U',
+                                  base_track_type='NG',
+                                  speed=35, # note rare non-standard speed, don't spill molten slag eh?
+                                  sprites_complete=False)
+
+    consist.add_unit(type=SlagLadleCar,
+                     chassis='buffers_only_16px')
+
     #--------------- pony ----------------------------------------------------------------------
 
     consist = SlagLadleCarConsist(roster_id='pony',
