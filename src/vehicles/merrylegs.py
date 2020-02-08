27d576e137dda369d52cd1dbe4e9af62ec68c06b
Andy <andy@teamrubber.com>
2020-02-08 23:08:42 +0000

    Change: more WIP on steam engine sprites

--- src/vehicles/merrylegs.py
+++ src/vehicles/merrylegs.py
@@ -12,7 +12,7 @@ def main(roster_id):
                             fixed_run_cost_points=120, # substantial cost bonus so it can make money
                             random_reverse=True,
                             gen=2,
-                            sprites_complete=True)
+                            sprites_complete=False)
 
     consist.add_unit(type=SteamEngineUnit,
                      weight=49,
