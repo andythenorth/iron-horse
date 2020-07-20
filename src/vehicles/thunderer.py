6d4e0d362586f7da9a6dfdc9287f18948ddd4fe6
Andy <andy@teamrubber.com>
2020-07-20 23:19:04 +0100

    Change: match Thunderer power to 50hp less than Swift, whilst being lighter (pax jokers should very nearly match the non-Joker equivalent in stats)

--- src/vehicles/thunderer.py
+++ src/vehicles/thunderer.py
@@ -8,8 +8,8 @@ def main(roster_id):
                             name='4-6-0 Thunderer', # shorter 2-6-0 version was tried, but doesn't fit a power band gap in the mixed traffic roster
                             role='heavy_express',
                             role_child_branch_num=-1,
-                            replacement_consist_id='kelpie', # this Joker ends with Wyvern, long-lived
-                            power=1450, # less than the Swift
+                            replacement_consist_id='wyvern', # this Joker ends with Wyvern, long-lived
+                            power=1500, # slightly less than the Swift (and lighter engine)
                             tractive_effort_coefficient=0.2,
                             gen=2,
                             fixed_run_cost_points=140, # give a bonus so this can be a genuine mixed-traffic engine
@@ -17,7 +17,7 @@ def main(roster_id):
                             sprites_complete=False)
 
     consist.add_unit(type=SteamEngineUnit,
-                     weight=84,
+                     weight=82,
                      vehicle_length=6,
                      spriterow_num=0)
 
