b945de6aeae988e95331b897ed4f39e33bc461d9
Andy <andy@teamrubber.com>
2020-07-20 17:06:13 +0100

    Change: revert Thunderer to longer 4-6-0 from 2-6-0, and increase power - the roster space in this era is for 1450hp not 1250hp

--- src/vehicles/thunderer.py
+++ src/vehicles/thunderer.py
@@ -5,25 +5,25 @@ def main(roster_id):
     consist = EngineConsist(roster_id=roster_id,
                             id='thunderer',
                             base_numeric_id=4830,
-                            name='2-6-0 Thunderer',
+                            name='4-6-0 Thunderer', # shorter 2-6-0 version was tried, but doesn't fit a power band gap in the mixed traffic roster
                             role='heavy_express',
                             role_child_branch_num=-1,
                             replacement_consist_id='kelpie', # this Joker ends with Wyvern, long-lived
-                            power=1250,
-                            tractive_effort_coefficient=0.18,
+                            power=1450, # less than the Swift
+                            tractive_effort_coefficient=0.2,
                             gen=2,
-                            fixed_run_cost_points=160, # give a bonus so this can be a genuine mixed-traffic engine
+                            fixed_run_cost_points=140, # give a bonus so this can be a genuine mixed-traffic engine
                             intro_date_offset=10, # introduce later than gen epoch by design
                             sprites_complete=False)
 
     consist.add_unit(type=SteamEngineUnit,
-                     weight=62,
-                     vehicle_length=5,
+                     weight=84,
+                     vehicle_length=6,
                      spriterow_num=0)
 
     consist.add_unit(type=SteamEngineTenderUnit,
                      weight=30,
-                     vehicle_length=3,
+                     vehicle_length=4,
                      spriterow_num=1)
 
     return consist
