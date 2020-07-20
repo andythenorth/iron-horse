d0d3e215e62f3074a1b1e0906e4743a0894c1edc
Andy <andy@teamrubber.com>
2020-07-20 22:21:25 +0100

    Change: Reliance expires when Carrack is introduced

--- src/vehicles/reliance.py
+++ src/vehicles/reliance.py
@@ -8,6 +8,7 @@ def main(roster_id):
                             name='2-4-0 Reliance',
                             role='express',
                             role_child_branch_num=-1,
+                            replacement_consist_id='carrack', # this Joker ends with Carrack
                             power=950,
                             tractive_effort_coefficient=0.12,
                             fixed_run_cost_points=140, # minor cost bonus so it can make money
