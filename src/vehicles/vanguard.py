fcecb2b4a7af8e1cdc68c6d9a58ffe213bae0670
Andy <andy@teamrubber.com>
2020-08-02 11:22:06 +0100

    Docs: adjust Vanguard quote

--- src/vehicles/vanguard.py
+++ src/vehicles/vanguard.py
@@ -21,7 +21,7 @@ def main(roster_id):
                      vehicle_length=8,
                      spriterow_num=0)
 
-    consist.description = """They said they wanted these for a freight engine.  No I said.  We need a mixed traffic engine I said.  We talked about it for twenty minutes then we decided I was right."""
+    consist.description = """They said they wanted these for a freight engine.  No I said.  We need a general purpose engine I said.  We talked about it for twenty minutes then we decided I was right."""
     consist.cite = """Mr. Train"""
 
     return consist
