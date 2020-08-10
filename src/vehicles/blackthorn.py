4317097b204afd073985bb011fc097d8c502bd6d
Andy <andy@teamrubber.com>
2020-08-10 15:43:44 +0100

    Change: adjust Blackthorn weight to make cost closer to Toaster

--- src/vehicles/blackthorn.py
+++ src/vehicles/blackthorn.py
@@ -14,7 +14,7 @@ def main(roster_id):
                             sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
-                     weight=120,
+                     weight=124,
                      vehicle_length=8,
                      spriterow_num=0)
 
