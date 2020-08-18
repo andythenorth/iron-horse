dd845e63630d9de3f2037e593d4064b363b7bb91
Andy <andy@teamrubber.com>
2020-08-18 09:08:51 +0100

    Change: sprites complete for Vulcan

--- src/vehicles/vulcan.py
+++ src/vehicles/vulcan.py
@@ -15,7 +15,7 @@ def main(roster_id):
                             intro_date_offset=1,  # introduce later than gen epoch by design
                             fixed_run_cost_points=240, # give a serious malus to this one (balancing eh?)
                             default_livery_extra_docs_examples=[('COLOUR_DARK_GREEN', 'COLOUR_ORANGE'), ('COLOUR_BLUE', 'COLOUR_BLUE')],
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
                      weight=105,
