c5f7765a3f9adae5a12a48f6f2265fe350725d7c
Andy <andy@teamrubber.com>
2020-08-31 19:37:54 +0100

    Change: sprites complete for Shredder

--- src/vehicles/shredder.py
+++ src/vehicles/shredder.py
@@ -14,7 +14,7 @@ def main(roster_id):
                             gen=5,
                             speed=125, # Shredder not replaced, but has gen 6 speeds
                             intro_date_offset=7,  # introduce later than gen epoch by design
-                            sprites_complete=False)
+                            sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
                      weight=76,
