a152da3602047b16c1c2748bf6c73074c23d01c2
Andy <andy@teamrubber.com>
2020-11-01 11:02:39 +0000

    Change: adjust Gronk description

--- src/vehicles/gronk.py
+++ src/vehicles/gronk.py
@@ -23,7 +23,7 @@ def main(roster_id):
                      vehicle_length=4,
                      spriterow_num=0)
 
-    consist.description = """No prizes for speed, but it gets a job done."""
+    consist.description = """The universal shunter.  Goes everywhere&hellip;slowly."""
     consist.foamer_facts = """BR Class 08/09"""
 
     return consist
