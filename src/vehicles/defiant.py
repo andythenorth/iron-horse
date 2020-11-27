fdbcb894b4f5cb9ab0af26bc0759c22e64a09c2e
Andy <andy@teamrubber.com>
2020-11-27 18:00:46 +0000

    Change: adjust Defiant run cost and TE

--- src/vehicles/defiant.py
+++ src/vehicles/defiant.py
@@ -9,14 +9,17 @@ def main(roster_id):
                             role='heavy_freight',
                             role_child_branch_num=-1,
                             power=3650,
+                            # dibble for game balance, assume super-slip control
+                            tractive_effort_coefficient=0.355,
                             random_reverse=True,
                             gen=6,
                             intro_date_offset=-4,  # let's be a little bit earlier for this one
+                            fixed_run_cost_points=280, # run cost nerf as light weight throws the cost too cheap
                             default_livery_extra_docs_examples=[('COLOUR_MAUVE', 'COLOUR_CREAM')],
                             sprites_complete=False)
 
     consist.add_unit(type=DieselEngineUnit,
-                     weight=100,
+                     weight=100, # notably low weight
                      vehicle_length=8,
                      spriterow_num=0)
 
