d0ee8cb263a235fd28a569abe32dcb311c61a264
Andy <andy@teamrubber.com>
2020-11-27 08:39:55 +0000

    Change: reduce Onslaught and Relentless run costs a little

--- src/vehicles/relentless.py
+++ src/vehicles/relentless.py
@@ -12,7 +12,7 @@ def main(roster_id):
                             random_reverse=True,
                             gen=6,
                             intro_date_offset=-7, # let's be earlier on this to keep the mail up with the HSTs etc
-                            fixed_run_cost_points=600, # give a serious malus to this one (balancing eh?)
+                            fixed_run_cost_points=300, # give a small malus to this one (balancing eh?)
                             sprites_complete=True)
 
     consist.add_unit(type=DieselEngineUnit,
