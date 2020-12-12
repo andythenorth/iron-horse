086b22d6d24610bc6a7da847b5151614a28ed537
Andy <andy@teamrubber.com>
2020-12-12 18:50:01 +0000

    Change: gen 5 Sliding Roof Vans

--- src/vehicles/sliding_roof_cars.py
+++ src/vehicles/sliding_roof_cars.py
@@ -5,27 +5,26 @@ def main():
     #--------------- pony ----------------------------------------------------------------------
 
     # gen 5 start, only B and C lengths
-    """
+
     consist = SlidingRoofCarConsist(roster_id='pony',
-                            base_numeric_id=340,
+                            base_numeric_id=5230,
                             gen=5,
                             subtype='B',
                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='2_axle_filled_greebled_24px')
+                     chassis='2_axle_1cc_filled_24px')
 
 
     consist = SlidingRoofCarConsist(roster_id='pony',
-                            base_numeric_id=350,
+                            base_numeric_id=5240,
                             gen=5,
                             subtype='C',
                             sprites_complete=True)
 
     consist.add_unit(type=FreightCar,
-                     chassis='4_axle_filled_greebled_32px')
+                     chassis='4_axle_1cc_filled_32px')
 
-    """
 
     consist = SlidingRoofCarConsist(roster_id='pony',
                             base_numeric_id=5220,
