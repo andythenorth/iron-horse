b6aca5d78960e3ef61ffb3c7b1c7d352bb5af2f8
Andy <andy@teamrubber.com>
2020-12-22 17:16:55 +0000

    Change: force Revolution run costs up, to be closer to Screamer

--- src/vehicles/revolution.py
+++ src/vehicles/revolution.py
@@ -17,6 +17,7 @@ def main(roster_id):
         random_reverse=True,
         pantograph_type="z-shaped-single",
         gen=6,
+        fixed_run_cost_points=280,  # run cost nerf for bi-mode flexibility + high el-power
         intro_date_offset=-2,  # introduce earlier than gen epoch by design
         sprites_complete=True,
     )
