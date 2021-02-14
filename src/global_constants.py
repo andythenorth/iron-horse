5a6eb219750cc5083a46f8240357451533b825b0
Andy <andy@teamrubber.com>
2021-02-14 16:45:47 +0000

    Codechange: fixed a comment

--- src/global_constants.py
+++ src/global_constants.py
@@ -72,7 +72,7 @@ metadata = {
 # cargo aging constant - OTTD default is 185
 CARGO_AGE_PERIOD = 185
 # setting bonuses has limited effect on pax payment over 0-256 tile routes (might show up on longer routes)
-# so instead set a malus, this nerfs standard pax so are relatively less profitable above somewhere around 64-128 tiles
+# so instead set a malus, this nerfs standard pax so they are relatively less profitable above somewhere around 64-128 tiles
 CARGO_AGE_PERIOD_STANDARD_PAX_MALUS = 56
 # metro mail and pax gets a total nerf, only use it on short routes
 CARGO_AGE_PERIOD_METRO_MALUS = 32
