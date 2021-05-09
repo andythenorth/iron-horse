0f45e35cbb87d891aadf26b3048473ecaf1fd5c0
Andy <andy@teamrubber.com>
2021-05-09 11:25:22 +0100

    Codechange: WIP on refactoring spritelayer cargos structure and templating

--- src/train.py
+++ src/train.py
@@ -3870,6 +3870,7 @@ class Train(object):
             temp_storage_ids=global_constants.temp_storage_ids,  # convenience measure
             graphics_path=global_constants.graphics_path,
             pax_car_ids=iron_horse.get_pax_car_ids(),
+            spritelayer_cargos=iron_horse.spritelayer_cargos,
         )
         return nml_result
 
