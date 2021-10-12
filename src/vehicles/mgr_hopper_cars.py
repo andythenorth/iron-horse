e31fe22ed300240c4477b7e5e0825763bc7efc10
Andy <andy@teamrubber.com>
2021-10-12 14:38:08 +0100

    Change: differentiate appearance of gen 5 mgr hoppers from gen 4

--- src/vehicles/mgr_hopper_cars.py
+++ src/vehicles/mgr_hopper_cars.py
@@ -34,7 +34,7 @@ def main():
         sprites_complete=True,
     )
 
-    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_16px")
 
     consist = HopperCarMGRConsist(
         roster_id="pony",
@@ -44,6 +44,6 @@ def main():
         sprites_complete=True,
     )
 
-    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_24px")
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_24px")
 
     # no gen 6 don't bother
