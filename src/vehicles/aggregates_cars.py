1c638c3b915547bee576eca7bb5979c2049bf78a
Andy <andy@teamrubber.com>
2021-10-17 18:43:53 +0100

    Change: sprites complete for aggregates cars

--- src/vehicles/aggregates_cars.py
+++ src/vehicles/aggregates_cars.py
@@ -20,7 +20,7 @@ def main():
         base_numeric_id=2350,
         gen=3,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
@@ -30,7 +30,7 @@ def main():
         base_numeric_id=2360,
         gen=3,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_gapped_24px")
@@ -40,7 +40,7 @@ def main():
         base_numeric_id=2370,
         gen=4,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
@@ -50,7 +50,7 @@ def main():
         base_numeric_id=2380,
         gen=4,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_sparse_24px")
@@ -60,7 +60,7 @@ def main():
         base_numeric_id=6700,
         gen=5,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_16px")
@@ -70,7 +70,7 @@ def main():
         base_numeric_id=1810,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_gapped_greebled_24px")
@@ -80,7 +80,7 @@ def main():
         base_numeric_id=2110,
         gen=5,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_gapped_greebled_32px")
@@ -90,7 +90,7 @@ def main():
         base_numeric_id=2400,
         gen=6,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_sparse_greebled_24px")
@@ -100,7 +100,7 @@ def main():
         base_numeric_id=2390,
         gen=6,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_sparse_greebled_32px")
