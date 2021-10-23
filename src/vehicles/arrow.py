d0d12a8c1bf87e024c5fa9b1ff9fad6e5a15cde2
Andy <andy@teamrubber.com>
2021-10-23 10:12:01 +0100

    Change: sprites and stats complete for Arrow

--- src/vehicles/arrow.py
+++ src/vehicles/arrow.py
@@ -9,32 +9,33 @@ def main(roster_id):
         name="2-6-2 Arrow",
         role="heavy_express",
         role_child_branch_num=-2,  # -ve because Joker
-        power=1850,
+        power=1900, # slightly higher power, offset by higher weight
         tractive_effort_coefficient=0.18,
         gen=3,
         intro_date_offset=4,  # introduce later than gen epoch by design
+        fixed_run_cost_points=150,  # give a small bonus to bring closer to Strongbow cost
         default_livery_extra_docs_examples=[
             ("COLOUR_DARK_GREEN", "COLOUR_DARK_GREEN"),
             ("COLOUR_GREY", "COLOUR_WHITE"),
             ("COLOUR_WHITE", "COLOUR_BLUE"),
         ],
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(
         type=SteamEngineUnit,
-        weight=94,
+        weight=96,
         vehicle_length=7,
-        effect_offsets=[(-3, 0), (-2, 0)],  # double the smoke eh?
+        effect_offsets=[(-3, 0)],
         spriterow_num=0,
     )
 
     consist.add_unit(
-        type=SteamEngineTenderUnit, weight=36, vehicle_length=3, spriterow_num=1
+        type=SteamEngineTenderUnit, weight=44, vehicle_length=3, spriterow_num=1
     )
 
     consist.description = (
-        """."""
+        """Passengers, mail, fish, coal, steel, grain, this one'll haul anything and do it well."""
     )
     consist.foamer_facts = """LNER V2"""
 
