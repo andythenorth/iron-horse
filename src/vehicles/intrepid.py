5c39eeb178aa499783de0b35ded9659c1e728c10
Andy <andy@teamrubber.com>
2021-11-11 08:35:06 +0000

    Change: WIP on railfreight alt livery for Intrepid

--- src/vehicles/intrepid.py
+++ src/vehicles/intrepid.py
@@ -15,7 +15,8 @@ def main(roster_id):
         fixed_run_cost_points=40,  # give a bonus so this can be a genuine mixed-traffic engine
         intro_date_offset=6,  # let's be later for this one
         default_livery_extra_docs_examples=[("COLOUR_GREEN", "COLOUR_WHITE")],
-        sprites_complete=True,
+        alternative_cc_livery="RAILFREIGHT_RED_STRIPE",
+        sprites_complete=False,
     )
 
     consist.add_unit(
