08e866198215ca237694b59c2af27b14a305945d
Andy <andy@teamrubber.com>
2021-11-29 08:40:31 +0000

    Change: sprites complete for gen 6 Swing Roof Hoppers

--- src/vehicles/swing_roof_hopper_cars.py
+++ src/vehicles/swing_roof_hopper_cars.py
@@ -29,7 +29,7 @@ def main():
         base_numeric_id=7050,
         gen=6,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_1cc_filled_hoppers_24px")
@@ -39,7 +39,7 @@ def main():
         base_numeric_id=7040,
         gen=6,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_hoppers_32px")
