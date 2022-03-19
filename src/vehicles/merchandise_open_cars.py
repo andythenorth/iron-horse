c23d79716eb40c32c4cd510da81569c2d9d80a45
Andy <andy@teamrubber.com>
2022-03-19 12:56:06 +0000

    Codechange: blacken merchandise_open_cars.py

--- src/vehicles/merchandise_open_cars.py
+++ src/vehicles/merchandise_open_cars.py
@@ -7,7 +7,11 @@ def main():
     # only type A for gen 1
 
     consist = OpenCarMerchandiseConsist(
-        roster_id="pony", base_numeric_id=7210, gen=1, subtype="A", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=7210,
+        gen=1,
+        subtype="A",
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_filled_16px")
@@ -15,13 +19,21 @@ def main():
     # no new type A for gen 2, gen 1 type A continues
 
     consist = OpenCarMerchandiseConsist(
-        roster_id="pony", base_numeric_id=7220, gen=2, subtype="B", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=7220,
+        gen=2,
+        subtype="B",
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_gapped_24px")
 
     consist = OpenCarMerchandiseConsist(
-        roster_id="pony", base_numeric_id=7230, gen=3, subtype="A", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=7230,
+        gen=3,
+        subtype="A",
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_filled_16px")
