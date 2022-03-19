6fe0f3a5be1fe17b3458e8465777a508e52243bc
Andy <andy@teamrubber.com>
2022-03-19 11:52:33 +0000

    Change: WIP on merchandise open cars

--- src/vehicles/merchandise_open_cars.py
+++ src/vehicles/merchandise_open_cars.py
@@ -61,7 +61,7 @@ def main():
         base_numeric_id=7270,
         gen=5,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_filled_greebled_16px")
@@ -71,7 +71,7 @@ def main():
         base_numeric_id=7280,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_filled_greebled_24px")
@@ -81,7 +81,7 @@ def main():
         base_numeric_id=7290,
         gen=5,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_32px")
