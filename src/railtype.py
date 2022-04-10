8448a9806c99e46b377f96be65f98ded6011ec62
Andy <andy@teamrubber.com>
2022-04-10 18:24:16 +0100

    Codechange: templating complete for railtypes

--- src/railtype.py
+++ src/railtype.py
@@ -8,7 +8,23 @@ class Railtype(object):
 
     def __init__(self, **kwargs):
         self.id = kwargs.get("id")
+        self.label = kwargs.get("label")
+        # 0 is no limit
+        self.speed_limit = kwargs.get("speed_limit", 0)
+        self.curve_speed_multiplier = kwargs.get("curve_speed_multiplier", 1)
+        self.railtype_flags = kwargs.get("railtype_flags", None)
+        self.construction_cost = kwargs.get("construction_cost", None)
+        self.maintenance_cost = kwargs.get("construction_cost", None)
+        self.map_colour = kwargs.get("map_colour", "0x25")
+        self.compatible_railtype_list = kwargs.get("compatible_railtype_list", [])
+        self.powered_railtype_list = kwargs.get("powered_railtype_list", [])
+        self.alternative_railtype_list = kwargs.get("alternative_railtype_list", [])
+        self.use_custom_signals = kwargs.get("use_custom_signals", False)
         self.disabled = False
+
+    def make_nml_railtype_list(self, railtype_list):
+        result = ",".join(['"' + railtype + '"' for railtype in railtype_list])
+        return "[" + result + "]"
 
     def register(self, disabled=False):
         registered_railtypes.append(self)
--- src/railtypes/narrow_gauge.py
+++ src/railtypes/narrow_gauge.py
@@ -1,7 +1,36 @@
 from railtype import Railtype
 
+
 def main(disabled=False):
     railtype = Railtype(
         id="narrow_gauge",
+        label="NAAN",
+        construction_cost=5,
+        maintenance_cost=7,
+        railtype_flags=[],
+        map_colour=0x25,
+        compatible_railtype_list=[
+            "NABN",
+            "NACN",
+            "NADN",
+            "NAEN",
+            "NAAE",
+            "NABE",
+            "NACE",
+            "NADE",
+            "NAEE",
+        ],
+        powered_railtype_list=[
+            "NABN",
+            "NACN",
+            "NADN",
+            "NAEN",
+            "NAAE",
+            "NABE",
+            "NACE",
+            "NADE",
+            "NAEE",
+        ],
+        alternative_railtype_list=["NABN", "NACN", "NADN", "NAEN"],
     )
     railtype.register(disabled=disabled)
