502f41caedc539707d9dd61d651c605e4b4a6793
Andy <andy@teamrubber.com>
2022-04-10 20:50:17 +0100

    Codechange: no need for separate lolz and catz railtypes for TGV

--- src/railtypes/lolz.py
+++ src/railtypes/lolz.py
@@ -7,8 +7,9 @@ def main(disabled=False):
         label="LOLZ",
         construction_cost=5,
         maintenance_cost=7,
-        railtype_flags=["RAILTYPE_FLAG_HIDDEN"],
+        railtype_flags=["RAILTYPE_FLAG_CATENARY"],
         map_colour=0x25,
+        # TGVs can go on ELRL etc, but this won't allow RAIL / ELRL onto the TGV tracks
         compatible_railtype_list=[
             "RAIL",
             "ELRL",
@@ -19,6 +20,6 @@ def main(disabled=False):
             "CATZ",
         ],
         use_custom_sprites=False,
-        alternative_railtype_list=["CATZ"],
+        alternative_railtype_list=[],
     )
     railtype.register(disabled=disabled)
--- src/train.py
+++ src/train.py
@@ -497,7 +497,7 @@ class Consist(object):
             "METRO": "METRO",  # assume METRO is always METRO, whether electric flag is set or not
         }
         if self.can_use_lgv_rails:
-            return "CATZ"
+            return "LOLZ"
         if self.requires_electric_rails:
             return eltrack_type_mapping[self.base_track_type]
         else:
