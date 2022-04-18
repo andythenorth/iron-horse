0f0c54b38f9f54a6e0030f85f51a1bf50a97f4fe
Andy <andy@teamrubber.com>
2022-04-18 20:36:05 +0100

    Codechange: simplify mapping to electrified railtypes

--- src/train.py
+++ src/train.py
@@ -487,25 +487,19 @@ class Consist(object):
 
     @property
     def track_type(self):
-        # are you sure you don't want base_track_type instead?
+        # are you sure you don't want base_track_type instead? (generally you do want base_track_type)
         # track_type handles converting base_track_type to an actual railtype label
         # this is done by looking up a railtype mapping in global constants, via internal labels
         # e.g. electric engines with "RAIL" as base_track_type will be translated to "ELRL"
         # narrow gauge trains will be similarly have "NG" translated to an appropriate NG railytpe label
-        # it's often more convenient to use base_track_type prop, which treats ELRL and RAIL as same (for example)
-        eltrack_type_mapping = {
-            "RAIL": "RAIL_ELECTRIFIED",
-            "NG": "NG_ELECTRIFIED",
-            "METRO": "METRO",  # assume METRO is always METRO, whether electric flag is set or not
-        }
         if self.requires_electric_rails:
             # for electrified vehicles, translate base_track_type before getting the mapping to labels
             # iff electrification types ever gain subtypes (AC, DC, etc), add further checks here
-            valid_railtype_labels = global_constants.base_track_type_to_railtype_mapping[eltrack_type_mapping[self.base_track_type]]
+            mapping_key = self.base_track_type + "_ELECTRIFIED"
         else:
-            # if unelectrified, just get the mapping to labels directly
-            valid_railtype_labels = global_constants.base_track_type_to_railtype_mapping[self.base_track_type]
-        # assume that the label we want for the vehile is the first in the list of valid types (the rest are fallbacks if the first railtype is missing)
+            mapping_key = self.base_track_type
+        valid_railtype_labels = global_constants.base_track_type_to_railtype_mapping[mapping_key]
+        # assume that the label we want for the vehicle is the first in the list of valid types (the rest are fallbacks if the first railtype is missing)
         result = valid_railtype_labels[0]
         # set modifiers on the label by modifying the last byte
         # modifiers are not orthogonal and the byte can only be set to a single value
