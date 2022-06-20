5d29ab79a43b1044704780d324d8a8dfe9f7ecd8
Andy <andy@teamrubber.com>
2022-06-20 22:23:14 +0100

    Change: stub in a basic Ibex test

---
+++ src/vehicles/snowplough_ibex_gen_2.py
@@ -0,0 +1,20 @@
+from train import SnowploughEngineConsist, SnowploughUnit
+
+
+def main(roster_id):
+    consist = SnowploughEngineConsist(
+        roster_id=roster_id,
+        id="snowplough_ibex_gen_2",
+        base_numeric_id=9020,
+        name="Snowplough",
+        gen=2,
+        speed=75,
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=SnowploughUnit, weight=50, vehicle_length=4)
+
+    consist.description = """CABBAGE."""
+    consist.foamer_facts = """CABBAGE"""
+
+    return consist
---
+++ src/vehicles/trient.py
@@ -0,0 +1,40 @@
+from train import EngineConsist, DieselEngineUnit
+
+
+def main(roster_id):
+    consist = EngineConsist(
+        roster_id=roster_id,
+        id="trient",
+        base_numeric_id=9030,
+        name="Trient",
+        role="super_heavy_freight",
+        role_child_branch_num=1,
+        power=4050,  # 750hp steps Vanguard -> Grid -> Cheddar Valley
+        # dibble for game balance, assume super-slip control
+        tractive_effort_coefficient=0.4,
+        random_reverse=True,
+        intro_date_offset=3,  # let's be a little bit later for this one
+        gen=5,
+        fixed_run_cost_points=210,  # unrealism: run cost nerf for being so high-powered
+        alternative_cc_livery="YEOMAN",
+        default_livery_extra_docs_examples=[
+            ("COLOUR_WHITE", "COLOUR_GREEN"),
+            ("COLOUR_ORANGE", "COLOUR_RED"),
+        ],
+        sprites_complete=False,
+    )
+
+    consist.add_unit(
+        type=DieselEngineUnit,
+        weight=125,
+        vehicle_length=8,
+        effect_offsets=[(1, 0)],
+        spriterow_num=0,
+    )
+
+    consist.description = """CABBAGE."""
+    consist.foamer_facts = (
+        """CABBAGE - GMD  / EMD Class 59, uprated GMD / EMD 710 series prime mover"""
+    )
+
+    return consist
