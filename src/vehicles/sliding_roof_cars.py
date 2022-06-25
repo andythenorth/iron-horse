946e525202c2c4c13c36738d06f8e270fd5cc259
Andy <andy@teamrubber.com>
2022-06-25 22:06:02 +0100

    Change: add sliding roof and sliding wall cars for Ibex

--- src/vehicles/sliding_roof_cars.py
+++ src/vehicles/sliding_roof_cars.py
@@ -45,3 +45,47 @@ def main():
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_32px")
+
+    # --------------- ibex ----------------------------------------------------------------------
+
+    # gen 5 start, only B and C lengths
+
+    consist = FlatCarSlidingRoofConsist(
+        roster_id="ibex",
+        base_numeric_id=8690,
+        gen=5,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_1cc_filled_24px")
+
+    consist = FlatCarSlidingRoofConsist(
+        roster_id="ibex",
+        base_numeric_id=8700,
+        gen=5,
+        subtype="C",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_32px")
+
+    consist = FlatCarSlidingRoofConsist(
+        roster_id="ibex",
+        base_numeric_id=8710,
+        gen=6,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_1cc_filled_24px")
+
+    consist = FlatCarSlidingRoofConsist(
+        roster_id="ibex",
+        base_numeric_id=8720,
+        gen=6,
+        subtype="C",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_32px")
--- src/vehicles/sliding_wall_cars.py
+++ src/vehicles/sliding_wall_cars.py
@@ -33,7 +33,11 @@ def main():
     )
 
     consist = BoxCarSlidingWallConsist(
-        roster_id="pony", base_numeric_id=9450, gen=5, subtype="B", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=9450,
+        gen=5,
+        subtype="B",
+        sprites_complete=True,
     )
 
     consist.add_unit(
@@ -43,7 +47,11 @@ def main():
     )
 
     consist = BoxCarSlidingWallConsist(
-        roster_id="pony", base_numeric_id=9480, gen=5, subtype="C", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=9480,
+        gen=5,
+        subtype="C",
+        sprites_complete=True,
     )
 
     consist.add_unit(
@@ -77,7 +85,11 @@ def main():
     )
 
     consist = BoxCarSlidingWallConsist(
-        roster_id="pony", base_numeric_id=9370, gen=6, subtype="B", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=9370,
+        gen=6,
+        subtype="B",
+        sprites_complete=True,
     )
 
     consist.add_unit(
@@ -87,7 +99,11 @@ def main():
     )
 
     consist = BoxCarSlidingWallConsist(
-        roster_id="pony", base_numeric_id=9360, gen=6, subtype="C", sprites_complete=True
+        roster_id="pony",
+        base_numeric_id=9360,
+        gen=6,
+        subtype="C",
+        sprites_complete=True,
     )
 
     consist.add_unit(
@@ -99,6 +115,114 @@ def main():
     consist = BoxCarSlidingWallConsist(
         roster_id="pony",
         base_numeric_id=5200,
+        gen=6,
+        subtype="D",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="2_axle_1cc_filled_20px",
+        symmetry_type="asymmetric",
+        spriterow_num=0,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="2_axle_1cc_filled_20px",
+        symmetry_type="asymmetric",
+        spriterow_num=4,
+    )
+
+    # --------------- ibex ----------------------------------------------------------------------
+
+    # starts gen 5, may add gen 4 later
+
+    consist = BoxCarSlidingWallConsist(
+        roster_id="ibex",
+        base_numeric_id=8730,
+        gen=5,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="2_axle_1cc_filled_24px",
+    )
+
+    consist = BoxCarSlidingWallConsist(
+        roster_id="ibex",
+        base_numeric_id=8740,
+        gen=5,
+        subtype="C",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="4_axle_1cc_filled_32px",
+    )
+
+    consist = BoxCarSlidingWallConsist(
+        roster_id="ibex",
+        base_numeric_id=10,
+        gen=5,
+        subtype="D",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="2_axle_1cc_filled_20px",
+        symmetry_type="asymmetric",
+        spriterow_num=0,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="2_axle_1cc_filled_20px",
+        symmetry_type="asymmetric",
+        spriterow_num=4,
+    )
+
+    consist = BoxCarSlidingWallConsist(
+        roster_id="ibex",
+        base_numeric_id=8760,
+        gen=6,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="2_axle_1cc_filled_24px",
+    )
+
+    consist = BoxCarSlidingWallConsist(
+        roster_id="ibex",
+        base_numeric_id=8770,
+        gen=6,
+        subtype="C",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        suppress_roof_sprite=True,  # non-standard roof for this wagon
+        chassis="4_axle_1cc_filled_32px",
+    )
+
+    consist = BoxCarSlidingWallConsist(
+        roster_id="ibex",
+        base_numeric_id=20,
         gen=6,
         subtype="D",
         sprites_complete=True,
