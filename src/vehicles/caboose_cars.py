e789dc4f407ce3f48b4239bce01e633e2adebac3
Andy <andy@teamrubber.com>
2022-08-16 11:20:42 +0100

    Change: add brown NG caboose cars for pony

--- src/vehicles/caboose_cars.py
+++ src/vehicles/caboose_cars.py
@@ -6,20 +6,27 @@ def main():
     # caboose names map to labelled spriterows, as defined in the vehicle files
     # note that spriterow mapping will need redefined for each roster and base track type
     # names are arbitrary strings, except for 'default_1', 'default_2', etc which must match the number of generations for this base track type in the roster
-    spriterow_labels = ["ng_enclosed_1", "ng_enclosed_2", "ng_enclosed_3", "ng_enclosed_4"]
+    spriterow_labels = [
+        "cc_enclosed_1",
+        "cc_enclosed_2",
+        "cc_enclosed_3",
+        "brown_enclosed_1",
+        "brown_enclosed_2",
+        "brown_enclosed_3",
+    ]
     # note that probability of a specific type within a family can be increased by repeating it in the list for that family
     caboose_families = {
-        "default_1": ["ng_enclosed_1"],
-        "default_2": ["ng_enclosed_2"],
-        "default_3": ["ng_enclosed_3"],
-        "default_4": ["ng_enclosed_4"],
+        "default_1": ["cc_enclosed_1", "brown_enclosed_1"],
+        "default_2": ["cc_enclosed_2", "brown_enclosed_2"],
+        "default_3": ["cc_enclosed_3", "brown_enclosed_3"],
+        "default_4": ["cc_enclosed_3", "brown_enclosed_3"],
     }
     # these should match the number of default families, in order
     buy_menu_sprite_pairs = [
-        ("ng_enclosed_1", "ng_enclosed_1"),
-        ("ng_enclosed_2", "ng_enclosed_2"),
-        ("ng_enclosed_3", "ng_enclosed_3"),
-        ("ng_enclosed_4", "ng_enclosed_4"),
+        ("cc_enclosed_1", "brown_enclosed_1"),
+        ("cc_enclosed_2", "brown_enclosed_3"),
+        ("cc_enclosed_3", "brown_enclosed_3"),
+        ("cc_enclosed_3", "brown_enclosed_3"),
     ]
 
     consist = CabooseCarConsist(
@@ -72,9 +79,26 @@ def main():
     ]
     # note that probability of a specific type within a family can be increased by repeating it in the list for that family
     caboose_families = {
-        "default_1": ["cc_enclosed_1", "brown_enclosed_1", "brown_enclosed_1", "cc_open_1", "brown_open_1"],
-        "default_2": ["cc_enclosed_2_3", "brown_enclosed_2_3", "cc_open_1", "brown_open_1"],
-        "default_3": ["cc_enclosed_2_3", "brown_enclosed_2_3", "cc_open_1", "cc_open_1", "brown_open_1"],
+        "default_1": [
+            "cc_enclosed_1",
+            "brown_enclosed_1",
+            "brown_enclosed_1",
+            "cc_open_1",
+            "brown_open_1",
+        ],
+        "default_2": [
+            "cc_enclosed_2_3",
+            "brown_enclosed_2_3",
+            "cc_open_1",
+            "brown_open_1",
+        ],
+        "default_3": [
+            "cc_enclosed_2_3",
+            "brown_enclosed_2_3",
+            "cc_open_1",
+            "cc_open_1",
+            "brown_open_1",
+        ],
         "default_4": [
             "cc_enclosed_4",
             "brown_enclosed_4",
