3afc6b58b733fc3363f14d14cce027f58d14edf6
Andy <andy@teamrubber.com>
2023-03-20 21:11:19 +0000

    Change: WIP on Yak sprites and stats

--- src/vehicles/yak.py
+++ src/vehicles/yak.py
@@ -6,7 +6,7 @@ def main(roster_id):
         roster_id=roster_id,
         id="yak",
         base_numeric_id=11060,
-        name="0-8-0 Yak",
+        name="0-8-2 Yak",
         role="branch_freight",
         role_child_branch_num=-1,
         power_by_power_source={
@@ -21,7 +21,7 @@ def main(roster_id):
         sprites_complete=False,
     )
 
-    consist.add_unit(type=SteamEngineUnit, weight=65, vehicle_length=6, spriterow_num=0)
+    consist.add_unit(type=SteamEngineUnit, weight=70, vehicle_length=6, spriterow_num=0)
 
     # !! tank engine rebuild of the Doister? https://www.lner.info/locos/Q/q1thompson.php
     consist.description = """We ought to do good to others as simply as a horse runs."""
