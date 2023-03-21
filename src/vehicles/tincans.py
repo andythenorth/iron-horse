58ca81e453949dc875b8c2233ca8f4e98d07fac4
Andy <andy@teamrubber.com>
2023-03-21 21:59:37 +0000

    Change: sprites complete for Tincans

--- src/vehicles/tincans.py
+++ src/vehicles/tincans.py
@@ -18,7 +18,8 @@ def main(roster_id):
         pantograph_type="z-shaped-single",
         intro_year_offset=-13,  # introduce earlier than gen epoch by design
         additional_liveries=[],
-        sprites_complete=False,
+        sprites_complete=True,
+        sprites_additional_liveries_needed=True, # railfreight?
     )
 
     consist.add_unit(
