63c8b2b7ef33e1f2448d84c7f99ee8ac8087622d
Andy <andy@teamrubber.com>
2023-03-28 08:16:33 +0100

    Change: move Blackthorn back to Grid replacement; replace Dreadnought with Defiant; adjust Quietus power and make it not a joker; Relentless is a joker, not in simplified gameplay

--- src/vehicles/relentless.py
+++ src/vehicles/relentless.py
@@ -8,7 +8,7 @@ def main(roster_id):
         base_numeric_id=13440,
         name="Relentless",
         role="super_heavy_express",
-        role_child_branch_num=3,
+        role_child_branch_num=-1,
         power_by_power_source={
             "DIESEL": 3800,
         },
