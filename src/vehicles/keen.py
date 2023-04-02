d5e9ce207252c16dead84f64ee6e493cf6e4aa8f
Andy <andy@teamrubber.com>
2023-04-02 19:43:46 +0100

    Change: sprites complete for Keen

--- src/vehicles/keen.py
+++ src/vehicles/keen.py
@@ -15,27 +15,21 @@ def main(roster_id):
         random_reverse=True,
         gen=3,
         intro_year_offset=-13,  # introduce much earlier than gen epoch by design
-        fixed_run_cost_points=140,  # minor run cost bonus as default algorithm makes run cost too high
+        fixed_run_cost_points=240, # adjust to match similar engines of same gen
         additional_liveries=[],
-        sprites_complete=False,
+        sprites_complete=True,
         sprites_additional_liveries_needed=True, # nightshade / nighthawk?
     )
 
     consist.add_unit(
         type=SteamEngineUnit,
-        weight=55,
+        weight=58,
         vehicle_length=5,
         spriterow_num=0,
         repeat=2,
     )
 
-    # !! what are these supposed to be?
-    # some 0-6-0t thing?  Classic jinty? Pannier?
-    # maybe Austerity J94 is simplest?
-    # 1937 18in Hunslet https://www.irsociety.co.uk/Archives/23/18in_Hunslets.htm
-    # https://preservedbritishsteamlocomotives.com/hunslet-works-no-1440-airdale-no-3-0-6-0st/
-    # or the chunky bagnalls? https://www.flickr.com/photos/60956647@N02/15691831650
     consist.description = """Gallop apace, you fiery-footed steeds."""
-    consist.foamer_facts = """"""
+    consist.foamer_facts = """18in Hunslet tanks, Austerity tanks, LNER J94 class"""
 
     return consist
