2dd29df54d43f837df64f32917dd3c64e53be240
Andy <andy@teamrubber.com>
2023-04-14 18:07:53 +0100

    Change: additional banger blue livery for Carrack

--- src/vehicles/carrack.py
+++ src/vehicles/carrack.py
@@ -15,9 +15,8 @@ def main(roster_id):
         tractive_effort_coefficient=0.18,
         gen=2,
         intro_year_offset=-3,  # introduce earlier than gen epoch by design
-        additional_liveries=[],
+        additional_liveries=["BANGER_BLUE"],
         sprites_complete=True,
-        sprites_additional_liveries_needed=True, # banger blue?
     )
 
     consist.add_unit(type=SteamEngineUnit, weight=60, vehicle_length=5, spriterow_num=0)
