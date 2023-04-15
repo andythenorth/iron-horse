3719e722768a94d800d5ec93d248c4da86764c55
Andy <andy@teamrubber.com>
2023-04-15 19:33:10 +0100

    Change: additional liveries for Screamer (and remedied unfinished RES livery)

--- src/vehicles/screamer.py
+++ src/vehicles/screamer.py
@@ -18,7 +18,7 @@ def main(roster_id):
         pantograph_type="z-shaped-double",
         intro_year_offset=2,  # introduce later than gen epoch by design
         # railfreight grey, intercity, GNER?
-        additional_liveries=["FREIGHTLINER_GBRF", "RES", "SWOOSH", "FREIGHTLINER_2"],
+        additional_liveries=["BANGER_BLUE", "FREIGHTLINER_GBRF", "RES", "SWOOSH", "DBSCHENKER", "FREIGHTLINER_2"],
         default_livery_extra_docs_examples=[
             ("COLOUR_WHITE", "COLOUR_BLUE"),
             ("COLOUR_BLUE", "COLOUR_WHITE"),
@@ -26,7 +26,6 @@ def main(roster_id):
             ("COLOUR_WHITE", "COLOUR_RED"),
         ],
         sprites_complete=True,
-        sprites_additional_liveries_needed=True, # banger blue, railfreight grey, GNER?
     )
 
     consist.add_unit(
