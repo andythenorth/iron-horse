3c13ef521671db271d6fff6c90c396765f7deb8b
Andy <andy@teamrubber.com>
2023-09-02 08:36:04 +0100

    Change: additional livery for Vulcan

--- src/vehicles/vulcan.py
+++ src/vehicles/vulcan.py
@@ -16,7 +16,7 @@ def main(roster_id):
         gen=4,
         intro_year_offset=1,  # introduce later than gen epoch by design
         fixed_run_cost_points=240,  # give a serious malus to this one (balancing eh?)
-        additional_liveries=["FINSBURY_CABS"],
+        additional_liveries=["FINSBURY_CABS", "BANGER_BLUE"],
         default_livery_extra_docs_examples=[
             ("COLOUR_DARK_GREEN", "COLOUR_ORANGE"),
             ("COLOUR_BLUE", "COLOUR_BLUE"),
