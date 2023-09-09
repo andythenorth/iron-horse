dbf21f620c3b45249a312a64c3479cfe7f259d47
Andy <andy@teamrubber.com>
2023-09-09 16:18:07 +0100

    Change: add standard gauge ingot cars

--- src/vehicles/ingot_cars.py
+++ src/vehicles/ingot_cars.py
@@ -15,3 +15,28 @@ def main():
     )
 
     consist.add_unit(type=IngotCar, chassis="empty_8px", repeat=2)
+
+    # --------------- pony -------------------------------------------------------------------------
+
+    consist = IngotCarConsist(
+        roster_id="pony",
+        base_numeric_id=80,
+        gen=1,
+        subtype="U",
+        speed=35,  # note rare non-standard speed, don't spill hot ingots eh?
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=IngotCar, chassis="empty_8px", repeat=2)
+
+
+    consist = IngotCarConsist(
+        roster_id="pony",
+        base_numeric_id=60,
+        gen=4,
+        subtype="U",
+        speed=35,  # note rare non-standard speed, don't spill hot ingots eh?
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=IngotCar, chassis="empty_8px", repeat=2)
