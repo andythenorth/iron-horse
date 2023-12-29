06fec8200ab071820ebb89764ddd0c6946be93bc
Andy <andy@teamrubber.com>
2023-12-29 09:47:30 +0000

    Change: banger blue additional livery for Little Bear

--- src/vehicles/little_bear.py
+++ src/vehicles/little_bear.py
@@ -18,7 +18,7 @@ def main(roster_id):
         gen=4,
         intro_year_offset=-6,
         #note that livery names are metadata only and can repeat for different spriterows
-        additional_liveries=["INDUSTRIAL_YELLOW"],
+        additional_liveries=["BANGER_BLUE", "INDUSTRIAL_YELLOW"],
         default_livery_extra_docs_examples=[
             ("COLOUR_PALE_GREEN", "COLOUR_WHITE"),
         ],
