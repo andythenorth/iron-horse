d678ebacbdc1db01b1175fcc05fe388e9be0d19a
Andy <andy@teamrubber.com>
2023-12-29 13:50:32 +0000

    Change: gen 5 high speed pax cars appear to be complete

--- src/vehicles/high_speed_passenger_cars.py
+++ src/vehicles/high_speed_passenger_cars.py
@@ -8,7 +8,7 @@ def main():
         base_numeric_id=11020,
         gen=5,
         subtype="U",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=PaxCar, chassis="4_axle_solid_express_32px")
