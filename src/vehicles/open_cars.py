9426152506822a238b947550c0557177b849cb55
Andy <andy@teamrubber.com>
2024-01-27 13:58:18 +0000

    Change: add gen 4C open car

--- src/vehicles/open_cars.py
+++ src/vehicles/open_cars.py
@@ -95,6 +95,16 @@ def main():
 
     consist = OpenCarConsist(
         roster_id="pony",
+        base_numeric_id=15980,
+        gen=4,
+        subtype="C",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_filled_32px")
+
+    consist = OpenCarConsist(
+        roster_id="pony",
         base_numeric_id=14940,
         gen=5,
         subtype="A",
