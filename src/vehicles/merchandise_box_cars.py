a247e3ed8ca50f6ea6911c96c04599d73b520930
Andy <andy@teamrubber.com>
2024-02-12 10:25:04 +0000

    Change: NG merchandise box cars; improve NG box car sprite marginally

--- src/vehicles/merchandise_box_cars.py
+++ src/vehicles/merchandise_box_cars.py
@@ -2,6 +2,30 @@ from train import BoxCarMerchandiseConsist, FreightCar
 
 
 def main():
+    # --------------- pony NG ----------------------------------------------------------------------
+
+    consist = BoxCarMerchandiseConsist(
+        roster_id="pony",
+        base_numeric_id=11860,
+        gen=3,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_16px")
+
+    consist = BoxCarMerchandiseConsist(
+        roster_id="pony",
+        base_numeric_id=11940,
+        gen=3,
+        subtype="B",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_24px")
+
     # --------------- pony ----------------------------------------------------------------------
 
     consist = BoxCarMerchandiseConsist(
