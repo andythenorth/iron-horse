b81ab7e572dff470c2ef3ec72d392b7b3c48530b
Andy <andy@teamrubber.com>
2024-02-17 20:50:57 +0000

    Codechange: remove dead line and add comment

--- src/render_docs.py
+++ src/render_docs.py
@@ -214,10 +214,9 @@ def render_docs_images(consist, static_dir_dst, generated_graphics_path, doc_hel
         )
 
     for processed_vehicle_image, variant in docs_image_variants:
-        cc_remap_indexes = doc_helper.remap_company_colours(variant["cc_remaps"])
         # probably fragile workaround to use the alternative livery spriterow
         # for the edge case that a docs default livery 2nd company colour matches the alternative livery triggers
-        # !! but this is the same line as above?? what's it supposed to be doing?
+        # ^ !! we what now?  is this actually doing this?
         cc_remap_indexes = doc_helper.remap_company_colours(variant["cc_remaps"])
 
         processed_vehicle_image = processed_vehicle_image.copy().point(
