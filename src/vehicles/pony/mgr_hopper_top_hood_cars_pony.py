a8a25a3786a0669f3250b095b107876a8b139695
Andy <andy@teamrubber.com>
2024-02-24 18:45:41 +0000

    Change: sprites complete for MGR Hood Hoppers

--- src/vehicles/pony/mgr_hopper_top_hood_cars_pony.py
+++ src/vehicles/pony/mgr_hopper_top_hood_cars_pony.py
@@ -11,7 +11,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=20230,
         gen=4,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
@@ -22,7 +22,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=20240,
         gen=4,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_24px")
@@ -33,7 +33,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=20250,
         gen=5,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_16px")
@@ -44,7 +44,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=20260,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_24px")
