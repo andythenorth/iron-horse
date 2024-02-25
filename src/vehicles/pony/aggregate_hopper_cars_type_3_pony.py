0bfe58b1f32f5d5cb36b389838c9b9f4e89f952a
Andy <andy@teamrubber.com>
2024-02-25 12:55:08 +0000

    Change: finish gen 5A pony aggregate car type 3 sprites; stub in (unused, unfinished) pony NG aggregate cars type 3

--- src/vehicles/pony/aggregate_hopper_cars_type_3_pony.py
+++ src/vehicles/pony/aggregate_hopper_cars_type_3_pony.py
@@ -2,6 +2,34 @@ from train import HopperCarAggregateConsistType3, FreightCar
 
 
 def main(roster_id, **kwargs):
+    # --------------- narrow gauge -----------------------------------------------------------------
+    """
+    consist = HopperCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34300,
+        gen=3,
+        intro_year_offset=20,  # let's be a little bit later for this one
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_16px")
+
+    consist = HopperCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=32850,
+        gen=3,
+        intro_year_offset=20,  # let's be a little bit later for this one
+        subtype="B",
+        base_track_type_name="NG",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_24px")
+    """
     # --------------- standard gauge ---------------------------------------------------------------
 
     consist = HopperCarAggregateConsistType3(
