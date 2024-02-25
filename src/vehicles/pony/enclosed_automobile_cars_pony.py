35d66485e3a2305f1047977da7e5db8ec1219695
Andy <andy@teamrubber.com>
2024-02-25 12:08:22 +0000

    Change: add pony 6/8 enclosed vehicle transporter cars

--- src/vehicles/pony/enclosed_automobile_cars_pony.py
+++ src/vehicles/pony/enclosed_automobile_cars_pony.py
@@ -6,6 +6,17 @@ def main(roster_id, **kwargs):
     consist = AutomobileEnclosedCarConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34410,
+        gen=3,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=ExpressCar, chassis="4_axle_solid_express_24px")
+
+    consist = AutomobileEnclosedCarConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=18000,
         gen=3,
         subtype="C",
@@ -17,6 +28,17 @@ def main(roster_id, **kwargs):
     consist = AutomobileEnclosedCarConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34420,
+        gen=4,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=ExpressCar, chassis="4_axle_solid_express_24px")
+
+    consist = AutomobileEnclosedCarConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=18010,
         gen=4,
         subtype="C",
@@ -24,6 +46,17 @@ def main(roster_id, **kwargs):
     )
 
     consist.add_unit(type=ExpressCar, chassis="4_axle_solid_express_32px")
+
+    consist = AutomobileEnclosedCarConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34430,
+        gen=5,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=ExpressCar, chassis="4_axle_filled_greebled_24px")
 
     consist = AutomobileEnclosedCarConsist(
         roster_id=roster_id,
