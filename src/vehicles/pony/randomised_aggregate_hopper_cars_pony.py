71cf24e4bc0bcd8c7de364854d13aaea4d130ab9
Andy <andy@teamrubber.com>
2024-02-26 22:13:11 +0000

    Change: more randomised aggregate hoppers for pony

--- src/vehicles/pony/randomised_aggregate_hopper_cars_pony.py
+++ src/vehicles/pony/randomised_aggregate_hopper_cars_pony.py
@@ -2,12 +2,81 @@ from train import HopperCarAggregateRandomisedConsist, FreightCar
 
 
 def main(roster_id, **kwargs):
-    # --------------- standard gauge ---------------------------------------------------------------
-    """
+    # --------------- narrow gauge -----------------------------------------------------------------
     consist = HopperCarAggregateRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=20320,
+        base_numeric_id=35230,
+        gen=1,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=35050,
+        gen=3,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34480,
+        gen=3,
+        subtype="B",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_24px")
+
+    # --------------- standard gauge ---------------------------------------------------------------
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34810,
+        gen=2,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34300,
+        gen=3,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34460,
+        gen=3,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_24px")
+
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34270,
         gen=4,
         subtype="A",
         sprites_complete=True,
@@ -18,7 +87,7 @@ def main(roster_id, **kwargs):
     consist = HopperCarAggregateRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=20330,
+        base_numeric_id=34440,
         gen=4,
         subtype="B",
         sprites_complete=True,
@@ -29,7 +98,7 @@ def main(roster_id, **kwargs):
     consist = HopperCarAggregateRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=20340,
+        base_numeric_id=34250,
         gen=4,
         subtype="C",
         sprites_complete=True,
@@ -39,7 +108,18 @@ def main(roster_id, **kwargs):
         type=FreightCar,
         chassis="empty_32px",
     )
-    """
+
+    consist = HopperCarAggregateRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=34230,
+        gen=5,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
     consist = HopperCarAggregateRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
