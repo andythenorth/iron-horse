88c255f2d26e2d5836bf4d301f4f02a793e86488
Andy <andy@teamrubber.com>
2024-02-28 20:27:46 +0000

    Change: more aggregate dump cars

--- src/vehicles/pony/aggregate_dump_cars_type_2_pony.py
+++ src/vehicles/pony/aggregate_dump_cars_type_2_pony.py
@@ -7,6 +7,63 @@ def main(roster_id, **kwargs):
     consist = DumpCarAggregateConsistType2(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=35080,
+        gen=1,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    # gen 1 also covers gen 2
+
+    consist = DumpCarAggregateConsistType2(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=35090,
+        gen=3,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    consist = DumpCarAggregateConsistType2(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=35100,
+        gen=3,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_gapped_24px")
+
+    consist = DumpCarAggregateConsistType2(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=35110,
+        gen=4,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    consist = DumpCarAggregateConsistType2(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=35120,
+        gen=4,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_sparse_24px")
+
+    consist = DumpCarAggregateConsistType2(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=34830,
         gen=5,
         subtype="A",
--- src/vehicles/pony/aggregate_dump_cars_type_3_pony.py
+++ src/vehicles/pony/aggregate_dump_cars_type_3_pony.py
@@ -7,6 +7,63 @@ def main(roster_id, **kwargs):
     consist = DumpCarAggregateConsistType3(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=33820,
+        gen=1,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    # gen 1 also covers gen 2
+
+    consist = DumpCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=33830,
+        gen=3,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    consist = DumpCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=33840,
+        gen=3,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_gapped_24px")
+
+    consist = DumpCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=33850,
+        gen=4,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    consist = DumpCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=33860,
+        gen=4,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_sparse_24px")
+
+    consist = DumpCarAggregateConsistType3(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=35300,
         gen=5,
         subtype="A",
