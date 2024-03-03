b3082be4416c65d0058fe3a487f8b291c02aedc9
Andy <andy@teamrubber.com>
2024-03-03 22:06:31 +0000

    Change: more WIP on gen 4 pony NG diesels

--- src/vehicles/pony/seven_star.py
+++ src/vehicles/pony/seven_star.py
@@ -17,7 +17,7 @@ def main(roster_id, **kwargs):
         extended_vehicle_life=True,  # extended vehicle life for all this generation of NG eh
         random_reverse=True,
         # note that livery names are metadata only and can repeat for different spriterows
-        #additional_liveries=["INDUSTRIAL_YELLOW"],
+        additional_liveries=["SWOOSH"],
         sprites_complete=False,
     )
 
