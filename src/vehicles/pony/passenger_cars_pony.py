4c879265f6ed5c232c942d2c0173f0f971c1c69c
Andy <andy@teamrubber.com>
2024-03-04 18:44:36 +0000

    Change: WIP on gen 4 pony NG pax cars and express engines

--- src/vehicles/pony/passenger_cars_pony.py
+++ src/vehicles/pony/passenger_cars_pony.py
@@ -77,6 +77,18 @@ def main(roster_id, **kwargs):
 
     consist.add_unit(type=PaxCar, chassis="4_axle_ng_24px")
 
+    consist = PassengerCarConsist(
+        roster_id=roster_id,
+        roster_id_providing_module = kwargs["roster_id_providing_module"],
+        base_numeric_id=25550,
+        gen=4,
+        subtype="C",
+        base_track_type_name="NG",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=PaxCar, chassis="4_axle_ng_32px")
+
     # --------------- standard gauge ---------------------------------------------------------------
 
     consist = PassengerCarConsist(
