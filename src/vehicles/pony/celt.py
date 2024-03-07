f6d247b2a27183c0cb5c0c03d106bb26f25061bc
Andy <andy@teamrubber.com>
2024-03-07 08:35:45 +0000

    Codechange: adjust Celt numeric ID to a free one

--- src/vehicles/pony/celt.py
+++ src/vehicles/pony/celt.py
@@ -5,7 +5,7 @@ def main(roster_id, **kwargs):
     consist = EngineConsist(
         roster_id=roster_id,
         id="celt",
-        base_numeric_id=35850,
+        base_numeric_id=31010,
         name="2-8-4 Celt",
         role="freight",
         role_child_branch_num=-1,
