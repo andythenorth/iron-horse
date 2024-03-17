7fd670fdddb5fce0a8c0fd28cd706ef712f28f18
Andy <andy@teamrubber.com>
2024-03-17 20:43:34 +0000

    Change: sprites complete for Golfinho

--- src/vehicles/pony/golfinho.py
+++ src/vehicles/pony/golfinho.py
@@ -13,9 +13,10 @@ def main(roster_id, **kwargs):
             "DIESEL": 900,
         },
         gen=4,
+        fixed_run_cost_points=20, # balance against Snapper
         extended_vehicle_life=True,  # extended vehicle life for all this generation of NG eh
         pax_car_capacity_type="railbus_combine_ng_2",  # specific to combined mail + pax consist
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(
