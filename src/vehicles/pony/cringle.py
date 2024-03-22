97e8cb9e0c52435c11bdf1cd5e50e8c6cfa3cba0
Andy <andy@teamrubber.com>
2024-03-22 22:16:56 +0000

    Change: don't offset intro dates for metro locomotives

--- src/vehicles/pony/cringle.py
+++ src/vehicles/pony/cringle.py
@@ -14,7 +14,6 @@ def main(roster_id, **kwargs):
         },
         random_reverse=True,
         gen=2,
-        intro_year_offset=3,  # introduce later than gen epoch by design
         fixed_run_cost_points=120,  # substantial cost bonus for balance against same-era steam engines
         extended_vehicle_life=True,
         # note that livery names are metadata only and can repeat for different spriterows
