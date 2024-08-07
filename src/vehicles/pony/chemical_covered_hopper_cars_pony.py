f9b3546f1b8d3f5321499d411b05b8d97cb261eb
Andy <andy@teamrubber.com>
2024-08-07 07:06:39 +0100

    Change: improved appearance of pony gen 5 6/8 and 8/8 covered hoppers

--- src/vehicles/pony/chemical_covered_hopper_cars_pony.py
+++ src/vehicles/pony/chemical_covered_hopper_cars_pony.py
@@ -103,7 +103,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=26910,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_24px")
@@ -114,7 +114,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=36920,
         gen=5,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_half_filled_greebled_32px")
