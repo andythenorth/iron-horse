6d413a829721b4146f95fd69a85f8cadcb0bf41c
Andy <andy@teamrubber.com>
2024-08-18 10:00:22 +0100

    Codechange: pony type 3 silo cars done, for now

--- src/vehicles/pony/silo_cars_type_3_pony.py
+++ src/vehicles/pony/silo_cars_type_3_pony.py
@@ -10,7 +10,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=27990,
         gen=4,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
@@ -21,7 +21,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=28010,
         gen=4,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_24px")
@@ -32,7 +32,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=28030,
         gen=4,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_greebled_32px")
@@ -43,7 +43,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=28050,
         gen=5,
         subtype="A",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_gapped_greebled_16px")
@@ -54,7 +54,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=28070,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_greebled_24px")
@@ -65,7 +65,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=28090,
         gen=5,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_greebled_32px")
