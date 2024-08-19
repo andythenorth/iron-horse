643a67fef32b78662180df5a81d137f0f01a0f0d
Andy <andy@teamrubber.com>
2024-08-19 20:27:17 +0100

    Change: improved sprites for gen 1 and 2 mill open cars

--- src/vehicles/pony/mill_open_cars_pony.py
+++ src/vehicles/pony/mill_open_cars_pony.py
@@ -28,7 +28,7 @@ def main(roster_id, **kwargs):
         sprites_complete=True,
     )
 
-    consist.add_unit(type=FreightCar, chassis="4_axle_gapped_24px")
+    consist.add_unit(type=FreightCar, chassis="3_axle_gapped_24px")
 
     consist = OpenCarMillConsist(
         roster_id=roster_id,
