3b6eceb652b7028502505bbf46405cfc9bdf48f7
Andy <andy@teamrubber.com>
2024-08-21 20:36:58 +0100

    Change: add NG pony merchandise box cars and randomised box cars

--- src/vehicles/pony/merchandise_box_cars_pony.py
+++ src/vehicles/pony/merchandise_box_cars_pony.py
@@ -2,6 +2,46 @@ from train import BoxCarMerchandiseConsist, FreightCar
 
 
 def main(roster_id, **kwargs):
+    # --------------- narrow gauge -----------------------------------------------------------------
+
+    consist = BoxCarMerchandiseConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=24370,
+        gen=1,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_16px")
+
+    # no gen 2 for NG, straight to gen 3
+
+    consist = BoxCarMerchandiseConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=24390,
+        gen=3,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_16px")
+
+    consist = BoxCarMerchandiseConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=24410,
+        gen=3,
+        subtype="B",
+        base_track_type_name="NG",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_24px")
+
     # --------------- standard gauge ---------------------------------------------------------------
 
     consist = BoxCarMerchandiseConsist(
