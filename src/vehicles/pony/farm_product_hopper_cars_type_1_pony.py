f48b668dc47ee6766f8537efdc7294f3e036c0f6
Andy <andy@teamrubber.com>
2024-08-21 21:28:05 +0100

    Change: add NG pony farm product hoppers

--- src/vehicles/pony/farm_product_hopper_cars_type_1_pony.py
+++ src/vehicles/pony/farm_product_hopper_cars_type_1_pony.py
@@ -2,6 +2,45 @@ from train import FarmProductsHopperCarConsistType1, FreightCar
 
 
 def main(roster_id, **kwargs):
+    # --------------- narrow gauge -----------------------------------------------------------------
+    consist = FarmProductsHopperCarConsistType1(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31350,
+        gen=2,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_16px")
+
+    # no gen 2 for NG, straight to gen 3
+
+    consist = FarmProductsHopperCarConsistType1(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31360,
+        gen=3,
+        subtype="A",
+        base_track_type_name="NG",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_16px")
+
+    consist = FarmProductsHopperCarConsistType1(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31370,
+        gen=3,
+        subtype="B",
+        base_track_type_name="NG",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="4_axle_ng_24px")
+
     # --------------- standard gauge ---------------------------------------------------------------
 
     consist = FarmProductsHopperCarConsistType1(
