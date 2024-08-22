3b8ad428747131601d8b8214d98df3ffdbd67ce8
Andy <andy@teamrubber.com>
2024-08-22 21:03:09 +0100

    Codechange: WIP on pony gen 5D twin hi-cube sliding roof van

--- src/vehicles/pony/sliding_roof_hi_cube_cars_pony.py
+++ src/vehicles/pony/sliding_roof_hi_cube_cars_pony.py
@@ -3,7 +3,6 @@ from train import SlidingRoofCarConsistHiCube, FreightCar
 
 def main(roster_id, **kwargs):
     # --------------- standard gauge ---------------------------------------------------------------
-    # gen 5 start, only B and C lengths
 
     consist = SlidingRoofCarConsistHiCube(
         roster_id=roster_id,
@@ -26,3 +25,27 @@ def main(roster_id, **kwargs):
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_32px")
+    """
+    consist = SlidingRoofCarConsistHiCube(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=840,
+        gen=5,
+        subtype="D",
+        sprites_complete=False,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        chassis="2_axle_1cc_filled_20px",
+        symmetry_type="asymmetric",
+        spriterow_num=0,
+    )
+
+    consist.add_unit(
+        type=FreightCar,
+        chassis="2_axle_1cc_filled_20px",
+        symmetry_type="asymmetric",
+        force_spriterow_group_in_output_spritesheet=1,  # special case
+    )
+    """
