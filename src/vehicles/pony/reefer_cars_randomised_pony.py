5b91994c53efae8ef9a90a9311f01afa1b2f2611
Andy <andy@teamrubber.com>
2024-08-22 22:41:57 +0100

    Change: add gen 3 and 4 pony randomised reefer cars

--- src/vehicles/pony/reefer_cars_randomised_pony.py
+++ src/vehicles/pony/reefer_cars_randomised_pony.py
@@ -8,6 +8,62 @@ def main(roster_id, **kwargs):
     consist = ReeferCarRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31930,
+        gen=3,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
+    consist = ReeferCarRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31940,
+        gen=3,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_24px")
+
+
+    consist = ReeferCarRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31920,
+        gen=4,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_16px")
+
+    consist = ReeferCarRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31900,
+        gen=4,
+        subtype="B",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_24px")
+
+    consist = ReeferCarRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=31910,
+        gen=4,
+        subtype="C",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="empty_32px")
+
+    consist = ReeferCarRandomisedConsist(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=23060,
         gen=5,
         subtype="B",
