0475d26a5d0e192f06e52d1b5b49901886030747
Andy <andy@teamrubber.com>
2024-09-15 08:05:42 +0100

    Change: sprites complete for gen 4 and 5 pipe cars

--- src/vehicles/pony/pipe_cars_pony.py
+++ src/vehicles/pony/pipe_cars_pony.py
@@ -4,82 +4,14 @@ from train import PipeCarConsist, FreightCar
 def main(roster_id, **kwargs):
 
     # --------------- standard gauge ---------------------------------------------------------------
-    """
-    consist = PipeCarConsist(
-        roster_id=roster_id,
-        roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=30460,
-        gen=1,
-        subtype="A",
-        sprites_complete=False,
-    )
-
-    consist.add_unit(type=FreightCar, chassis="2_axle_filled_16px")
-
-    # no gen 2A, gen 1A continues in pony
 
     consist = PipeCarConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21190,
-        gen=2,
-        subtype="B",
-        sprites_complete=False,
-    )
-
-    consist.add_unit(type=FreightCar, chassis="3_axle_gapped_24px")
-
-    consist = PipeCarConsist(
-        roster_id=roster_id,
-        roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21390,
-        gen=3,
-        subtype="A",
-        sprites_complete=False,
-    )
-
-    consist.add_unit(type=FreightCar, chassis="2_axle_filled_16px")
-
-    consist = PipeCarConsist(
-        roster_id=roster_id,
-        roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21200,
-        gen=3,
-        subtype="B",
-        sprites_complete=False,
-    )
-
-    consist.add_unit(type=FreightCar, chassis="4_axle_gapped_24px")
-
-    consist = PipeCarConsist(
-        roster_id=roster_id,
-        roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21590,
-        gen=3,
-        subtype="C",
-        sprites_complete=False,
-    )
-
-    consist.add_unit(type=FreightCar, chassis="4_axle_gapped_32px")
-
-    consist = PipeCarConsist(
-        roster_id=roster_id,
-        roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21600,
-        gen=4,
-        subtype="A",
-        sprites_complete=False,
-    )
-
-    consist.add_unit(type=FreightCar, chassis="2_axle_filled_16px")
-
-    consist = PipeCarConsist(
-        roster_id=roster_id,
-        roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21580,
+        base_numeric_id=20010,
         gen=4,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_24px")
@@ -87,21 +19,21 @@ def main(roster_id, **kwargs):
     consist = PipeCarConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=21570,
+        base_numeric_id=28490,
         gen=4,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_gapped_32px")
-    """
+
     consist = PipeCarConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=35590,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_24px")
@@ -112,7 +44,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=34830,
         gen=5,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_1cc_filled_32px")
