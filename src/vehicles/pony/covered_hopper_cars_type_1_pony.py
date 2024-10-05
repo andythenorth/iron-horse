dadeb9889629bda89bce9b9bc33123a6026dcb35
Andy <andy@teamrubber.com>
2024-10-05 12:02:07 +0100

    Change: add gen 5A type 1 covered hopper car

--- src/vehicles/pony/covered_hopper_cars_type_1_pony.py
+++ src/vehicles/pony/covered_hopper_cars_type_1_pony.py
@@ -89,6 +89,17 @@ def main(roster_id, **kwargs):
     consist = CoveredHopperCarConsistType1(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
+        base_numeric_id=36980,
+        gen=5,
+        subtype="A",
+        sprites_complete=True,
+    )
+
+    consist.add_unit(type=FreightCar, chassis="2_axle_gapped_16px")
+
+    consist = CoveredHopperCarConsistType1(
+        roster_id=roster_id,
+        roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=23860,
         gen=5,
         subtype="B",
