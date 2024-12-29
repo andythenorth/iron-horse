b3b3f102d2ec8e45dc97316ab0ffdefa983d88ec
Andy <andy@teamrubber.com>
2024-12-29 22:30:07 +0000

    Change: more randomised tank cars

--- src/vehicles/pony/tank_cars_randomised_pony.py
+++ src/vehicles/pony/tank_cars_randomised_pony.py
@@ -7,7 +7,7 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31160,
+        base_numeric_id=37020,
         gen=2,
         subtype="A",
         intro_year_offset=-10,  # let's be earlier for this one
@@ -19,7 +19,7 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31180,
+        base_numeric_id=32900,
         gen=3,
         subtype="A",
         sprites_complete=True,
@@ -30,7 +30,7 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31200,
+        base_numeric_id=32940,
         gen=3,
         subtype="B",
         sprites_complete=True,
@@ -41,7 +41,7 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31220,
+        base_numeric_id=35900,
         gen=4,
         subtype="A",
         sprites_complete=True,
@@ -52,7 +52,7 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31240,
+        base_numeric_id=35920,
         gen=4,
         subtype="B",
         sprites_complete=True,
@@ -63,18 +63,18 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31020,
+        base_numeric_id=35940,
         gen=4,
         subtype="C",
         sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="empty_32px")
-
+    """
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31040,
+        base_numeric_id=35960,
         gen=5,
         subtype="A",
         sprites_complete=True,
@@ -85,14 +85,14 @@ def main(roster_id, **kwargs):
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
-        base_numeric_id=31260,
+        base_numeric_id=35980,
         gen=5,
         subtype="B",
         sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="empty_24px")
-    """
+
     consist = TankCarStandardRandomisedConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
