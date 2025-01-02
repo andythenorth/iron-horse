41628227239807b3f29307ba1342bc57d98686bb
Andy <andy@teamrubber.com>
2025-01-02 22:18:58 +0000

    Change: gen 5 type 2 farm products vans; improved sprites for gen 5 type 1 farm products vans

--- src/vehicles/pony/farm_product_box_cars_type_2_pony.py
+++ src/vehicles/pony/farm_product_box_cars_type_2_pony.py
@@ -123,14 +123,14 @@ def main(roster_id, **kwargs):
         suppress_roof_sprite=False,  # non-standard roof for this wagon
         chassis="4_axle_1cc_filled_32px",
     )
-
+    """
     consist = FarmProductsBoxCarConsistType2(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
         base_numeric_id=28430,
         gen=5,
         subtype="B",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="2_axle_filled_greebled_24px")
@@ -141,8 +141,7 @@ def main(roster_id, **kwargs):
         base_numeric_id=32960,
         gen=5,
         subtype="C",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     consist.add_unit(type=FreightCar, chassis="4_axle_filled_greebled_32px")
-    """
