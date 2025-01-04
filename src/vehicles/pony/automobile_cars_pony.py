c28cf46d53999447deb45dc54c01fc901889b4c8
Andy <andy@teamrubber.com>
2025-01-04 15:06:04 +0000

    Change: add gen 3B automobile car

--- src/vehicles/pony/automobile_cars_pony.py
+++ src/vehicles/pony/automobile_cars_pony.py
@@ -3,18 +3,18 @@ from train import AutomobileCarConsist, AutomobileCarSymmetric
 
 def main(roster_id, **kwargs):
     # --------------- standard gauge ---------------------------------------------------------------    # no gen 1 or 2, straight to gen 3
-    """
+
     consist = AutomobileCarConsist(
         roster_id=roster_id,
         roster_id_providing_module = kwargs["roster_id_providing_module"],
-        base_numeric_id=24790,
+        base_numeric_id=22540,
         gen=3,
         subtype="B",
-        sprites_complete=True,
+        sprites_complete=False,
     )
 
     consist.add_unit(type=AutomobileCarSymmetric, chassis="3_axle_solid_express_24px")
-    """
+
     consist = AutomobileCarConsist(
         roster_id=roster_id,
         roster_id_providing_module=kwargs["roster_id_providing_module"],
