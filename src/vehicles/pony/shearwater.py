3025afd7f7d71179fdfa0c0295dc2acd208818e0
Andy <andy@teamrubber.com>
2025-05-16 21:50:00 +0100

    Change: WIP on Gwynt sprite

--- src/vehicles/pony/shearwater.py
+++ src/vehicles/pony/shearwater.py
@@ -7,8 +7,8 @@ def main(**kwargs):
     model_def = ModelDef(
         class_name="SimpleEngine",
         model_id="shearwater",
-        base_numeric_id=15290,
-        name="0-6-0 Gwynt",
+        base_numeric_id=30080,
+        name="0-8-4 Gwynt",
         subrole="freight",
         subrole_child_branch_num=-1,
         power_by_power_source={
@@ -20,22 +20,15 @@ def main(**kwargs):
         intro_year_offset=5,  # let's be a little bit later for this one
         extended_vehicle_life=True,
         # note that livery names are metadata only and can repeat for different spriterows
-        liveries=["VANILLA", "FREIGHT_BLACK"],
+        liveries=["VANILLA", "SWOOSH", "FREIGHT_BLACK"],
         sprites_complete=False,
     )
 
     model_def.add_unit_def(
         class_name="SteamEnginePoweredUnit",
-        weight=65,
-        vehicle_length=5,
+        weight=99,
+        vehicle_length=8,
         rel_spriterow_index=0,
-    )
-
-    model_def.add_unit_def(
-        class_name="SteamEngineTenderUnit",
-        weight=31,
-        vehicle_length=3,
-        rel_spriterow_index=1,
     )
 
     model_def.define_description("""""")
@@ -44,6 +37,7 @@ def main(**kwargs):
     )  # ACTUALLY GWR 2251 CABBAGE, LNER J36 (NBR Class C)
     # NOPE - swap to 2-6-0 https://en.wikipedia.org/wiki/SECR_N1_class
     # AND - https://en.wikipedia.org/wiki/SECR_N_class
+    # NOPE GWR 7200 ish
 
     result.append(model_def)
 
