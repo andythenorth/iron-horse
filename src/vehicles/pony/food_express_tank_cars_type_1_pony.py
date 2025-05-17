6cf4a4c0f6db5dc2ba7606cb48d06bdbf27a7fe9
Andy <andy@teamrubber.com>
2025-05-17 20:37:17 +0100

    Change: sprites complete for NG express food tankers

--- src/vehicles/pony/food_express_tank_cars_type_1_pony.py
+++ src/vehicles/pony/food_express_tank_cars_type_1_pony.py
@@ -54,7 +54,7 @@ def main(**kwargs):
         gen=4,
         subtype="A",
         base_track_type="NG",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     model_def.add_unit_def(class_name="FreightCarUnit", chassis="4_axle_ng_sparse_16px")
@@ -67,7 +67,7 @@ def main(**kwargs):
         gen=4,
         subtype="B",
         base_track_type="NG",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     model_def.add_unit_def(class_name="FreightCarUnit", chassis="4_axle_ng_sparse_24px")
