913463f6fedc08ddba03aab2f773a9e40b9922d6
Andy <andy@teamrubber.com>
2025-06-16 22:30:52 +0100

    Change: twin Pikel show cab leading, not calf

--- src/vehicles/pony/pikel.py
+++ src/vehicles/pony/pikel.py
@@ -45,12 +45,12 @@ def main(**kwargs):
 
     # this is a JFDI thing, the 2-unit version varies sprites per unit position, which is generally supported
     # but the *buy menu* compositor does not support that as of Jan 2024, so hax
-    model_def_clone.unit_defs[0].rel_spriterow_index = 1
+    model_def_clone.unit_defs[0].rel_spriterow_index = 0
     model_def_clone.add_unit_def(
         class_name="DieselEngineUnit",
         weight=22,
         vehicle_length=4,
-        rel_spriterow_index=0,
+        rel_spriterow_index=1,
     )
 
     model_def = model_def_clone.complete_clone()
