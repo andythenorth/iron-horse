d2582a549675180b20e07337181413a3725fcdf1
Andy <andy@teamrubber.com>
2025-12-17 21:59:06 +0000

    Codechange: placate pyflakes for train/catalogue.py

--- src/train/catalogue.py
+++ src/train/catalogue.py
@@ -1,6 +1,9 @@
 import random
 from dataclasses import dataclass
 from typing import List
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .livery import LiveryDef
 from functools import cached_property
 
 import iron_horse
