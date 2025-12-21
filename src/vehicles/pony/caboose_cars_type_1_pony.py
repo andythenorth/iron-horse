e2ddecdc8127fd2738d54898a75b4bc4a6014266
Andy <andy@teamrubber.com>
2025-12-21 20:51:45 +0000

    Change: improved sprites complete for cabooses

--- src/vehicles/pony/caboose_cars_type_1_pony.py
+++ src/vehicles/pony/caboose_cars_type_1_pony.py
@@ -25,7 +25,7 @@ def main(**kwargs):
         gen=1,
         subtype="B",
         base_track_type="NG",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     model_def.add_unit_def(unit_cls_name="CabooseCarUnit", chassis="4_axle_ng_16px")
--- src/vehicles/pony/caboose_cars_type_2_pony.py
+++ src/vehicles/pony/caboose_cars_type_2_pony.py
@@ -12,7 +12,7 @@ def main(**kwargs):
         gen=1,
         subtype="A",
         base_track_type="NG",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     model_def.add_unit_def(unit_cls_name="CabooseCarUnit", chassis="2_axle_ng_8px")
@@ -25,7 +25,7 @@ def main(**kwargs):
         gen=1,
         subtype="B",
         base_track_type="NG",
-        sprites_complete=False,
+        sprites_complete=True,
     )
 
     model_def.add_unit_def(unit_cls_name="CabooseCarUnit", chassis="4_axle_ng_16px")
