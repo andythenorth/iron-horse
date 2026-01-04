07852377e95e37a6c3d6628b379d2741a893d49b
Andy <andy@teamrubber.com>
2026-01-04 19:41:44 +0000

    Codechange: don't generate ih_variant_group_id debug-only badges needlessly, there were 3974 of them

--- src/train/schemas.py
+++ src/train/schemas.py
@@ -372,10 +372,11 @@ class SchemaBase(object):
         if getattr(self, "subtype", None) is not None:
             result.append("ih_wagon_subtype/" + self.subtype.lower())
         # variant_group_id is for debug only, variant groups in game determined by standalone action 0 prop
-        if self.catalogue_entry.variant_group_id is not None:
-            result.append(
-                "ih_variant_group_id/" + self.catalogue_entry.variant_group_id
-            )
+        if False: # make True to turn on debug
+            if self.catalogue_entry.variant_group_id is not None:
+                result.append(
+                    "ih_variant_group_id/" + self.catalogue_entry.variant_group_id
+                )
         return result
 
     @cached_property
