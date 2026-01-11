1209f3153339408e2912d241a2c682565868325f
Andy <andy@teamrubber.com>
2026-01-11 21:44:35 +0000

    Change: WIP on gen 4 tarpaulin coil carriers

--- src/vehicles/pony/coil_cars_tarpaulin_pony.py
+++ src/vehicles/pony/coil_cars_tarpaulin_pony.py
@@ -5,7 +5,35 @@ def main(**kwargs):
     result = []
 
     # --------------- standard gauge ---------------------------------------------------------------
-    # gen 5 start, only B and C lengths
+    # gen 4 start
+
+    model_def = ModelDef(
+        schema_name="CoilCarTarpaulin",
+        base_numeric_id=16960,
+        gen=4,
+        subtype="B",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="FreightCarUnit", chassis="4_axle_gapped_24px"
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="CoilCarTarpaulin",
+        base_numeric_id=17000,
+        gen=4,
+        subtype="C",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="FreightCarUnit", chassis="4_axle_gapped_32px"
+    )
+
+    result.append(model_def)
 
     model_def = ModelDef(
         schema_name="CoilCarTarpaulin",
