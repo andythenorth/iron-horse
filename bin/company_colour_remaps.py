0cc8ed0f30d39b710427076ab7281ce78f8d5eef
Andy <andy@teamrubber.com>
2026-01-16 20:22:16 +0000

    Change: additional livery for Wyvern

--- bin/company_colour_remaps.py
+++ bin/company_colour_remaps.py
@@ -9,7 +9,7 @@ from polar_fox import graphics_units
 # requires a tmp dir to exist, which should be gitignored
 # set the filename
 input_filenames = [
-    "reliance",
+    "wyvern",
 ]
 
 CC1 = 198
