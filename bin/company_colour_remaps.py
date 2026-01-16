b9042a5a7739b574fd70079e2b32c15835739fb3
Andy <andy@teamrubber.com>
2026-01-16 20:39:36 +0000

    Change: add livery for Hurly Burly

--- bin/company_colour_remaps.py
+++ bin/company_colour_remaps.py
@@ -9,7 +9,7 @@ from polar_fox import graphics_units
 # requires a tmp dir to exist, which should be gitignored
 # set the filename
 input_filenames = [
-    "swift",
+    "hurly_burly",
 ]
 
 CC1 = 198
