d1355c57a5776f3ee938611bcc9fcc1ffe5be3b7
Andy <andy@teamrubber.com>
2026-01-30 06:54:47 +0000

    Fix: missing refit support for legacy cargos on flatbed wagons

--- src/train/schemas.py
+++ src/train/schemas.py
@@ -5035,7 +5035,9 @@ class FlatCarBase(CarSchemaBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.class_refit_groups = ["flatbed_freight"]
-        self.label_refits_allowed = ["GOOD"]
+        self.label_refits_allowed = polar_fox.constants.allowed_refits_by_label[
+            "legacy_allowed_flatbed"
+        ]
         self.label_refits_disallowed = []
         self.default_cargos = polar_fox.constants.default_cargos["flat"]
         self._intro_date_months_offset = global_constants.intro_month_offsets_by_role[
