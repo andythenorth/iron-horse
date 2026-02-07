12bd62d55832840aebf9da4ea539ad2f54491fae
Andy <andy@teamrubber.com>
2026-02-07 09:47:42 +0000

    Change: WIP on drop-centre flat cars

---
+++ src/vehicles/pony/drop_centre_flat_cars_type_1_pony.py
@@ -0,0 +1,129 @@
+from train.model_def import ModelDef
+
+
+def main(**kwargs):
+    result = []
+
+    # --------------- standard gauge ---------------------------------------------------------------
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=32730,
+        gen=2,
+        subtype="B",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_24px"
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=32840,
+        gen=2,
+        subtype="C",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_32px",
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=32880,
+        gen=3,
+        subtype="B",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_24px"
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=32900,
+        gen=3,
+        subtype="C",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_32px",
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=32920,
+        gen=4,
+        subtype="B",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_24px"
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=32940,
+        gen=4,
+        subtype="C",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_32px",
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=33410,
+        gen=5,
+        subtype="B",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_24px"
+    )
+
+    result.append(model_def)
+
+    model_def = ModelDef(
+        schema_name="FlatCarDropCentreType1",
+        base_numeric_id=33450,
+        gen=5,
+        subtype="C",
+        sprites_complete=False,
+    )
+
+    model_def.add_unit_def(
+        unit_cls_name="AutomobileCarAsymmetricUnit", # CABBAGE
+        chassis="4_axle_running_gear_only_32px",
+    )
+
+    result.append(model_def)
+
+    return result
