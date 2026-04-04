8d1f81b9ab0c0862cf210e0afe11557431211f5c
Andy <andy@teamrubber.com>
2026-04-04 11:19:32 +0100

    Change: swap Cheddar Valley ARC-ish livery to Bumblebee

--- src/vehicles/pony/cheddar_valley.py
+++ src/vehicles/pony/cheddar_valley.py
@@ -21,7 +21,7 @@ def main(**kwargs):
         gen=5,
         fixed_run_cost_points=210,  # unrealism: run cost nerf for being so high-powered
         # unfinished EWS livery exists, but eh
-        liveries=["LOWER_LINES", "LOWER_LINES", "FREIGHTLINER_GBRF", "SHOW_PONY", "SHOW_PONY"],
+        liveries=["LOWER_LINES", "LOWER_LINES", "FREIGHTLINER_GBRF", "SHOW_PONY", "BUMBLEBEE"],
         sprites_complete=True,
     )
 
