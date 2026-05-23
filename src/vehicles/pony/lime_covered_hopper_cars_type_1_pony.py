7cf05f28f4bf82a04112ae069449d94c5be03dbe
Andy <andy@teamrubber.com>
2026-05-23 22:10:27 +0100

    Change: use hopper chassis for ore hoppers, some covered hoppers

--- src/vehicles/pony/lime_covered_hopper_cars_type_1_pony.py
+++ src/vehicles/pony/lime_covered_hopper_cars_type_1_pony.py
@@ -38,7 +38,7 @@ def main(**kwargs):
         sprites_complete=True,
     )
 
-    model_def.add_unit_def(unit_cls_name="FreightCarUnit", chassis="2_axle_gapped_16px")
+    model_def.add_unit_def(unit_cls_name="FreightCarUnit", chassis="2_axle_gapped_hopper_16px")
 
     result.append(model_def)
 
@@ -50,7 +50,7 @@ def main(**kwargs):
         sprites_complete=True,
     )
 
-    model_def.add_unit_def(unit_cls_name="FreightCarUnit", chassis="2_axle_gapped_24px")
+    model_def.add_unit_def(unit_cls_name="FreightCarUnit", chassis="2_axle_gapped_hopper_24px")
 
     result.append(model_def)
 
