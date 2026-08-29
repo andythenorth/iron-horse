d42164772439348cf260622499436f5d27c931a2
Andy <andy@teamrubber.com>
2026-08-29 11:01:13 +0100

    Change: additional liveries for Haar

--- src/vehicles/pony/haar.py
+++ src/vehicles/pony/haar.py
@@ -16,7 +16,7 @@ def main(**kwargs):
         },
         tractive_effort_coefficient=0.24,
         gen=3,
-        liveries=["WORKHORSE", "FREIGHT_BLACK"],
+        liveries=["WORKHORSE", "CONVENTIONAL_WISDOM", "STOCK_STANDARD", "FREIGHT_BLACK"],
         sprites_complete=True,
     )
 
